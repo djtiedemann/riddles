17ecf5952863e9740848e5d349965f83ba308775
djtiedemann <dtiedemann91@gmail.com>
2019-11-24 16:28:52 -0500

    Adding solution for continuous state string

---
+++ states-continuous-string/stateStringSolver.py
@@ -0,0 +1,55 @@
+import sys
+
+class StateSolver:
+	def __init__(self):
+		self.count = 0
+
+	def solve(self, allPossibleStates, hash):
+		biggestString = ''
+		for state in allPossibleStates:
+			newString = self.getBiggestString(state, hash)
+			print(newString)
+			sys.stdout.flush()
+			if(len(newString) > len(biggestString)):
+				biggestString = newString
+		return biggestString
+		
+	def getBiggestString(self, previousStates, hash):
+		biggestString = previousStates
+		mostRecentState = previousStates[-2:]
+		possibleNextStates = hash[mostRecentState]
+		for nextState in possibleNextStates:
+			if(nextState not in previousStates):
+				newString = previousStates + nextState[1]
+				newBiggestString = self.getBiggestString(newString, hash)				
+				if len(newBiggestString) > len(biggestString):
+					biggestString = newBiggestString
+		return biggestString
+		
+	def buildHash(self, allPossibleStates):
+		hash = {}
+		for state in allPossibleStates:
+			validPairs = []
+			for secondState in allPossibleStates:
+				if(secondState[0] == state[1] and secondState != state):
+					validPairs.append(secondState)
+			hash[state] = validPairs
+		#print(hash)
+		return hash
+					
+					
+		
+file = open('stateInput.txt', 'r')
+line = file.readline()
+states = []
+while line:
+	states.append(line.rstrip())
+	line = file.readline()
+solver = StateSolver()
+hash = solver.buildHash(states)
+biggestString = solver.solve(states, hash)
+print('---')
+print(biggestString)
+
+##
+solution = 'FMNVIDCALAKSCOHINCTNMPWVARIASDE'
