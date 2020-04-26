52b88852e14396ac348299232f34fb8fe9b6be8a
djtiedemann <dtiedemann91@gmail.com>
2020-04-26 12:27:24 -0400

    Added in playlist search, outline of trenchcoat

---
+++ trench-coat/trenchcoat.py
@@ -0,0 +1,19 @@
+# https://fivethirtyeight.com/features/can-you-best-the-mysterious-man-in-the-trench-coat/
+# a person in a trench coat has a number between $1 and $1000 and will give you the money if you guess the number
+# you get 9 guesses, after each one, you'll get higher or lower
+class Trenchcoat:
+	def __init__(self):
+		self.lookupMatrix = []
+		
+	def guess(self, guess, currentMin, currentMax, numGuessesRemainingAfterThisGuess):
+		numValuesThatCouldHaveBeenGuessed = (currentMax - currentMin + 1)
+		pGuessIsCorrect = 1.0 / numValuesThatCouldHaveBeenGuessed
+		pGuessIsLow = (guess - currentMin) / numValuesThatCouldHaveBeenGuessed
+		pGuessIsHigh = (currentMax - guess) / numValuesThatCouldHaveBeenGuessed
+		print('sample size: ' + str(numValuesThatCouldHaveBeenGuessed))
+		print('pGuessIsCorrect: ' + str(pGuessIsCorrect))
+		print('pGuessIsLow: ' + str(pGuessIsLow))
+		print('pGuessIsHigh: ' + str(pGuessIsHigh))
+		
+trenchcoat = Trenchcoat()
+trenchcoat.guess(1, 10, 3, 0)
