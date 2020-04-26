efa9295f47cdeec8604fd330063cb2665fdfd293
djtiedemann <dtiedemann91@gmail.com>
2020-04-26 13:18:09 -0400

    added in correct calculation for recursive case without memoization

--- trench-coat/trenchcoat.py
+++ trench-coat/trenchcoat.py
@@ -12,14 +12,27 @@ class Trenchcoat:
 		for guess in range(currentMin, currentMax+1):
 			pGuessIsCorrect = (1.0 if guess >= currentMin and guess <= currentMax else 0.0) / numValuesThatCouldHaveBeenGuessed
 			pGuessIsLow = (guess - currentMin) / numValuesThatCouldHaveBeenGuessed if guess >= currentMin and guess <= currentMax else (1.0 if guess < currentMin else 0.0)
-			pGuessIsHigh = (currentMax - guess) / numValuesThatCouldHaveBeenGuessed if guess >= currentMin and guess <= currentMax else (0.0 if guess < currentMin else 1.0)		
+			pGuessIsHigh = (currentMax - guess) / numValuesThatCouldHaveBeenGuessed if guess >= currentMin and guess <= currentMax else (0.0 if guess < currentMin else 1.0)
+			
 			expectedValue = guess * pGuessIsCorrect
+			if (numGuessesRemainingAfterThisGuess > 0):
+				outcomeForLowGuess = (self.guess(currentMin, guess-1,numGuessesRemainingAfterThisGuess-1)[1] if guess > currentMin else 0) * pGuessIsLow
+				outcomeForHighGuess = (self.guess(guess+1, currentMax, numGuessesRemainingAfterThisGuess-1)[1] if guess < currentMax else 0) * pGuessIsHigh
+				print('guess: ' + str(guess))
+				print('outcomeForLowGuess: ' + str(outcomeForLowGuess))
+				print('outcomeForHighGuess: ' + str(outcomeForHighGuess))
+				print('outcomeForCurrentGuess: ' + str(expectedValue))
+				expectedValue = outcomeForLowGuess + outcomeForHighGuess + expectedValue
+				print('expectedValue: ' + str(expectedValue))
+				print('')
+			
 			if (expectedValue > bestExpectedValue):
 				bestExpectedValue = expectedValue
 				bestGuess = guess
+		
 		return (bestGuess, bestExpectedValue)
 		
 trenchcoat = Trenchcoat()
-(bestGuess, bestExpectedValue) = trenchcoat.guess(1, 10, 0)
+(bestGuess, bestExpectedValue) = trenchcoat.guess(1, 10, 1)
 print(bestGuess)
 print(bestExpectedValue)
