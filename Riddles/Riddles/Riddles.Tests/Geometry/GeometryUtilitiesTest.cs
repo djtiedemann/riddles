bb69f445d3efe3db0ebb01bf8767f39e472f0136
djtiedemann <dtiedemann91@gmail.com>
2020-07-31 22:49:01 -0400

    Added in quadratic formula + tests

--- Riddles/Riddles/Riddles.Tests/Geometry/GeometryUtilitiesTest.cs
+++ Riddles/Riddles/Riddles.Tests/Geometry/GeometryUtilitiesTest.cs
@@ -1,18 +1,41 @@
 using NUnit.Framework;
+using Riddles.Geometry;
+using System;
 
 namespace Riddles.Tests
 {
     public class Tests
     {
+        const double Epsilon = 0.000001;
+
         [SetUp]
         public void Setup()
         {
         }
 
-        [Test]
-        public void TestQuadraticFormula()
+        [TestCase(1, 1, 1, null, null, Description = "x^2 + x + 1, no solutions")]
+        [TestCase(1, 0, 0, 0, null, Description = "x^2, solution: x = 0")]
+        [TestCase(1, 4, 4, -2, null, Description = "x^2 + 4x + 4, solution: x = -2")]
+        [TestCase(1, -2, -3, -1, 3, Description = "x^2 - 2x - 3, solution: x = -1, 3")]
+        [TestCase(8, -2, -3, -0.5, 0.75, Description = "(2x + 1)(4x - 3) = 8x^2 - 2x - 3")]
+        public void TestQuadraticFormula(double a, double b, double c, double? solution1, double? solution2)
         {
-            Assert.Pass();
+            GeometryUtilities utilities = new GeometryUtilities();
+            var quadraticFormulaSolutions = utilities.QuadraticFormula(a, b, c);
+            if(solution1 == null)
+			{
+                Assert.AreEqual(quadraticFormulaSolutions.Count, 0);
+                return;
+			}
+            if(solution2 == null)
+			{
+                Assert.AreEqual(quadraticFormulaSolutions.Count, 1);
+                Assert.LessOrEqual(Math.Abs(solution1.Value - quadraticFormulaSolutions[0]), Epsilon);
+                return;
+			}
+            Assert.AreEqual(quadraticFormulaSolutions.Count, 2);
+            Assert.LessOrEqual(Math.Abs(solution1.Value - quadraticFormulaSolutions[0]), Epsilon);
+            Assert.LessOrEqual(Math.Abs(solution2.Value - quadraticFormulaSolutions[1]), Epsilon);
         }
     }
 }
--- Riddles/Riddles/Riddles/Geometry/GeometryUtilities.cs
+++ Riddles/Riddles/Riddles/Geometry/GeometryUtilities.cs
@@ -6,6 +6,13 @@ namespace Riddles.Geometry
 {
     public class GeometryUtilities
     {
+        /// <summary>
+        /// Returns the distinct solutions to ax^2 + bx + c = 0
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
         public List<double> QuadraticFormula(double a, double b, double c)
         {
             if(4*a*c > Math.Pow(b, 2))
@@ -13,13 +20,14 @@ namespace Riddles.Geometry
                 return new List<double>();
             }
 
-            double maxYValue = (-1 * b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
             List<double> solution = new List<double>();
-            solution.Add(maxYValue);
+            double minYValue = (-1 * b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
+            solution.Add(minYValue);
+            
             if (4*a*c < Math.Pow(b, 2))
             {
-                double minYValue = (-1 * b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
-                solution.Add(minYValue);
+                double maxYValue = (-1 * b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
+                solution.Add(maxYValue);
             }
             return solution;
         }
