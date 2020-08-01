c3b8c75e94df847c65ca19eab2e37692b0c01de3
djtiedemann <dtiedemann91@gmail.com>
2020-07-31 21:56:58 -0400

    Adding in test project

---
+++ Riddles/Riddles/Riddles.Tests/Geometry/GeometryUtilitiesTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Riddles.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestQuadraticFormula()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Geometry/GeometryUtilities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riddles.Geometry
+{
+    public class GeometryUtilities
+    {
+        public List<double> QuadraticFormula(double a, double b, double c)
+        {
+            if(4*a*c > Math.Pow(b, 2))
+            {
+                return new List<double>();
+            }
+
+            double maxYValue = (-1 * b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
+            List<double> solution = new List<double>();
+            solution.Add(maxYValue);
+            if (4*a*c < Math.Pow(b, 2))
+            {
+                double minYValue = (-1 * b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
+                solution.Add(minYValue);
+            }
+            return solution;
+        }
+    }
+}
