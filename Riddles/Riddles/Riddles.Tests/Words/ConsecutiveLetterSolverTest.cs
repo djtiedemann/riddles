0de4c0f8891ba04abf5eca73cc176ccde786c013
djtiedemann <dtiedemann91@gmail.com>
2020-08-14 18:05:31 -0400

    Adding in solver for words with consecutive letters

---
+++ Riddles/Riddles/Riddles.Tests/Words/ConsecutiveLetterSolverTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Riddles.Words;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riddles.Tests.Words
+{
+	public class ConsecutiveLetterSolverTest
+	{
+		[Test]
+		public void FindAnswer()
+		{
+			var consecutiveLetterSolver = new ConsecutiveLetterSolver();
+			var actualAnswer = consecutiveLetterSolver.GetWordsWithNConsecutiveLetters(@"C:\Users\djt74_000\Projects\riddles\Riddles\Riddles\Riddles.Tests\Words\enable1dictionary.txt", 4);
+		}
+
+		[TestCase("hijab", 2, true)]
+		[TestCase("hijab", 3, true)]
+		[TestCase("hijab", 4, false)]
+		[TestCase("nope", 3, true)]
+		[TestCase("nope", 4, false)]
+		public void TestWordHasNConsecutiveLetters(string word, int numConsecutiveLetters, bool expectedAnswer)
+		{
+			var consecutiveLetterSolver = new ConsecutiveLetterSolver();
+			var actualAnswer = consecutiveLetterSolver.DoesWordHaveNConsecutiveLetters(word, numConsecutiveLetters);
+			Assert.AreEqual(expectedAnswer, actualAnswer);
+		}
+	}
+}
---
+++ Riddles/Riddles/Riddles/Words/ConsecutiveLetterSolver.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Riddles.Words
+{
+	public class ConsecutiveLetterSolver
+	{
+		public List<string> GetWordsWithNConsecutiveLetters(string dictionaryFileName, int numLetters)
+		{
+			List<string> wordsWithNConsecutiveLetters = new List<string>();
+			using (StreamReader sr = new StreamReader(dictionaryFileName))
+			{
+				while (sr.Peek() >= 0)
+				{
+					var nextWord = sr.ReadLine();
+					if(this.DoesWordHaveNConsecutiveLetters(nextWord, numLetters))
+					{
+						wordsWithNConsecutiveLetters.Add(nextWord);
+					}
+				}
+			}
+			return wordsWithNConsecutiveLetters;
+		}
+
+		public bool DoesWordHaveNConsecutiveLetters(string word, int numLetters)
+		{
+			var currentStreak = 0;
+			var maxStreak = 0;
+			for (int i=0; i<word.Length; i++)
+			{
+				if (i == 0 || word[i] - word[i - 1] != 1)
+				{
+					currentStreak = 1;
+				}
+				else
+				{
+					currentStreak = currentStreak + 1;
+				}
+				if(currentStreak > maxStreak)
+				{
+					maxStreak = currentStreak;
+				}
+			}
+			return maxStreak >= numLetters;
+		}
+	}
+}
