c3ddcb4e25607f245de06511b817c7941a77a5de
djtiedemann <dtiedemann91@gmail.com>
2020-09-18 22:06:17 -0400

    solved centrifuge balancer

---
+++ Riddles/Riddles/Riddles.Tests/Probability/CentrifugeBalancerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Riddles.Probability;
+
+namespace Riddles.Tests.Probability
+{
+	public class CentrifugeBalancerTest
+	{
+		[TestCase(4, 0, 1, Description = "trivially true")]
+		[TestCase(4, 2, 2, Description = "2 valid configurations")]
+		[TestCase(4, 4, 1, Description = "Only 1 configuration, but it's valid")]
+		[TestCase(12, 2, 6, Description = "Only 6 distinct ways")]
+		[TestCase(12, 1, 0, Description = "Impossible")]
+		[TestCase(12, 7, 12, Description = "Only 1 valid configuration, but 12 ways to rotate them")]
+		public void TestFindConfigurationThatBalancesCentrifuge(int numSlots, int numSlotsFilled, int expectedNumBalancedConfigurations)
+		{
+			var centrifugeBalancer = new CentrifugeBalancer();
+			var balancedResults = centrifugeBalancer.FindConfigurationThatBalancesCentrifuge(numSlots, numSlotsFilled);
+			Assert.AreEqual(expectedNumBalancedConfigurations, balancedResults.Count);
+		}
+	}
+}
---
+++ Riddles/Riddles/Riddles/Probability/CentrifugeBalancer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Riddles.Geometry;
+using System.Linq;
+
+namespace Riddles.Probability
+{
+	public class CentrifugeBalancer
+	{
+		public GeometryUtilities geometryUtilities;
+		public double Epsilon = 0.000001;
+
+		public CentrifugeBalancer()
+		{
+			geometryUtilities = new GeometryUtilities();
+		}
+
+		public List<List<Point>> FindConfigurationThatBalancesCentrifuge(int numSlots, int numSlotsFilled)
+		{
+			if(numSlotsFilled == 0)
+			{
+				return new List<List<Point>> { new List<Point>() };
+			}
+			var circle = new Circle { Center = new Point { X = 0, Y = 0 }, Radius = 1 };
+			var positionsAroundCircle = circle.GenerateNPointsEvenlyAroundCircle(numSlots, 0).ToArray();
+			var successfulPositions = new List<List<Point>>();
+
+			var numConfigurations = Math.Pow(2, numSlots);
+			var configurations = Enumerable.Range(0, (int)numConfigurations)
+				.Select(id => Convert.ToString(id, 2).PadLeft(numSlots, '0').ToCharArray().Select(a => a - '0').ToArray()).ToList();
+			var configurationsContainingCorrectNumSlotsFilled = configurations.Where(c => c.Sum() == numSlotsFilled).ToList();
+			foreach(var configuration in configurationsContainingCorrectNumSlotsFilled)
+			{
+				var filledPositionsInCentrifuge = positionsAroundCircle.Where((point, i) => configuration[i] == 1).ToList();
+				var centroid = geometryUtilities.CalculateCenterOfMass(filledPositionsInCentrifuge);
+				if(Math.Abs(centroid.X) < this.Epsilon && Math.Abs(centroid.Y) < this.Epsilon)
+				{
+					successfulPositions.Add(filledPositionsInCentrifuge);
+				}
+			}
+			return successfulPositions;
+
+		}
+	}
+}
