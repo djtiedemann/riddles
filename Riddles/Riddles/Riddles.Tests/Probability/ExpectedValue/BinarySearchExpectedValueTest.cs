fbed70a970b50ae2e0a311a6a9f425ec40a85c57
djtiedemann <dtiedemann91@gmail.com>
2020-09-18 22:53:21 -0400

    Calculating expected value of guesses in binary search

---
+++ Riddles/Riddles/Riddles.Tests/Probability/ExpectedValue/BinarySearchExpectedValueTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Riddles.Probability.ExpectedValue;
+
+namespace Riddles.Tests.Probability.ExpectedValue
+{
+	public class BinarySearchExpectedValueTest
+	{
+		public double Epsilon = 0.000001;
+
+		[TestCase(1, 1)]
+		[TestCase(2, 1.5)]
+		[TestCase(3, 1.666667)]
+		[TestCase(4, 2)]
+		[TestCase(5, 2.2)]
+		[TestCase(6, 2.33333333)]
+		[TestCase(7, 2.42857143)]
+		[TestCase(8, 2.625)]
+		[TestCase(267_751, 17.041956892784714)]
+		public void TestCalculateNumExpectedGuesses(int numOptions, double expectedExpectedValue)
+		{
+			var binarySearchExpectedValueSolver = new BinarySearchExpectedValue();
+			var expectedValue = binarySearchExpectedValueSolver.CalculateNumExpectedGuesses(numOptions);
+			Assert.LessOrEqual(Math.Abs(expectedExpectedValue - expectedValue), this.Epsilon);
+		}
+	}
+}
---
+++ Riddles/Riddles/Riddles/Probability/ExpectedValue/BinarySearchExpectedValue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riddles.Probability.ExpectedValue
+{
+	public class BinarySearchExpectedValue
+	{
+		public BinarySearchExpectedValue()
+		{
+
+		}
+
+		public double CalculateNumExpectedGuesses(int numOptions)
+		{
+			var worstCaseNumGuesses = (int)Math.Ceiling(Math.Log2(numOptions + 1));
+			var expectedValueRunningTotal = 0.0;
+			var numOptionsRemaining = numOptions;
+			for (int guessNum = 1; guessNum <= worstCaseNumGuesses; guessNum++)
+			{
+				var numberOfChoicesCoveredByExactlyNGuesses = (int)Math.Min(Math.Pow(2, guessNum - 1), numOptionsRemaining);
+				expectedValueRunningTotal += numberOfChoicesCoveredByExactlyNGuesses * guessNum;
+				numOptionsRemaining -= numberOfChoicesCoveredByExactlyNGuesses;
+			}
+			return expectedValueRunningTotal / (double) numOptions;
+		}
+	}
+}
