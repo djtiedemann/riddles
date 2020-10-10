166cff2fe2c6c093384bfde86379bb32d1d85d8b
djtiedemann <dtiedemann91@gmail.com>
2020-10-10 09:04:27 -0400

    Finishing up test

---
+++ Riddles/Riddles/Riddles.Tests/Probability/ChocolateSolverTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Riddles.Probability;
+
+namespace Riddles.Tests.Probability
+{
+	public class ChocolateSolverTest
+	{
+		private Dictionary<int, Dictionary<ChocolateType, int>> initialChocolateCounts = new Dictionary<int, Dictionary<ChocolateType, int>>
+		{
+			{ 1, new Dictionary<ChocolateType, int> { { ChocolateType.Milk, 2}, { ChocolateType.Dark, 1} } },
+			{ 2, new Dictionary<ChocolateType, int> { { ChocolateType.Milk, 2}, { ChocolateType.Dark, 2} } },
+			{ 3, new Dictionary<ChocolateType, int> { { ChocolateType.Milk, 3}, { ChocolateType.Dark, 1} } },
+			{ 4, new Dictionary<ChocolateType, int> { { ChocolateType.Milk, 4}, { ChocolateType.Dark, 1} } },
+			{ 5, new Dictionary<ChocolateType, int> { { ChocolateType.Milk, 3}, { ChocolateType.Dark, 2} } },
+			{ 6, new Dictionary<ChocolateType, int> { { ChocolateType.Milk, 3}, { ChocolateType.Dark, 3} } },
+			{ 7, new Dictionary<ChocolateType, int> { { ChocolateType.Milk, 2}, { ChocolateType.Dark, 8} } },
+			{ 8, new Dictionary<ChocolateType, int> { { ChocolateType.Milk, 1}, { ChocolateType.Dark, 1} } },
+			{ 9, new Dictionary<ChocolateType, int> { { ChocolateType.Milk, 2}, { ChocolateType.Dark, 1} } },
+			{ 10, new Dictionary<ChocolateType, int> { { ChocolateType.Milk, 2}, { ChocolateType.Dark, 1} } },
+			{ 11, new Dictionary<ChocolateType, int> { { ChocolateType.Milk, 2}, { ChocolateType.Dark, 2} } },
+		};
+
+		private Dictionary<int, Dictionary<ChocolateType, double>> expectedProbabilitiesForEachChocolateType
+			= new Dictionary<int, Dictionary<ChocolateType, double>>
+		{
+			{ 1, new Dictionary<ChocolateType, double>{ { ChocolateType.Milk, 0.5}, { ChocolateType.Dark, 0.5} } },
+			{ 2, new Dictionary<ChocolateType, double>{ { ChocolateType.Milk, 0.5}, { ChocolateType.Dark, 0.5} } },
+			{ 3, new Dictionary<ChocolateType, double>{ { ChocolateType.Milk, 0.5}, { ChocolateType.Dark, 0.5} } },
+			{ 4, new Dictionary<ChocolateType, double>{ { ChocolateType.Milk, 0.5}, { ChocolateType.Dark, 0.5} } },
+			{ 5, new Dictionary<ChocolateType, double>{ { ChocolateType.Milk, 0.5}, { ChocolateType.Dark, 0.5} } },
+			{ 6, new Dictionary<ChocolateType, double>{ { ChocolateType.Milk, 0.5}, { ChocolateType.Dark, 0.5} } },
+			{ 7, new Dictionary<ChocolateType, double>{ { ChocolateType.Milk, 0.5}, { ChocolateType.Dark, 0.5} } },
+			{ 8, new Dictionary<ChocolateType, double>{ { ChocolateType.Milk, 1.0/4.0}, { ChocolateType.Dark, 3.0/4.0} } },
+			{ 9, new Dictionary<ChocolateType, double>{ { ChocolateType.Milk, 1.0/3.0}, { ChocolateType.Dark, 2.0/3.0} } },
+			{ 10, new Dictionary<ChocolateType, double>{ { ChocolateType.Milk, 2.0/3.0}, { ChocolateType.Dark, 1.0/3.0} } },
+			{ 11, new Dictionary<ChocolateType, double>{ { ChocolateType.Milk, 5.0/12.0}, { ChocolateType.Dark, 7.0/12.0} } },
+		};
+
+		[TestCase(1, null)]
+		[TestCase(2, null)]
+		[TestCase(3, null)]
+		[TestCase(4, null)]
+		[TestCase(5, null)]
+		[TestCase(6, null)]
+		[TestCase(7, null)]
+		[TestCase(8, ChocolateType.Milk)]
+		[TestCase(9, ChocolateType.Milk)]
+		[TestCase(10, ChocolateType.Dark)]
+		[TestCase(11, ChocolateType.Milk)]
+
+		public void TestGetOddsOfEachChocolateTypeBeingLastChocolateDrawn(int testCaseId, ChocolateType lastChocolateDrawn)
+		{
+			double epsilon = 0.0001;
+			var initialChocolateCounts = this.initialChocolateCounts[testCaseId];
+			var chocolateSolver = new ChocolateSolver();
+			var actualProbabilitiesForEachChocolateType = chocolateSolver.GetOddsOfEachChocolateTypeBeingLastChocolateDrawn(
+				initialChocolateCounts, lastChocolateDrawn);
+			var expectedProbabilitiesForEachChocolateType = this.expectedProbabilitiesForEachChocolateType[testCaseId];
+			Assert.AreEqual(actualProbabilitiesForEachChocolateType.Keys.Count, expectedProbabilitiesForEachChocolateType.Keys.Count);
+			foreach(var chocolateType in actualProbabilitiesForEachChocolateType.Keys)
+			{
+				Assert.IsTrue(expectedProbabilitiesForEachChocolateType.ContainsKey(chocolateType));
+				Assert.IsTrue(actualProbabilitiesForEachChocolateType.ContainsKey(chocolateType));
+				Assert.LessOrEqual(Math.Abs(expectedProbabilitiesForEachChocolateType[chocolateType] 
+					- actualProbabilitiesForEachChocolateType[chocolateType]), epsilon);
+			}
+		}
+	}
+}
---
+++ Riddles/Riddles/Riddles/Probability/ChocolateSolver.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Riddles.Probability
+{
+	public enum ChocolateType { 
+		Dark = 1,
+		Milk = 2
+	}
+
+	public class ChocolateState : CalculationState<ChocolateType>
+	{
+		private Dictionary<ChocolateType, int> numChocolatesOfEachTypeRemaining;
+		private ChocolateType? lastChocolateTypeDrawn;
+		public ChocolateState(Dictionary<ChocolateType, int> numChocolatesOfEachTypeRemaining, ChocolateType? lastChocolateTypeDrawn)
+		{
+			this.numChocolatesOfEachTypeRemaining = numChocolatesOfEachTypeRemaining;
+			this.lastChocolateTypeDrawn = lastChocolateTypeDrawn;
+		}
+
+		public string GetMemoizationKey()
+		{
+			var memoizationKey = this.IsTerminalState() 
+				? "" 
+				: $"{(lastChocolateTypeDrawn.HasValue ? $"{lastChocolateTypeDrawn}" : "None")}:";
+			foreach (var chocolateType in numChocolatesOfEachTypeRemaining.Keys.OrderBy(k => k))
+			{
+				memoizationKey += $"{chocolateType}:{numChocolatesOfEachTypeRemaining[chocolateType]},";
+			}
+			return memoizationKey;
+		}
+
+		public bool IsTerminalState()
+		{
+			return numChocolatesOfEachTypeRemaining.Values.Where(v => v > 0).Count() <= 1;
+		}
+
+		public Dictionary<ChocolateType, double> GetProbabilitiesFromTerminalState()
+		{
+			if (!this.IsTerminalState())
+			{
+				throw new Exception($"{nameof(ChocolateState.GetProbabilitiesFromTerminalState)} shouldn't be called outside of a terminal state");
+			}
+
+			return numChocolatesOfEachTypeRemaining.Keys.Select(k => new
+			{
+				ChocolateType = k,
+				Probability = numChocolatesOfEachTypeRemaining[k] > 0 ? 1.0 : 0.0
+			}).ToDictionary(c => c.ChocolateType, c => c.Probability);
+		}
+
+		public List<ProbabilityOfEachObjectBeingDrawnGivenState<ChocolateType>> GetProbabilityOfAllNewStatesFromCurrentState()
+		{
+			var numChocolatesRemaining = (double)numChocolatesOfEachTypeRemaining.Values.Sum();
+			var numChocolatesRemainingOfTypeLastDrawn = lastChocolateTypeDrawn.HasValue 
+				? (double)numChocolatesOfEachTypeRemaining[lastChocolateTypeDrawn.Value]
+				: 0;
+			var oddsOfSameChocolateTypeBeingDrawnAgain = numChocolatesRemainingOfTypeLastDrawn / numChocolatesRemaining;
+			var oddsOfNewChocolateTypeBeingDrawn = 1 - oddsOfSameChocolateTypeBeingDrawnAgain;
+
+			List<ProbabilityOfEachObjectBeingDrawnGivenState<ChocolateType>> newStates
+					= new List<ProbabilityOfEachObjectBeingDrawnGivenState<ChocolateType>>();
+		
+			foreach(var chocolateType in numChocolatesOfEachTypeRemaining.Keys)
+			{
+				var probabilityOfThisChocolateBeingDrawn = chocolateType == lastChocolateTypeDrawn
+					? oddsOfSameChocolateTypeBeingDrawnAgain + oddsOfNewChocolateTypeBeingDrawn * oddsOfSameChocolateTypeBeingDrawnAgain
+					: oddsOfNewChocolateTypeBeingDrawn * (double)numChocolatesOfEachTypeRemaining[chocolateType] / numChocolatesRemaining;
+				var nextState = numChocolatesOfEachTypeRemaining.Keys.Select(k => new
+				{
+					ChocolateType = k,
+					NumChocolatesRemaining = k == chocolateType ? numChocolatesOfEachTypeRemaining[k] - 1 : numChocolatesOfEachTypeRemaining[k]
+				}).ToDictionary(k => k.ChocolateType, k => k.NumChocolatesRemaining);
+				newStates.Add(new ProbabilityOfEachObjectBeingDrawnGivenState<ChocolateType>
+				{
+					Probability = probabilityOfThisChocolateBeingDrawn,
+					CalculationState = new ChocolateState(nextState, chocolateType)
+				});
+			}
+			return newStates;
+		}
+	}
+
+	public class ChocolateSolver
+	{
+		public OddsOfCertainTypeBeingLastObjectDrawn oddsOfCertainTypeBeingLastObjectDrawn;
+		public ChocolateSolver()
+		{
+			this.oddsOfCertainTypeBeingLastObjectDrawn = new OddsOfCertainTypeBeingLastObjectDrawn();
+		}
+
+		public Dictionary<ChocolateType, double> GetOddsOfEachChocolateTypeBeingLastChocolateDrawn(
+			Dictionary<ChocolateType, int> initialChocolatesForEachType, ChocolateType? lastChocolateType)
+		{
+			var initialState = new ChocolateState(initialChocolatesForEachType, lastChocolateType);
+			return this.oddsOfCertainTypeBeingLastObjectDrawn.GetOddsCertainTypeIsLastTypeDrawn<ChocolateType, ChocolateState>(initialState);
+		}
+	}
+}
