9870dd125adf6afaf5b88f8cfc91aaa462e48ef2
djtiedemann <dtiedemann91@gmail.com>
2021-01-22 22:42:16 -0500

    Added in code to compute how likely you were to win if up at halftime

---
+++ Riddles/Riddles/Riddles.Tests/Probability/SkiSlopeSolverTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Riddles.Probability;
+using NUnit.Framework;
+
+namespace Riddles.Tests.Probability
+{
+	public class SkiSlopeSolverTest
+	{
+		double epsilon = 0.0001;
+
+		[TestCase(1, 1.0)]
+		[TestCase(2, 0.75)]
+		[TestCase(3, 0.5833333)]
+		[TestCase(4, 0.46875)]
+		[TestCase(10, 0.19980468)]
+		[TestCase(30, 0.066666)]
+		public void TestGetProbabilityOfVictory(int numSkiers, double expectedProbability)
+		{
+			var skiSlopeSolver = new SkiSlopeSolver();
+			var actualProbability = skiSlopeSolver.GetProbabilityOfVictory(numSkiers);
+			Assert.LessOrEqual(Math.Abs(expectedProbability - actualProbability), epsilon);
+		}
+	}
+}
---
+++ Riddles/Riddles/Riddles/Probability/SkiSlopeSolver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riddles.Probability
+{
+	public class SkiSlopeSolver
+	{
+		public SkiSlopeSolver()
+		{
+
+		}
+
+		// https://fivethirtyeight.com/features/can-you-skillfully-ski-the-slopes/
+		// You are racing against n opponents. Your times between 2 runs will be summed and whoever has the faster overall time wins
+		// You are in first place after the first run. What are the odds that you win.
+		//
+		// You win against an opponent by either
+		// 1) winning against them in the second round
+		// 2) losing against them in the second round, but losing by a smaller margin
+		//
+		// You have an equal chance of finishing in each of the n positions in the second round
+		// If you finish in position i, then there were i-1 opponents who beat you in that round.
+		// For each opponent who beat you, there's a 50% chance that the time gap was smaller than your victory in the first round
+		// That's because each run is independent of each other, and there's an equal probability that the difference will be greater in the first or
+		// second run. 
+		//
+		// You need to beat every opponent to claim victory, so if you finish in position i, your odds of winning are (1/2)^(i-1)
+		//
+		// So the overall odds of victory are sum from i=1 to n of (1/n)*(1/2)^(i-1)
+		public double GetProbabilityOfVictory(int numSkiers)
+		{
+			var totalProbability = 0.0;
+			for(int i=1; i<=numSkiers; i++)
+			{
+				double probabilityOfWinningInPositionI = Math.Pow(0.5, i - 1);
+				totalProbability += (1.0 / (double)numSkiers) * probabilityOfWinningInPositionI;
+			}
+			return totalProbability;
+		}
+	}
+}
