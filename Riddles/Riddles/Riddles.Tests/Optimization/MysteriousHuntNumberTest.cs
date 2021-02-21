fb3f20bc83dd2b1a9d2db4936a60493a8a4a93f1
djtiedemann <dtiedemann91@gmail.com>
2021-02-21 15:40:52 -0500

    Added in another mysterious number hunt test case

--- Riddles/Riddles/Riddles.Tests/Optimization/MysteriousHuntNumberTest.cs
+++ Riddles/Riddles/Riddles.Tests/Optimization/MysteriousHuntNumberTest.cs
@@ -13,7 +13,8 @@ namespace Riddles.Tests.Optimization
 			{ 1, new int[,] { { 3, 9, 2 }, {4, 5, 3 } }},
 			{ 2, new int[,]{ {7, 7, 6 }, { 9, 8, 3}, { 9, 5, 3 }, { 7, 2, 7 }, { 8, 2, 7 }, { 7, 4, 3 }, { 5, 7, 7 }, { 8, 5, 1 } } },
 			{ 3, new int[,] { { 3, 9, 5 }, { 5, 9, 1 }, { 8, 1, 8 }, { 5, 7, 8 }, { 5, 7, 2 }, } },
-			{ 4, new int[,] { { 7, 6, 5}, { 9, 8, 2}, { 3, 9, 2}, { 5, 9, 3}, { 1, 4, 1}, { 7, 1, 7 } } }
+			{ 4, new int[,] { { 7, 6, 5}, { 9, 8, 2}, { 3, 9, 2}, { 5, 9, 3}, { 1, 4, 1}, { 7, 1, 7 } } },
+			{ 5, new int[,] { { 7, 8, 5 }, { 3, 8, 7 }, { 9, 6, 3 }, { 9, 8, 5 }, { 3, 5, 4 }, { 4, 8, 8 }, { 9, 2, 7 } } }
 		};
 
 		[TestCase(1, new int[] { 12, 45, 6 }, new int[] { 54, 60 }, true)]
@@ -33,6 +34,7 @@ namespace Riddles.Tests.Optimization
 		[TestCase(2, new int[] { 8_890_560, 156_800, 55_566 }, new int[] { 294, 216, 135, 98, 112, 84, 245, 40})]
 		[TestCase(3, new int[] { 3_000, 3_969, 640}, new int[] { 135, 45, 64, 280, 70})]
 		[TestCase(4, new int[] { 6_615, 15_552, 420 }, new int[] { 210, 144, 54, 135, 4, 49 })]
+		[TestCase(5, new int[] { 183_708, 245_760, 117_600 }, new int[] { 280, 168, 162, 360, 60, 256, 126})]
 		public void TestFindSolution(int solutionId, int[] bottomRowSolutions, int[] rightSideSolutions)
 		{
 			var mysteriousHuntNumberSolver = new MysteriousNumberHunt();
