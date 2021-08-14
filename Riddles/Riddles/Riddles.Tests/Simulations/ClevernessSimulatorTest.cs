de2506b2cb45627b96d2558f2d91ed8d0a31d0ed
djtiedemann <dtiedemann91@gmail.com>
2021-08-14 12:23:19 -0400

    Add in simulation to verify solution for cleverness problem

---
+++ Riddles/Riddles/Riddles.Tests/Simulations/ClevernessSimulatorTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Riddles.Tests.Simulations
+{
+	public class ClevernessSimulatorTest
+	{
+		//[TestCase(0.9, 1.0, 100_000)]
+		public void TestSimulation(double clevernessLowerBound, double clevernessUpperBound, int numSimulations) {
+			double runningSum = 0;
+			for (int i = 0; i < numSimulations; i++) {
+				var randomCleverness = clevernessLowerBound + (clevernessUpperBound - clevernessLowerBound) * (new Random()).NextDouble();
+				runningSum += Math.Pow(randomCleverness, 9);
+			}
+			var result = runningSum / (double)numSimulations;
+		}
+	}
+}
