24fd7d42d80a4dffb18b26becd2c90278bbb0d39
djtiedemann <dtiedemann91@gmail.com>
2021-08-15 23:10:54 -0400

    Added DiceRerollOptimizer and Tests - V1

---
+++ Riddles/Riddles/Riddles.Tests/Optimization/DiceRerollOptimizerTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Riddles.Optimization;
+using NUnit.Framework;
+
+namespace Riddles.Tests.Optimization
+{
+	public class DiceRerollOptimizerTest
+	{
+		double epsilon = 0.000001;
+
+		[TestCase(1, 6, 3.5)]
+		[TestCase(1, 8, 4.5)]
+		[TestCase(2, 6, 4.11805556)]
+		public void TestGetExpectedValueOfRerollingDice(int numDice, int numSides, double expectedExpectedValue) {
+			var diceRerollOptimizer = new DiceRerollOptimizer();
+			var actualExpectedValue = diceRerollOptimizer.GetExpectedValueOfRerollingDice(numDice, numSides) / numDice;
+			Assert.LessOrEqual(Math.Abs(actualExpectedValue - expectedExpectedValue), epsilon);
+		}
+
+		[TestCase(new int[] { 4, 5, 6}, 15)]
+		[TestCase(new int[] { 6, 4, 5 }, 15)]
+		[TestCase(new int[] { 3, 6, 5}, 14.5)]
+		[TestCase(new int[] { 3, 5, 6 }, 14.5)]
+		[TestCase(new int[] { 5, 3, 6 }, 14.5)]
+		[TestCase(new int[] { 5, 6, 3 }, 14.5)]
+		[TestCase(new int[] { 6, 3, 5 }, 14.5)]
+		[TestCase(new int[] { 6, 5, 3 }, 14.5)]
+		[TestCase(new int[] { 1, 6, 5 }, 14.5)]
+		[TestCase(new int[] { 1, 5, 6 }, 14.5)]
+		[TestCase(new int[] { 5, 1, 6 }, 14.5)]
+		[TestCase(new int[] { 5, 6, 1 }, 14.5)]
+		[TestCase(new int[] { 6, 1, 5 }, 14.5)]
+		[TestCase(new int[] { 6, 5, 1 }, 14.5)]
+		[TestCase(new int[] { 1, 6, 4 }, 14.23611112)]
+		[TestCase(new int[] { 1, 4, 6 }, 14.23611112)]
+		[TestCase(new int[] { 4, 1, 6 }, 14.23611112)]
+		[TestCase(new int[] { 4, 6, 1 }, 14.23611112)]
+		[TestCase(new int[] { 6, 1, 4 }, 14.23611112)]
+		[TestCase(new int[] { 6, 4, 1 }, 14.23611112)]
+		public void TestCalculateExpectedValueOfCurrentDiceRoll(int[] diceRoll, double expectedExpectedValue)
+		{
+			var diceRerollOptimizer = new DiceRerollOptimizer();
+			var memo = new Dictionary<int, double> { { 1, 3.5 }, { 2, 4.11805556 } };
+			var actualExpectedValue = diceRerollOptimizer.CalculateExpectedValueOfCurrentDiceRoll(diceRoll.ToList(), diceRoll.Length, memo);
+			Assert.LessOrEqual(Math.Abs(actualExpectedValue - expectedExpectedValue), epsilon);
+		}
+	}
+}
---
+++ Riddles/Riddles/Riddles/Optimization/DiceRerollOptimizer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Riddles.Combinatorics.SetGeneration;
+using System.Linq;
+
+namespace Riddles.Optimization
+{
+	// https://fivethirtyeight.com/features/are-you-clever-enough/
+	public class DiceRerollOptimizer
+	{
+		private OutcomeGenerator _outcomeGenerator;
+		public DiceRerollOptimizer() {
+			_outcomeGenerator = new OutcomeGenerator();
+		}
+
+		/// <summary>
+		/// You want to optimize your score when rolling N dice. You roll all of the dice, and after the roll, you must freeze at least 1 dice.
+		/// You may reroll any number of the other dice that you choose. What is the maximum expected value you can acheive in this situation.
+		/// </summary>
+		public double GetExpectedValueOfRerollingDice(int numDice, int numSides) { 
+			if(numDice <= 0 || numSides <= 0)
+			{
+				return 0;
+			}
+
+			Dictionary<int, double> numDiceToExpectedValuePerDieDictionary = new Dictionary<int, double>();
+			for (int i = 1; i <= numDice; i++) {
+				var outcomes = this._outcomeGenerator.GenerateAllOutcomes(i, numSides, '1');
+				var diceRolls = this.TransformOutcomesToDiceRolls(outcomes);
+				numDiceToExpectedValuePerDieDictionary[i] = this.GetExpectedValueInternal(diceRolls, i, numDiceToExpectedValuePerDieDictionary);
+			}
+			return numDiceToExpectedValuePerDieDictionary[numDice] * numDice;
+		}
+
+		private double GetExpectedValueInternal(List<List<int>> diceRolls, int numDice, Dictionary<int, double> memo) {
+			double rollingSumOfExpectedValues = 0;
+			int numDiceRolls = diceRolls.Count;
+			foreach (var diceRoll in diceRolls) {
+				var expectedValue = this.CalculateExpectedValueOfCurrentDiceRoll(diceRoll, numDice, memo);
+				rollingSumOfExpectedValues += expectedValue;
+			}
+
+			return (rollingSumOfExpectedValues / numDiceRolls) / numDice;
+		}
+
+		public double CalculateExpectedValueOfCurrentDiceRoll(List<int> diceRoll, int numDice, Dictionary<int, double> memo)
+		{
+			var orderedDiceRoll = diceRoll.OrderByDescending(d => d).ToList();
+			double expectedValue = orderedDiceRoll[0];
+			for (int i = 1; i < orderedDiceRoll.Count; i++)
+			{
+				int numDiceRemaining = orderedDiceRoll.Count - i;
+				if (orderedDiceRoll[i] > memo[numDiceRemaining])
+				{
+					expectedValue += orderedDiceRoll[i];
+				}
+				else
+				{
+					expectedValue += memo[numDiceRemaining] * numDiceRemaining;
+					break;
+				}
+			}
+			return expectedValue;
+		}
+
+		private List<List<int>> TransformOutcomesToDiceRolls(List<string> outcomes) {
+			return outcomes.Select(outcome => outcome.ToCharArray().Select(roll => (roll - '1') + 1).ToList()).ToList();
+		}
+	}
+}
