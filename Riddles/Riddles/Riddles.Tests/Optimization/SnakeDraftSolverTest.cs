15c942d4edb7fdbe64dd24ba643f4095d4be4c5f
djtiedemann <dtiedemann91@gmail.com>
2021-09-04 22:34:00 -0400

    Solved snake draft

---
+++ Riddles/Riddles/Riddles.Tests/Optimization/SnakeDraftSolverTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Riddles.Optimization;
+
+namespace Riddles.Tests.Optimization
+{
+	public enum Position
+	{
+		Quarterback = 1,
+		RunningBack = 2,
+		WideReceiver = 3
+	}
+
+	public class SnakeDraftSolverTest
+	{
+		private Dictionary<int, Dictionary<Position, List<Player>>> availablePlayersByTestCase
+			= new Dictionary<int, Dictionary<Position, List<Player>>>
+			{
+				{1,  new Dictionary<Position, List<Player>> {
+					{
+						Position.Quarterback,
+						new List<Player> {
+							new Player { Name = "Patrick Mahomes", PlayerId = 1, ExpectedPoints = 400 },
+							new Player { Name = "Josh Allen", PlayerId = 2, ExpectedPoints = 350 },
+						}
+					},
+					{
+						Position.RunningBack,
+						new List<Player> {
+							new Player { Name = "Christian McCaffrey", PlayerId = 4, ExpectedPoints = 300 },
+							new Player { Name = "Dalvin Cook", PlayerId = 5, ExpectedPoints = 225 },
+						}
+					},
+					}
+				},
+				{2,  new Dictionary<Position, List<Player>> {
+					{
+						Position.Quarterback,
+						new List<Player> {
+							new Player { Name = "Patrick Mahomes", PlayerId = 1, ExpectedPoints = 400 },
+							new Player { Name = "Josh Allen", PlayerId = 2, ExpectedPoints = 350 },
+							new Player { Name = "Kyler Murray", PlayerId = 3, ExpectedPoints = 300 },
+						}
+					},
+					{
+						Position.RunningBack,
+						new List<Player> {
+							new Player { Name = "Christian McCaffrey", PlayerId = 4, ExpectedPoints = 300 },
+							new Player { Name = "Dalvin Cook", PlayerId = 5, ExpectedPoints = 225 },
+							new Player { Name = "Derrick Henry", PlayerId = 6, ExpectedPoints = 200 },
+						}
+					},
+					{
+						Position.WideReceiver,
+						new List<Player> {
+							new Player { Name = "Davante Adams", PlayerId = 7, ExpectedPoints = 250 },
+							new Player { Name = "Tyreek Hill", PlayerId = 8, ExpectedPoints = 225 },
+							new Player { Name = "Stephon Diggs", PlayerId = 9, ExpectedPoints = 175 },
+						}
+					},
+				}
+			}
+		};
+		private Dictionary<int, Dictionary<Position, int>> teamNeedsByTestCaseId = new Dictionary<int, Dictionary<Position, int>> {
+			{1,  new Dictionary<Position, int> {
+					{ Position.Quarterback, 1}, { Position.RunningBack, 1} } },
+			{2,  new Dictionary<Position, int> {
+					{ Position.Quarterback, 1}, { Position.RunningBack, 1}, { Position.WideReceiver, 1} } }
+		};
+
+		[TestCase(1, 1, 2, 0)]
+		[TestCase(2, 2, 3, 1)]
+		public void TestFindOptimalDraftPosition(int playersAvailableTestCaseId, int teamNeedsTestCaseId, int numTeams, int expectedOptimalTeam)
+		{
+			var snakeDraftSolver = new SnakeDraftSolver();
+			var availablePlayers = availablePlayersByTestCase[playersAvailableTestCaseId];
+			var needsByTeam = this.GetTeamNeedsByTeam(teamNeedsByTestCaseId[teamNeedsTestCaseId], numTeams);
+			var optimalPosition = snakeDraftSolver.FindOptimalDraftPosition(numTeams, needsByTeam, availablePlayers);
+			Assert.AreEqual(expectedOptimalTeam, optimalPosition);
+		}
+
+		private Dictionary<int, Dictionary<Position, int>> GetTeamNeedsByTeam(Dictionary<Position, int> teamNeeds, int numTeams) {
+			var teamNeedsByTeam = new Dictionary<int, Dictionary<Position, int>>();
+			for (int teamId = 0; teamId < numTeams; teamId++) {
+				var teamNeedsCopy = new Dictionary<Position, int>();
+				foreach(var position in teamNeeds.Keys)
+				{
+					teamNeedsCopy[position] = teamNeeds[position];
+				}
+				teamNeedsByTeam[teamId] = teamNeedsCopy;
+			}
+			return teamNeedsByTeam;
+		}
+	}
+}
---
+++ Riddles/Riddles/Riddles/Optimization/SnakeDraftSolver.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Riddles.Optimization
+{
+	public class Player {
+		public int PlayerId { get; set; }
+		public string Name { get; set; }
+		public int ExpectedPoints { get; set; }
+	}
+
+	public class SnakeDraftSolver
+	{
+		public SnakeDraftSolver()
+		{
+
+		}
+
+		public int FindOptimalDraftPosition<TPositionEnum>(
+			int numTeams,
+			Dictionary<int, Dictionary<TPositionEnum, int>> positionsNeededByTeam,
+			Dictionary<TPositionEnum, List<Player>> playersAvailableByPosition) where TPositionEnum : System.Enum
+		{
+			var rostersByTeam = new Dictionary<int, List<Player>> { };
+			var optimalDraft = this.FindOptimalDraftInternal(
+				numTeams,
+				0,
+				positionsNeededByTeam,
+				playersAvailableByPosition,
+				Enumerable.Range(0, numTeams).ToDictionary(t => t, t => new List<Player>()),
+				new Dictionary<string, Dictionary<int, List<Player>>> { },
+				new Dictionary<string, string> { }
+			);
+			var scores = optimalDraft.Keys
+				.Select(t => new { teamId = t, score = optimalDraft[t].Sum(p => p.ExpectedPoints) }).ToList();
+			var bestScore = scores.OrderByDescending(s => s.score).First().teamId;
+			return bestScore;
+		}
+
+		private Dictionary<int, List<Player>> FindOptimalDraftInternal<TPositionEnum>(
+			int numTeams,
+			int currentDraftPosition,
+			Dictionary<int, Dictionary<TPositionEnum, int>> positionsNeededByTeam,
+			Dictionary<TPositionEnum, List<Player>> playersAvailableByPosition,
+			Dictionary<int, List<Player>> rostersByTeam,
+			Dictionary<string, Dictionary<int, List<Player>>> memo,
+			Dictionary<string, string> debugMemo) where TPositionEnum : System.Enum
+		{
+			var currentDraftRepresentation = this.GetCurrentDraftStateRepresentation(rostersByTeam);
+			if (memo.ContainsKey(currentDraftRepresentation)) {
+				return memo[currentDraftRepresentation];
+			}	
+			var currentTeam = this.GetCurrentTeamFromDraftPosition(currentDraftPosition, numTeams);
+			var positionsNeeded = positionsNeededByTeam[currentTeam].Keys
+				.Where(k => positionsNeededByTeam[currentTeam][k] > 0).ToList();
+			
+			if (positionsNeeded.Count == 0)
+			{
+				memo[currentDraftRepresentation] = new Dictionary<int, List<Player>>();
+				for(int i=0; i<numTeams; i++)
+				{
+					memo[currentDraftRepresentation][i] = new List<Player>();
+				}
+				debugMemo[currentDraftRepresentation] = "";
+				return memo[currentDraftRepresentation];
+			}
+			var bestPosition = positionsNeeded.First();
+			var bestScore = 0;
+			foreach(var position in positionsNeeded)
+			{
+				var bestPlayer = playersAvailableByPosition[position].OrderByDescending(p => p.ExpectedPoints).First();
+				rostersByTeam[currentTeam].Add(bestPlayer);
+				playersAvailableByPosition[position].Remove(bestPlayer);
+				positionsNeededByTeam[currentTeam][position]--;
+
+				var remainingDraft = this.FindOptimalDraftInternal(
+					numTeams,
+					currentDraftPosition + 1,
+					positionsNeededByTeam,
+					playersAvailableByPosition,
+					rostersByTeam,
+					memo,
+					debugMemo
+				);
+
+				var score = (rostersByTeam[currentTeam]?.Sum(t => t.ExpectedPoints) ?? 0)
+					+ (remainingDraft[currentTeam]?.Sum(t => t.ExpectedPoints) ?? 0);
+				if(score > bestScore)
+				{
+					bestScore = score;
+					bestPosition = position;
+				}
+
+				rostersByTeam[currentTeam].Remove(bestPlayer);
+				playersAvailableByPosition[position].Add(bestPlayer);
+				positionsNeededByTeam[currentTeam][position]++;
+			}
+			var bestPick = playersAvailableByPosition[bestPosition].OrderByDescending(p => p.ExpectedPoints).First();
+			
+			rostersByTeam[currentTeam].Add(bestPick);
+			var nextDraftRepresentation = this.GetCurrentDraftStateRepresentation(rostersByTeam);
+			var optimalDraft = memo[nextDraftRepresentation];
+			var optimalDraftCopy = this.CopyDraft(optimalDraft);
+			optimalDraftCopy[currentTeam].Add(bestPick);
+			memo[currentDraftRepresentation] = optimalDraftCopy;
+			debugMemo[currentDraftRepresentation] = this.GetCurrentDraftStateRepresentation(optimalDraftCopy);
+			rostersByTeam[currentTeam].Remove(bestPick);
+			return optimalDraftCopy;
+		}
+
+		private int GetCurrentTeamFromDraftPosition(int currentDraftPosition, int numTeams) {
+			var mod = (currentDraftPosition % (2 * numTeams));
+			return mod < numTeams
+				? mod
+				: (2 * numTeams) - mod - 1;
+		}
+
+		private string GetCurrentDraftStateRepresentation(Dictionary<int, List<Player>> currentDraft)
+		{
+			var teamInfo = currentDraft.Keys.OrderBy(k => k)
+				.Select(k => $"{k}:{String.Join(",", currentDraft[k].Select(p => p.PlayerId))}");
+			return String.Join("|", teamInfo);
+		}
+
+		private Dictionary<int, List<Player>> CopyDraft(Dictionary<int, List<Player>> draft)
+		{
+			Dictionary<int, List<Player>> copyDraft = new Dictionary<int, List<Player>>();
+			foreach(var team in draft.Keys)
+			{
+				List<Player> players = new List<Player>();
+				foreach(var player in draft[team])
+				{
+					players.Add(new Player
+					{
+						Name = player.Name,
+						PlayerId = player.PlayerId,
+						ExpectedPoints = player.ExpectedPoints
+					});
+				}
+				copyDraft[team] = players;
+			}
+			return copyDraft;
+		}
+	}
+}
