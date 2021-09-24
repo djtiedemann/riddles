0fa22951c2b59422a503d0ca6231ac641b58847c
djtiedemann <dtiedemann91@gmail.com>
2021-09-23 22:59:30 -0400

    Test radius as well

--- Riddles/Riddles/Riddles.Tests/Geometry/PointTest.cs
+++ Riddles/Riddles/Riddles.Tests/Geometry/PointTest.cs
@@ -11,21 +11,21 @@ namespace Riddles.Tests.Geometry
 		double epsilon = 0.000001;
 
 		[TestCase(0, 0, 0, 0)]
-		[TestCase(1, 0, 1, 0)]
-		[TestCase(0.866025404, 0.5, 1, Math.PI / 6)]
-		[TestCase(0.707106781, 0.707106781, 1, Math.PI / 4)]
+		[TestCase(4*1, 0, 4*1, 0)]
+		[TestCase(9*0.866025404, 9*0.5, 9*1, Math.PI / 6)]
+		[TestCase(16*0.707106781, 16*0.707106781, 16*1, Math.PI / 4)]
 		[TestCase(0.5, 0.866025404, 1, Math.PI / 3)]
 		[TestCase(0, 1, 1, Math.PI/2)]
 		[TestCase(-0.5, 0.866025404, 1, 2*Math.PI / 3)]
 		[TestCase(-0.707106781, 0.707106781, 1, 3*Math.PI / 4)]
 		[TestCase(-0.866025404, 0.5, 1, 5*Math.PI / 6)]
-		[TestCase(-1, 0, 1, Math.PI)]
+		[TestCase(4*-1, 0, 4*1, Math.PI)]
 		[TestCase(-0.866025404, -0.5, 1, 7 * Math.PI / 6)]
 		[TestCase(-0.707106781, -0.707106781, 1, 5 * Math.PI / 4)]
 		[TestCase(-0.5, -0.866025404, 1, 4 * Math.PI / 3)]
 		[TestCase(0, -1, 1, 3*Math.PI / 2)]
-		[TestCase(0.5, -0.866025404, 1, 5 * Math.PI / 3)]
-		[TestCase(0.707106781, -0.707106781, 1, 7 * Math.PI / 4)]
+		[TestCase(25*0.5, 25*-0.866025404, 25*1, 5 * Math.PI / 3)]
+		[TestCase(9*0.707106781, 9*-0.707106781, 9*1, 7 * Math.PI / 4)]
 		[TestCase(0.866025404, -0.5, 1, 11 * Math.PI / 6)]
 		public void TestCoordinateConversion(double x, double y, double r, double thetaRadians) {
 			var point1 = new Point(x, y);
