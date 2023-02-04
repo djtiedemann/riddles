781e8d47e76db0f8c2916735d44eed1d4fd96d47
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2023-02-04 17:15:07 -0500

    Add Binomial Theorem Calculator

---
+++ Riddles/Riddles/Riddles.Tests/Combinatorics/BinomialTheoremCalculatorTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Riddles.Combinatorics.Core;
+
+namespace Riddles.Tests.Combinatorics
+{
+    public class BinomialTheoremCalculatorTest
+    {
+        [TestCase(3, -1, 0)]
+        [TestCase(3, 4, 0)]
+        [TestCase(-1, -4, 0)]
+        [TestCase(4, 0, 1)]
+        [TestCase(4, 1, 4)]
+        [TestCase(4, 2, 6)]
+        [TestCase(4, 3, 4)]
+        [TestCase(4, 4, 1)]
+        public void TestCalculateBinomialCoefficient(int n, int k, int expected)
+        {
+            var binomialCalculator = new BinomialTheoremCalculator();
+            var result = binomialCalculator.CalculateBinomialCoefficient(n, k);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Combinatorics/Core/BinomialTheoremCalculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riddles.Combinatorics.Core
+{
+    public class BinomialTheoremCalculator
+    {
+        private bool _useCache = true;
+        private Dictionary<(int, int), int> _cache;
+        private FactorialCalculator _factorialCalculator;
+
+        public BinomialTheoremCalculator(bool useCache = true)
+        {
+            this._factorialCalculator = new FactorialCalculator();
+            this._useCache = useCache;
+            this._cache = new Dictionary<(int, int), int>();
+        }
+
+        public int CalculateBinomialCoefficient(int n, int k)
+        {
+            if(n <= 0 || k <0 || n < k)
+            {
+                return 0;
+            }
+            if(this._useCache && this._cache.ContainsKey((n, k)))
+            {
+                return this._cache[(n, k)];
+            }
+            var result = this._factorialCalculator.Factorial(n)
+                / (this._factorialCalculator.Factorial(k) * 
+                this._factorialCalculator.Factorial(n - k));
+            if (this._useCache)
+            {
+                this._cache[(n, k)] = result;
+            }
+            return result;
+        }
+    }
+}
