c0adc846f87c7997c0166e71af1a44dab5b9d16e
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2023-02-04 13:11:14 -0500

    Added even spaced strategy for BlindLetterOptimizer

---
+++ Riddles/Riddles/Riddles.Tests/Optimization/BlindLetterChallengeOptimizerTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Riddles.Optimization;
+
+namespace Riddles.Tests.Optimization
+{
+    public class BlindLetterChallengeOptimizerTest
+    {
+        private double Epsilon = 0.00000001;
+
+        [TestCase(26, 2, Strategy.EvenSpacing, 0.76)]
+        [TestCase(26, 3, Strategy.EvenSpacing, 0.53410256410256407)]
+        [TestCase(26, 4, Strategy.EvenSpacing, 0.36256967670011148)]
+        [TestCase(26, 5, Strategy.EvenSpacing, 0.24141177662916793)] // - takes 53 seconds, commented out
+        public void TestGetPercentSuccessfulAttempts(int numLetters, int numSlots, Strategy strategy, double expectedPercentSuccessfulPermutations)
+        {
+            var blindLetterChallengeOptimizer = new BlindLetterChallengeOptimizer();
+            var percentSuccessfulPermutations = blindLetterChallengeOptimizer.GetPercentSuccessfulAttempts(numLetters, numSlots, strategy);
+            Assert.LessOrEqual(Math.Abs(expectedPercentSuccessfulPermutations - percentSuccessfulPermutations), Epsilon); 
+        }
+
+        [TestCase(new int[] { 19, 15, 3, 2, 1 }, 26, false)]
+        [TestCase(new int[] { 19, 2, 18, 22, 1 }, 26, false)]
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, 26, true)]
+        [TestCase(new int[] { 5, 4, 3, 2, 1 }, 26, false)]
+        [TestCase(new int[] { 22, 23, 24, 25, 26 }, 26, false)]
+        [TestCase(new int[] { 26, 25, 24, 23, 22 }, 26, true)]
+        [TestCase(new int[] { 13, 2, 6, 25, 4 }, 26, false)]
+        [TestCase(new int[] { 13, 2, 6, 17, 26 }, 26, true)]
+        [TestCase(new int[] { 8, 18, 7, 21, 14 }, 26, true)]
+        public void TestIsSuccessfulAttempt(int[] attempt, int numLetters, bool expectedResult)
+        {
+            var blindLetterChallengeOptimizer = new BlindLetterChallengeOptimizer();
+            var isSuccess = blindLetterChallengeOptimizer.IsSuccessfulAttemptEvenSpacingStrategy(attempt, numLetters);
+            Assert.AreEqual(expectedResult, isSuccess);
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Optimization/BlindLetterChallengeOptimizer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Riddles.Combinatorics.Core.SetGeneration;
+
+namespace Riddles.Optimization
+{
+    public enum Strategy
+    {
+        Optimal = 0,
+        EvenSpacing = 1,
+    }
+    public class BlindLetterChallengeOptimizer
+    {
+        private PermutationWithoutRepetitionGenerator _permutationGenerator;
+        public BlindLetterChallengeOptimizer() { 
+            this._permutationGenerator = new PermutationWithoutRepetitionGenerator();
+        }
+
+        public double GetPercentSuccessfulAttempts(int numLetters, int numSlots, Strategy strategy)
+        {
+            switch (strategy)
+            {
+                case Strategy.Optimal:
+                    return 0.0;
+                case Strategy.EvenSpacing:
+                    return this.GetPercentSuccessfulAttemptsEvenSpacing(numLetters, numSlots);
+            }
+            return 0.0;
+        }
+
+        private double GetPercentSuccessfulAttemptsEvenSpacing(int numLetters, int numSlots)
+        {
+            var permutationState = this._permutationGenerator.GenerateNextPermutation(new PermutationWithoutRepetitionGenerator.PermutationState(null, null, Enumerable.Range(1, numLetters).ToArray(), numSlots));
+            var numPermutations = 0;
+            var numSuccessfulPermutations = 0;
+            while(permutationState.CurrentPermutation != null)
+            {
+                if(this.IsSuccessfulAttemptEvenSpacingStrategy(permutationState.CurrentPermutation.GetPermutation(), numLetters)){
+                    numSuccessfulPermutations++;
+                }
+                numPermutations++;
+                permutationState = this._permutationGenerator.GenerateNextPermutation(permutationState);
+            }
+            return (double)numSuccessfulPermutations/(double)numPermutations;
+        }
+
+        public bool IsSuccessfulAttemptEvenSpacingStrategy(int[] attempt, int numLetters)
+        {
+            int[] result = Enumerable.Range(0, attempt.Length).Select(x => -1).ToArray();
+            foreach(int letter in attempt)
+            {
+                var minInfo = result.Select((x, i) => new { Value = x + 1, Index = i + 1}).Where(x => x.Value <= letter && x.Value != 0).OrderByDescending(x => x.Value).FirstOrDefault() ?? new { Value = 1, Index = 0};
+                var maxInfo = result.Select((x, i) => new { Value = x - 1, Index = i - 1 }).Where(x => x.Value >= letter && x.Value != -2).OrderBy(x => x.Value).FirstOrDefault() ?? new { Value = numLetters, Index = attempt.Length - 1 };
+                if(minInfo.Index >= attempt.Length || maxInfo.Index < 0 || minInfo.Index > maxInfo.Index)
+                {
+                    return false;
+                }
+                var numSlots = (maxInfo.Index - minInfo.Index) + 1;
+                var numPossibleCharacters = (maxInfo.Value - minInfo.Value) + 1;
+                var characterIndex = letter - minInfo.Value;
+                var percentage = (double)characterIndex / (numPossibleCharacters);
+                var index = (int)(percentage * numSlots) + minInfo.Index;
+                result[index] = letter;
+            }
+            return true;
+        }
+    }
+}
