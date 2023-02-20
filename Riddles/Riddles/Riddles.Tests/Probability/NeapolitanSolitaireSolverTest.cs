82f5fa117a6374fcd8e368b866465112d6097a86
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2023-02-19 23:44:28 -0500

    Add in code to solve neapolitan solitaire

--- Riddles/Riddles/Riddles.Tests/Probability/NeapolitanSolitaireSolverTest.cs
+++ Riddles/Riddles/Riddles.Tests/Probability/NeapolitanSolitaireSolverTest.cs
@@ -1,86 +1,88 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using Riddles.Probability;
+using System.Linq;
 
 namespace Riddles.Tests.Probability
 {
     public class NeapolitanSolitaireSolverTest
     {
-        private Dictionary<int, (Dictionary<string, int>, Dictionary<string, List<int>>, double)>
+        private double Epsilon = 0.00000001;
+        private Dictionary<int, (Dictionary<string, int>, Dictionary<string, HashSet<int>>, double)>
             TestCaseDictionary
-            = new Dictionary<int, (Dictionary<string, int>, Dictionary<string, List<int>>, double)> {
+            = new Dictionary<int, (Dictionary<string, int>, Dictionary<string, HashSet<int>>, double)> {
                 {1, (
                     new Dictionary<string, int> { { "1", 1}, {"2", 1 } },
-                    new Dictionary<string, List<int>> { 
-                        { "1", new List<int> { 0 } },
-                        { "2", new List<int> { 1 } } 
+                    new Dictionary<string, HashSet<int>> { 
+                        { "1", new HashSet<int> { 0 } },
+                        { "2", new HashSet<int> { 1 } } 
                     },
                     0.5 )
                 },
                 {2, (
                     new Dictionary<string, int> { { "1", 1}, {"2", 1 } },
-                    new Dictionary<string, List<int>> { 
-                        { "1", new List<int> { 0, 1 } } 
+                    new Dictionary<string, HashSet<int>> { 
+                        { "1", new HashSet<int> { 0, 1 } } 
                     },
                     0.0 )
                 },
                 {3, (
                     new Dictionary<string, int> { { "1", 1}, {"2", 1 } },
-                    new Dictionary<string, List<int>> {
-                        { "1", new List<int> { 0 } },
-                        { "2", new List<int> { 0 } } 
+                    new Dictionary<string, HashSet<int>> {
+                        { "1", new HashSet<int> { 0 } },
+                        { "2", new HashSet<int> { 0 } } 
                     },
                     0.0 )
                 },
                 {4, (
                     new Dictionary<string, int> { { "1", 1}, {"2", 1 } },
-                    new Dictionary<string, List<int>> { { "3", new List<int> { 0, 1} } },
+                    new Dictionary<string, HashSet<int>> { { "3", new HashSet<int> { 0, 1} } },
                     1.0 )
                 },
                 {5, (
                     new Dictionary<string, int> { { "1", 1}, {"2", 1 }, {"3", 1 } },
-                    new Dictionary<string, List<int>> {
-                        { "1", new List<int> { 0 } },
-                        { "2", new List<int> { 1 } },
-                        { "3", new List<int> { 2 } },
+                    new Dictionary<string, HashSet<int>> {
+                        { "1", new HashSet<int> { 0 } },
+                        { "2", new HashSet<int> { 1 } },
+                        { "3", new HashSet<int> { 2 } },
                     },
-                    0.5 )
+                    1.0/3.0 )
                 },
                 {6, (
                     new Dictionary<string, int> { { "1", 1}, {"2", 1 }, {"3", 1 } },
-                    new Dictionary<string, List<int>> { 
-                        { "1", new List<int> { 2 } },
-                        { "3", new List<int> { 2 } }, 
+                    new Dictionary<string, HashSet<int>> { 
+                        { "1", new HashSet<int> { 2 } },
+                        { "3", new HashSet<int> { 2 } }, 
                     },
                     1.0/3.0 )
                 },
                 {7, (
                     new Dictionary<string, int> {
                         { "1", 2}, {"2", 2 }, {"3", 2 }, {"4", 2 }, {"5", 2 }},
-                    new Dictionary<string, List<int>> {
-                        { "1", new List<int> { 1, 3, 5, 7, 9 } }
+                    new Dictionary<string, HashSet<int>> {
+                        { "1", new HashSet<int> { 1, 3, 5, 7, 9 } }
                     },
                     2.0/9.0 )
                 },
                 {8, (
                     new Dictionary<string, int> {
                         { "1", 2}, {"2", 2 }, {"3", 2 }, {"4", 2 }, {"5", 2 }},
-                    new Dictionary<string, List<int>> {
-                        { "1", new List<int> { 0, 1, 2, 3, 4 } }
+                    new Dictionary<string, HashSet<int>> {
+                        { "1", new HashSet<int> { 0, 1, 2, 3, 4 } }
                     },
                     2.0/9.0 )
                 },
                 {9, (
                     new Dictionary<string, int> {
-                        { "1", 4}, {"2", 4 }, {"3", 4 }, {"4", 4 }, {"5", 4 },
-                        { "6", 4}, {"7", 4 }, {"8", 4 }, {"9", 4 }, {"10", 4 }
+                        { "1", 4}, {"2", 4 }, {"3", 4 }, {"4", 28 }
                     },
-                    new Dictionary<string, List<int>> {
-                        { "1", new List<int> { 0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39 } },
-                        { "2", new List<int> { 1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34, 37 } },
-                        { "3", new List<int> { 2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35, 38 } }
+                    new Dictionary<string, HashSet<int>> {
+                        { "1", new HashSet<int> { 0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39 } },
+                        { "2", new HashSet<int> { 1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34, 37 } },
+                        { "3", new HashSet<int> { 2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35, 38 } }
                     },
-                    1.0/3.0 )
+                    0.0083070055718199344 )
                 },
             };
 
@@ -96,7 +98,53 @@ namespace Riddles.Tests.Probability
         // 3 can't be in %3 = 0
         public void TestGetOddsOfSuccess(int testCaseId)
         {
-            // need to calculate expected value for test cases 7 + 8
+            var testCase = TestCaseDictionary[testCaseId];
+            var neapolitanSolitaireSolver = new NeapolitanSolitaireSolver();
+            var oddsOfSuccess = neapolitanSolitaireSolver.CalculateOddsOfSuccess(
+                testCase.Item1, testCase.Item2
+            );
+            Assert.LessOrEqual(Math.Abs(oddsOfSuccess - testCase.Item3), Epsilon);
+        }
+
+        // [TestCase(9)]
+        // correct answer: 0.0083070055718199344
+        // 0.008324
+        // 0.0082895
+        // 0.0082549
+        // 0.0083064
+        // 0.0083057
+        // 0.0082889
+        // 0.0083491
+        public void SimulateGame(int testCaseId)
+        {
+            var testCase = TestCaseDictionary[testCaseId];
+            var numIterations = 10_000_000;
+            var cards =
+                Enumerable.Range(0, 4).Select(c => "1")
+                .Concat(Enumerable.Range(0, 4).Select(c => "2"))
+                .Concat(Enumerable.Range(0, 4).Select(c => "3"))
+                .Concat(Enumerable.Range(0, 28).Select(c => "4")).ToArray();
+            var random = new Random();
+            var restrictions = testCase.Item2;
+            var numSuccesses = 0;
+            for(int iteration=0; iteration<numIterations; iteration++)
+            {
+                cards = cards.OrderBy(x => random.Next()).ToArray();
+                var isSuccess = true;
+                for(int i=0; i<cards.Count(); i++)
+                {
+                    if (restrictions.ContainsKey(cards[i]) && restrictions[cards[i]].Contains(i))
+                    {
+                        isSuccess = false;
+                        break;
+                    }
+                }
+                if(isSuccess)
+                {
+                    numSuccesses++;
+                }
+            }
+            var result = (double)numSuccesses / numIterations;
         }
     }
 }
--- Riddles/Riddles/Riddles/Probability/NeapolitanSolitaireSolver.cs
+++ Riddles/Riddles/Riddles/Probability/NeapolitanSolitaireSolver.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using Riddles.Caching;
+using static Riddles.MarkovChains.BottlesOfBeerSolver;
 
 namespace Riddles.Probability
 {
@@ -12,18 +14,92 @@ namespace Riddles.Probability
         public NeapolitanSolitaireSolver() { 
             
         }
-        public double CalculateOddsOfSuccess()
+        public double CalculateOddsOfSuccess(
+            Dictionary<string, int> cardCount, 
+            Dictionary<string, HashSet<int>> restrictions)
         {
-            return 0;
+            var numCardsRemaining = cardCount.Keys.Sum(x => cardCount[x]);
+            var solitaireState = new SolitaireState(cardCount, numCardsRemaining);
+            var cache = new Dictionary<SolitaireState, double>();
+            return this.CalculateOddsOfSuccess(solitaireState, restrictions, cache, 0);
+        }
+
+        private double CalculateOddsOfSuccess(
+            SolitaireState state,
+            Dictionary<string, HashSet<int>> restrictions,
+            Dictionary<SolitaireState, double> cache,
+            int numCardsDrawn)
+        {
+            if (!cache.ContainsKey(state))
+            {
+                if(state.NumCardsRemaining == 0)
+                {
+                    cache[state] = 1;
+                }
+                else
+                {
+                    var cumulativeProbability = 0.0;
+                    foreach(var key in state.RemainingCardsOfType.Dictionary.Keys)
+                    {
+                        if (restrictions.ContainsKey(key) && 
+                            restrictions[key].Contains(numCardsDrawn))
+                        {
+                            continue;
+                        }
+                        
+                        var probabilityOfCardDrawn 
+                            = (double)state.RemainingCardsOfType.Dictionary[key]
+                            / (double)state.NumCardsRemaining;
+                        
+                        var remainingCardsOfTypeClone =
+                            state.RemainingCardsOfType.Clone();
+                        remainingCardsOfTypeClone.Dictionary[key]--;
+                        if (remainingCardsOfTypeClone.Dictionary[key] == 0)
+                        {
+                            remainingCardsOfTypeClone.Dictionary.Remove(key);
+                        }
+                        cumulativeProbability += probabilityOfCardDrawn 
+                            * this.CalculateOddsOfSuccess(
+                                new SolitaireState(
+                                    remainingCardsOfTypeClone.Dictionary,
+                                    state.NumCardsRemaining - 1
+                                ),
+                                restrictions,
+                                cache,
+                                numCardsDrawn+1
+                            );
+                    }
+                    cache[state] = cumulativeProbability;
+                }
+            }
+            return cache[state];
         }
     }
 
     public class SolitaireState
     {
-        Dictionary<string, int> RemainingCardsOfType { get; }
-        public SolitaireState(Dictionary<string, int> remainingCardsOfType)
+        public CacheableDictionary<string, int> RemainingCardsOfType { get; }
+        public int NumCardsRemaining { get; }
+        public SolitaireState(Dictionary<string, int> remainingCardsOfType, int numCardsRemaining)
         {
-            this.RemainingCardsOfType = remainingCardsOfType;
+            this.RemainingCardsOfType = new CacheableDictionary<string, int>(remainingCardsOfType);
+            this.NumCardsRemaining = numCardsRemaining;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is SolitaireState))
+            {
+                return false;
+            }
+            // note, don't need to consider numCardsRemaining because it can be derived from
+            // RemainingCardsOfType
+            return ((SolitaireState)obj).RemainingCardsOfType.Equals(this.RemainingCardsOfType);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.RemainingCardsOfType.GetHashCode();
         }
     }
 }
