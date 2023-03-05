1c3eb92e132c8158b40243eb3c11586863a55c8a
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2023-03-04 21:12:33 -0500

    Code and tests to interleave lists

---
+++ Riddles/Riddles/Riddles.Tests/Combinatorics/Application/ListInterleaverTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using Riddles.Combinatorics.Application;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riddles.Tests.Combinatorics.Application
+{
+    public class ListInterleaverTest
+    {
+        Dictionary<int, (List<int>, int)> testCaseDictionary = new Dictionary<int, (List<int>, int)>
+        {
+            {1, (new List<int> { 1 }, 1) },
+            {2, (new List<int> { 4 }, 1) },
+            {3, (new List<int> {  }, 0) },
+            {4, (new List<int> { 4, 1, -1 }, 0) },
+            {5, (new List<int> { 4, 0, 0, 0 }, 1) },
+            {6, (new List<int> { 4, 1 }, 5) },
+            {7, (new List<int> { 4, 4 }, 70) },
+            {8, (new List<int> { 1, 1, 1, 1, 1 }, 120) },
+            {9, (new List<int> {2, 2, 2}, 90) }
+        };
+
+        [TestCase(1)] // one list
+        [TestCase(2)] // one list
+        [TestCase(3)] // no lists
+        [TestCase(4)] // list with invalid count
+        [TestCase(5)] // multiple lists with 0 elements
+        [TestCase(6)] // two lists, one of size 1
+        [TestCase(7)] // two non-trivial lists
+        [TestCase(8)] // five lists of size 1
+        [TestCase(9)] // three lists of size 2
+        public void TestCountNumberOfWaysToInterleaveLists(int testCaseId)
+        {
+            var listInterleaver = new ListInterleaver();
+            var testCase = testCaseDictionary[testCaseId];
+            var actual = listInterleaver.CountNumberOfWaysToInterleaveLists(testCase.Item1);
+            var expected = testCase.Item2;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Combinatorics/Application/ListInterleaver.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Riddles.Combinatorics.Application
+{
+    public class ListInterleaver
+    {
+        public Dictionary<(int, int), int> _cache;
+        public ListInterleaver()
+        {
+            this._cache = new Dictionary<(int, int), int>();
+        }
+        /// <summary>
+        /// Used to count the number of ways to interleave lists of specific sizes.
+        /// https://docs.google.com/document/d/1pnmNARUuVb0AGc95-xyJDHboSB_MsS41U9Sl7w0uFvk/edit
+        /// </summary>
+        /// <param name="listCounts"></param>
+        /// <returns></returns>
+        public int CountNumberOfWaysToInterleaveLists(List<int> listCounts)
+        {
+            if(listCounts.Count() == 0 || !listCounts.All(x => x >= 0))
+            {
+                return 0;
+            }
+            var numElementsInList = 0;
+            var numWaysToOrderList = 1;
+            // when finding the ways to interleave multiple lists, can do it iteratively
+            // first, find the number of ways to interleave the first two elements in the list.
+            // then consider the result one list, and interleave it with the next list
+            // then multiply the number of ways to combine the lists together.
+            foreach(var count in listCounts)
+            {
+                numWaysToOrderList *= 
+                    this.CountNumberOfWaysToInterleaveTwoLists(count, numElementsInList);
+                numElementsInList += count;
+            }
+            return numWaysToOrderList;
+        }
+
+        private int CountNumberOfWaysToInterleaveTwoLists(int list1Count, int list2Count)
+        {
+            var min = Math.Min(list1Count, list2Count);
+            var max = Math.Max(list1Count, list2Count);
+
+            if(!this._cache.ContainsKey((min, max)))
+            {
+                if(min == 0)
+                {
+                    this._cache[(min, max)] = 1;
+                }
+                else if(min == 1)
+                {
+                    this._cache[(min, max)] = max + 1;
+                }
+                else
+                {
+                    // there are two options: either take the first element off list 1
+                    // or take the first element off of list 2
+                    this._cache[(min, max)] =
+                        this.CountNumberOfWaysToInterleaveTwoLists(list1Count - 1, list2Count) +
+                        this.CountNumberOfWaysToInterleaveTwoLists(list1Count, list2Count - 1);
+
+                }
+            }
+            return this._cache[(min, max)];
+        }
+    }
+}
