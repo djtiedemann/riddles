3d6085d75d9272d7705dfc76d8376713ccf689cb
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2023-03-23 22:01:15 -0400

    Add solution for stacking deck with suits

--- Riddles/Riddles/Riddles.Tests/Poker/Application/MultiSuitPokerHandProbabilityCalculatorTest.cs
+++ Riddles/Riddles/Riddles.Tests/Poker/Application/MultiSuitPokerHandProbabilityCalculatorTest.cs
@@ -1,26 +1,65 @@
-﻿using Riddles.Poker.Core;
+﻿using NUnit.Framework;
+using Riddles.Poker.Core;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Riddles.Tests.Poker.Application
 {
+    // https://fivethirtyeight.com/features/can-you-stack-the-deck-with-suits/
     public class MultiSuitPokerHandProbabilityCalculatorTest
     {
-        public void TestNumSuitsRequiredForFullHouseToBeMoreCommonThanStraightFiveCardStud()
+        private double Epsilon = 0.00000001;
+
+        [TestCase(16, 4.0336134453781511, 4.0345368916797488)]
+        [TestCase(17, 5.5510204081632653, 5.4631083202511777)]
+        public void TestNumSuitsRequiredForFullHouseToBeMoreCommonThanStraightFiveCardStud(
+            int numSuits,
+            double expectedFullHouseOdds,
+            double expectedStraightOdds
+        )
         {
             var fiveCardStudProbabilityCalculator
                 = new FiveCardStudPokerHandProbabilityCalculator();
 
-            for(int numSuits = 4; numSuits<100; numSuits++)
-            {
+            var fullHouseOdds = fiveCardStudProbabilityCalculator
+                .CalculateOddsOfHand(
+                    Riddles.Poker.Domain.HandType.FullHouse,
+                    numSuits
+                );
+            var straightOdds = fiveCardStudProbabilityCalculator
+                .CalculateOddsOfHand(
+                    Riddles.Poker.Domain.HandType.Straight,
+                    numSuits
+                );
 
-            }
+            Assert.LessOrEqual(Math.Abs(expectedFullHouseOdds - fullHouseOdds), this.Epsilon);
+            Assert.LessOrEqual(Math.Abs(expectedStraightOdds - straightOdds), this.Epsilon);
         }
 
-        public void TestNumSuitsRequiredForFullHouseToBeMoreCommonThanStraightSevenCardStud()
+        [TestCase(7, 2.4637318387114329, 2.590349663668214)]
+        [TestCase(8, 6.87833705174947, 6.6875965358035341)]
+        public void TestNumSuitsRequiredForFullHouseToBeMoreCommonThanStraightSevenCardStud(
+            int numSuits,
+            double expectedFullHouseOdds,
+            double expectedStraightOdds
+        )
         {
+            var sevenCardStudProbabilityCalculator
+                = new SevenCardStudPokerHandProbabilityCalculator();
 
+            var fullHouseOdds = sevenCardStudProbabilityCalculator
+                .CalculateOddsOfHand(
+                    Riddles.Poker.Domain.HandType.FullHouse,
+                    numSuits
+                );
+            var straightOdds = sevenCardStudProbabilityCalculator
+                .CalculateOddsOfHand(
+                    Riddles.Poker.Domain.HandType.Straight,
+                    numSuits
+                );
+            Assert.LessOrEqual(Math.Abs(expectedFullHouseOdds - fullHouseOdds), this.Epsilon);
+            Assert.LessOrEqual(Math.Abs(expectedStraightOdds - straightOdds), this.Epsilon);
         }
     }
 }
