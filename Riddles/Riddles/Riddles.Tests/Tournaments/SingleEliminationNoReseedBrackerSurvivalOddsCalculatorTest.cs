8eeb566d0ce2709078f88230ec3ed1a7cbf7c18b
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2023-03-28 23:23:37 -0400

    Add in code to determine how likely teams are to survive single elimination bracket

---
+++ Riddles/Riddles/Riddles.Tests/Tournaments/SingleEliminationNoReseedBrackerSurvivalOddsCalculatorTest.cs
@@ -0,0 +1,237 @@
+﻿using NUnit.Framework;
+using Riddles.Probability;
+using Riddles.Tournaments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Riddles.Tests.Tournaments
+{
+    public class SingleEliminationNoReseedBrackerSurvivalOddsCalculatorTest
+    {
+        private double Epsilon = 0.00000001;
+
+        private Dictionary<int, (SingleEliminationNoReseedBrackerSurvivalOddsCalculator.WinningLikelihoodModel, int[], double[])> TestCaseDictionary =
+            new Dictionary<int, (SingleEliminationNoReseedBrackerSurvivalOddsCalculator.WinningLikelihoodModel, int[], double[])>
+            {
+                { 1, (
+                    SingleEliminationNoReseedBrackerSurvivalOddsCalculator.WinningLikelihoodModel.EqualOdds,
+                    new int[] { 1, 16, 8, 9},
+                    new double[] {0.25, 0.25, 0.25, 0.25 }
+                ) },
+                { 2, (
+                    SingleEliminationNoReseedBrackerSurvivalOddsCalculator.WinningLikelihoodModel.EqualOdds,
+                    new int[] { 4, 13, 5, 12},
+                    new double[] {0.25, 0.25, 0.25, 0.25 }
+                ) },
+                { 3, (
+                    SingleEliminationNoReseedBrackerSurvivalOddsCalculator.WinningLikelihoodModel.EqualOdds,
+                    new int[] { 3, 14, 6, 11},
+                    new double[] {0.25, 0.25, 0.25, 0.25 }
+                ) },
+                { 4, (
+                    SingleEliminationNoReseedBrackerSurvivalOddsCalculator.WinningLikelihoodModel.EqualOdds,
+                    new int[] { 2, 15, 7, 10},
+                    new double[] {0.25, 0.25, 0.25, 0.25 }
+                ) },
+                { 5, (
+                    SingleEliminationNoReseedBrackerSurvivalOddsCalculator.WinningLikelihoodModel.WeightedOdds,
+                    new int[] { 1, 16, 8, 9},
+                    new double[] { 0.742678945, 0.0012570550000000013, 0.144696175, 0.11136782499999998 }
+                ) },
+                { 6, (
+                    SingleEliminationNoReseedBrackerSurvivalOddsCalculator.WinningLikelihoodModel.WeightedOdds,
+                    new int[] { 4, 13, 5, 12},
+                    new double[] { 0.474325783, 0.060522216999999982, 0.38544972099999997, 0.079702279 }
+                ) },
+                { 7, (
+                    SingleEliminationNoReseedBrackerSurvivalOddsCalculator.WinningLikelihoodModel.WeightedOdds,
+                    new int[] { 3, 14, 6, 11},
+                    new double[] { 0.56463932499999991, 0.039904674999999994, 0.299736115, 0.095719884999999977 }
+                ) },
+                { 8, (
+                    SingleEliminationNoReseedBrackerSurvivalOddsCalculator.WinningLikelihoodModel.WeightedOdds,
+                    new int[] { 2, 15, 7, 10},
+                    new double[] { 0.654665371, 0.019574628999999986, 0.21890994099999994, 0.10685005899999998 }
+                ) },
+                { 9, (
+                    SingleEliminationNoReseedBrackerSurvivalOddsCalculator.WinningLikelihoodModel.WeightedOdds,
+                    new int[] { 1, 2, 3, 4, 5, 6, 7, 8 },
+                    new double[] { 
+                        0.2642096966605828,
+                        0.20633362732316921,
+                        0.1591677212675254,
+                        0.12077636935295818,
+                        0.091652024338911439,
+                        0.06955308421469919,
+                        0.051460123002606217,
+                        0.036847353839547577
+                    }
+                ) },
+                { 10, (
+                    SingleEliminationNoReseedBrackerSurvivalOddsCalculator.WinningLikelihoodModel.WeightedOdds,
+                    new int[] { 1, 2, 3, 4, 5, 6, 7 },
+                    new double[] {
+                        0.3614359735438889,
+                        0.19455251178300281,
+                        0.14949019396398325,
+                        0.10457189766039186,
+                        0.0784117342457192,
+                        0.064302527715000724,
+                        0.047235161088013226
+                    }
+                ) },
+                { 11, (
+                    SingleEliminationNoReseedBrackerSurvivalOddsCalculator.WinningLikelihoodModel.WeightedOdds,
+                    new int[] { 1, 2, 3, 4, 5, 6 },
+                    new double[] {
+                        0.34512090418781505,
+                        0.29256016809474106,
+                        0.13366889202274698,
+                        0.098896431695945,
+                        0.07384827011624,
+                        0.055905333882511996,
+                    }
+                ) },
+                { 12, (
+                    SingleEliminationNoReseedBrackerSurvivalOddsCalculator.WinningLikelihoodModel.WeightedOdds,
+                    new int[] { 1, 2, 3, 4, 5 },
+                    new double[] {
+                        0.336949750921,
+                        0.272280149947,
+                        0.22315340905300002,
+                        0.096053961263000018,
+                        0.071562728816,
+                    }
+                ) },
+                { 13, (
+                    SingleEliminationNoReseedBrackerSurvivalOddsCalculator.WinningLikelihoodModel.EqualOdds,
+                    new int[] { 1, 2, 3, 4, 5, 6, 7, 8 },
+                    new double[] {
+                        1.0/8.0,
+                        1.0/8.0,
+                        1.0/8.0,
+                        1.0/8.0,
+                        1.0/8.0,
+                        1.0/8.0,
+                        1.0/8.0,
+                        1.0/8.0,
+                    }
+                ) },
+                { 14, (
+                    SingleEliminationNoReseedBrackerSurvivalOddsCalculator.WinningLikelihoodModel.EqualOdds,
+                    new int[] { 1, 2, 3, 4, 5, 6, 7 },
+                    new double[] {
+                        1.0/4.0,
+                        1.0/8.0,
+                        1.0/8.0,
+                        1.0/8.0,
+                        1.0/8.0,
+                        1.0/8.0,
+                        1.0/8.0,
+                    }
+                ) },
+                { 15, (
+                    SingleEliminationNoReseedBrackerSurvivalOddsCalculator.WinningLikelihoodModel.EqualOdds,
+                    new int[] { 1, 2, 3, 4, 5, 6 },
+                    new double[] {
+                        1.0/4.0,
+                        1.0/4.0,
+                        1.0/8.0,
+                        1.0/8.0,
+                        1.0/8.0,
+                        1.0/8.0,
+                    }
+                ) },
+                { 16, (
+                    SingleEliminationNoReseedBrackerSurvivalOddsCalculator.WinningLikelihoodModel.EqualOdds,
+                    new int[] { 1, 2, 3, 4, 5 },
+                    new double[] {
+                        1.0/4.0,
+                        1.0/4.0,
+                        1.0/4.0,
+                        1.0/8.0,
+                        1.0/8.0,
+                    }
+                ) },
+            };
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(6)]
+        [TestCase(7)]
+        [TestCase(8)]
+        [TestCase(9)]
+        [TestCase(10)]
+        [TestCase(11)]
+        [TestCase(12)]
+        [TestCase(13)]
+        [TestCase(14)]
+        [TestCase(15)]
+        [TestCase(16)]
+        public void TestCalculateOddsOfTeamEachAdvancing(int testCaseId)
+        {
+            var (likelihoodModel, seeds, expectedOdds) = this.TestCaseDictionary[testCaseId];
+            var singleEliminationNoReseedBrackerSurvivalOddsCalculator = new SingleEliminationNoReseedBrackerSurvivalOddsCalculator();
+            var actualOdds = singleEliminationNoReseedBrackerSurvivalOddsCalculator.CalculateOddsOfTeamEachAdvancing(
+                seeds.ToList(),
+                likelihoodModel
+            );
+            Assert.AreEqual(expectedOdds.Length, actualOdds.Count);
+            var cumulativeProbability = 0.0;
+            for (int i = 0; i < seeds.Length; i++)
+            {
+                Assert.LessOrEqual(Math.Abs(expectedOdds[i] - actualOdds[seeds[i]]), this.Epsilon);
+                cumulativeProbability += actualOdds[seeds[i]];
+            }
+            Assert.LessOrEqual(Math.Abs(cumulativeProbability - 1), this.Epsilon);
+        }
+
+        // test internals. may be helpful for debugging
+        /*[TestCase(0, 0, 0, 0)]
+        [TestCase(1, 0, 1, 1)]
+        [TestCase(2, 0, 2, 2)]
+        [TestCase(3, 0, 3, 3)]
+        [TestCase(4, 0, 4, 4)]
+        [TestCase(5, 0, 5, 5)]
+        [TestCase(6, 0, 6, 6)]
+        [TestCase(7, 0, 7, 7)]
+        [TestCase(0, 1, 1, 1)]
+        [TestCase(1, 1, 0, 0)]
+        [TestCase(2, 1, 3, 3)]
+        [TestCase(3, 1, 2, 2)]
+        [TestCase(4, 1, 5, 5)]
+        [TestCase(5, 1, 4, 4)]
+        [TestCase(6, 1, 7, 7)]
+        [TestCase(7, 1, 6, 6)]
+        [TestCase(0, 2, 2, 3)]
+        [TestCase(1, 2, 2, 3)]
+        [TestCase(2, 2, 0, 1)]
+        [TestCase(3, 2, 0, 1)]
+        [TestCase(4, 2, 6, 7)]
+        [TestCase(5, 2, 6, 7)]
+        [TestCase(6, 2, 4, 5)]
+        [TestCase(7, 2, 4, 5)]
+        [TestCase(0, 3, 4, 7)]
+        [TestCase(1, 3, 4, 7)]
+        [TestCase(2, 3, 4, 7)]
+        [TestCase(3, 3, 4, 7)]
+        [TestCase(4, 3, 0, 3)]
+        [TestCase(5, 3, 0, 3)]
+        [TestCase(6, 3, 0, 3)]
+        [TestCase(7, 3, 0, 3)]
+        public void TestMinMax(int seed, int round, int expectedMin, int expectedMax)
+        {
+            var singleEliminationNoReseedBrackerSurvivalOddsCalculator
+                = new SingleEliminationNoReseedBrackerSurvivalOddsCalculator();
+            var (actualMin, actualMax) = singleEliminationNoReseedBrackerSurvivalOddsCalculator
+                .GenerateCompetitionRangeInclusive(seed, round);
+            Assert.AreEqual(expectedMin, actualMin);
+            Assert.AreEqual(expectedMax, actualMax);
+        }*/
+    }
+}
---
+++ Riddles/Riddles/Riddles/Tournaments/SingleEliminationNoReseedBrackerSurvivalOddsCalculator.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Riddles.Tournaments
+{
+    public class SingleEliminationNoReseedBrackerSurvivalOddsCalculator
+    {
+        public enum WinningLikelihoodModel
+        {
+            EqualOdds = 1,
+            WeightedOdds = 2
+        }
+
+        private SingleEliminationNoReseedBracketCreator _singleEliminationNoReseedBracketCreator;
+        public SingleEliminationNoReseedBrackerSurvivalOddsCalculator()
+        {
+            this._singleEliminationNoReseedBracketCreator =
+                new SingleEliminationNoReseedBracketCreator();
+        }
+
+        public Dictionary<int, double> CalculateOddsOfTeamEachAdvancing(
+            List<int> seeds,
+            WinningLikelihoodModel likelihoodModel
+        )
+        {
+            var bracket = this._singleEliminationNoReseedBracketCreator
+                .CreateBracket(seeds);
+            var oddsOfAdvancingLastRound = bracket.Select(i => 1.0).ToArray();
+            var oddsOfAdvancingThisRound = new double[bracket.Length];
+            var numRounds = Math.Log2(bracket.Length);
+            for (int round = 1; round <= numRounds; round++)
+            { 
+                for (int seed = 0; seed < bracket.Length; seed++)
+                {
+                    var range = this.GenerateCompetitionRangeInclusive(seed, round);
+                    var cumulativeOddsOfWinningRound = 0.0;
+                    for(int competitor=range.Item1; competitor<=range.Item2; competitor++)
+                    {
+                        cumulativeOddsOfWinningRound += this.HeadToHeadProbabilityFunction(bracket[seed], bracket[competitor], likelihoodModel) * oddsOfAdvancingLastRound[competitor];
+                    }
+                    oddsOfAdvancingThisRound[seed] = cumulativeOddsOfWinningRound * oddsOfAdvancingLastRound[seed];
+                }
+                oddsOfAdvancingLastRound = oddsOfAdvancingThisRound.ToArray();
+            }
+            var result =
+                bracket.Select((x, i) => (x, ((x != null) ? (double?)oddsOfAdvancingLastRound[i] : (double?)null)))
+                .Where(i => i.x != null && i.Item2 != null)
+                .ToDictionary(i => (int)i.x, i => (double)i.Item2);
+            return result;
+        }
+
+        /*
+         * Generates the range of competitors for seed for this round of the bracket
+         * The idea is that you can calculate the competition that should be in range
+         * for this round of the bracket and subtract the competitors that you already
+         * played (the ones that were in range for the last round of the bracket).
+         * 
+         * Then you can use the probability that the team would make it to this part
+         * of the bracket and the probability that this team would beat that team
+         */
+        private (int, int) GenerateCompetitionRangeInclusive(int seed, int round)
+        {
+            if(round == 0)
+            {
+                return (seed, seed);
+            }
+            var rangeSize = 1 << round;
+            var min = (seed / rangeSize) * rangeSize;
+            var max = min + (rangeSize - 1);
+            var previousRangeSize = 1 << (round - 1);
+            var prevMin = round > 1 ? (seed / previousRangeSize) * previousRangeSize : seed;
+            var prevMax = prevMin + (previousRangeSize - 1);
+            var actualMin = (min == prevMin) ? prevMax + 1 : min;
+            var actualMax = (max == prevMax) ? prevMin - 1 : max;
+            return (actualMin, actualMax);
+        }
+
+        private double HeadToHeadProbabilityFunction(
+            int? seed1,
+            int? seed2,
+            WinningLikelihoodModel likelihoodModel)
+        {
+            // null means the team has a bye.
+            // if two teams that are null face each other in a hypothetical bracket
+            // the matchup is inconsequential because they will both already be 
+            // eliminated
+            if(seed2 == null)
+            {
+                return 1.0;
+            }
+            if(seed1 == null)
+            {
+                return 0.0;
+            }
+            switch (likelihoodModel)
+            {
+                case WinningLikelihoodModel.EqualOdds:
+                    return 0.5;
+                case WinningLikelihoodModel.WeightedOdds:
+                    return 0.5 + 0.033 * (seed2.Value - seed1.Value);
+            }
+            throw new NotImplementedException("Unsupported likelihood model");
+        }
+    }
+}
