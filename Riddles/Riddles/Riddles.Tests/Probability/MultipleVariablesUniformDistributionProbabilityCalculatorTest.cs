d705c150769768cc5aa7857be6b5dc9be6e54b30
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2023-04-08 17:43:15 -0400

    Calculate probability of multiple variables on uniform distribution

---
+++ Riddles/Riddles/Riddles.Tests/Probability/MultipleVariablesUniformDistributionProbabilityCalculatorTest.cs
@@ -0,0 +1,155 @@
+﻿using NUnit.Framework;
+using Riddles.Probability;
+using Riddles.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Riddles.Tests.Probability
+{
+    public class MultipleVariablesUniformDistributionProbabilityCalculatorTest
+    {
+        private double Epsilon = 0.00000001;
+        private Dictionary<int, (List<List<(double, double)>>, double[])>
+            _testCaseDictionary =
+            new Dictionary<int, (List<List<(double, double)>>, double[])>
+            {
+                {1, (new List<List<(double, double)>>{
+                        new List<(double, double)> {
+                            (0.0, 1.0)
+                        },new List<(double, double)> {
+                            (0.25, 0.75)
+                        }
+                    },
+                    new double[]{
+                        0.5,
+                        0.5
+                    })
+                },
+                {2, (new List<List<(double, double)>>{
+                        new List<(double, double)> {
+                            (0.0, 0.25),
+                            (0.75, 1.0)
+                        },new List<(double, double)> {
+                            (0.25, 0.75)
+                        }
+                    },
+                    new double[]{
+                        0.5,
+                        0.5
+                    })
+                },
+                {3, (new List<List<(double, double)>>{
+                        new List<(double, double)> {
+                            (0.0, 1.0)
+                        },
+                        new List<(double, double)> {
+                            (0.0, 0.25),
+                            (0.75, 1.0)
+                        }
+                    },
+                    new double[]{
+                        1.0/2.0,
+                        1.0/2.0
+                    })
+                },
+                {4, (new List<List<(double, double)>>{
+                        new List<(double, double)> {
+                            (0.0, 1.0)
+                        },
+                        new List<(double, double)> {
+                            (0.0, 1.0)
+                        },
+                        new List<(double, double)> {
+                            (0.0, 1.0)
+                        }
+                    },
+                    new double[]{
+                        1.0/3.0,
+                        1.0/3.0,
+                        1.0/3.0
+                    })
+                },
+                {5, (new List<List<(double, double)>>{
+                        new List<(double, double)> {
+                            (0.5, 1.0)
+                        },
+                        new List<(double, double)> {
+                            (0.0, 1.0)
+                        }
+                    },
+                    new double[]{
+                        3.0/4.0,
+                        1.0/4.0
+                    })
+                },
+                {6, (new List<List<(double, double)>>{
+                        new List<(double, double)> {
+                            (0.0, 0.5)
+                        },
+                        new List<(double, double)> {
+                            (0.0, 1.0)
+                        }
+                    },
+                    new double[]{
+                        1.0/4.0,
+                        3.0/4.0
+                    })
+                },
+                {7, (new List<List<(double, double)>>{
+                        new List<(double, double)> { 
+                            (0.0, 1.0)
+                        },
+                        new List<(double, double)> {
+                            (0.0, 0.25),
+                            (0.75, 1.0)
+                        },new List<(double, double)> {
+                            (0.25, 0.75)
+                        }
+                    }, 
+                    new double[]{ 
+                        1.0/3.0,
+                        1.0/3.0,
+                        1.0/3.0
+                    }) 
+                }
+            };
+
+        [TestCase(1)] // two teams, equal probability
+        [TestCase(2)] // two teams, equal probability
+        [TestCase(3)] // two teams, equal probability
+        [TestCase(4)] // three teams, equal probability
+        [TestCase(5)] // two teams, skewed probability
+        [TestCase(6)] // two teams, skewed probability
+        //[TestCase(7)]
+        public void TestCalculateProbabilityTeamHasHighestValue(int testCaseId)
+        {
+            var testCase = this._testCaseDictionary[testCaseId];
+            var distribution = testCase.Item1;
+            var expectedProbabilities = testCase.Item2;
+            var multipleVariablesUniformDistributionProbabilityCalculator =
+                new MultipleVariablesUniformDistributionProbabilityCalculator();
+            var actualProbabilities =
+                multipleVariablesUniformDistributionProbabilityCalculator
+                    .CalculateProbabilityTeamHasHighestValue(
+                        distribution
+                    );
+            Assert.AreEqual(
+                expectedProbabilities.Length, 
+                actualProbabilities.Length
+            );
+            Assert.LessOrEqual(
+                Math.Abs(actualProbabilities.Sum() - 1),
+                this.Epsilon
+            );
+            for(int i=0; i<expectedProbabilities.Length; i++)
+            {
+                Assert.LessOrEqual(
+                    Math.Abs(expectedProbabilities[i] - actualProbabilities[i]),
+                    this.Epsilon
+                );
+            }
+        }
+    }
+}
--- Riddles/Riddles/Riddles/Probability/MultipleVariablesUniformDistributionProbabilityCalculator.cs
+++ Riddles/Riddles/Riddles/Probability/MultipleVariablesUniformDistributionProbabilityCalculator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Riddles.Combinatorics.Core.SetGeneration;
+using Riddles.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,11 +9,81 @@ namespace Riddles.Probability
 {
     public class MultipleVariablesUniformDistributionProbabilityCalculator
     {
-        public double CalculateProbabilityTeamHasHighestValue(
-            List<List<(double, double)>> distributions,
-            int teamId)
+        private MutuallyExclusiveCollectivelyExhaustiveRangeCreator _meceRangeCreator;
+        private PermutationWithRepetitionGenerator _outcomeGenerator;
+        public MultipleVariablesUniformDistributionProbabilityCalculator()
         {
-            throw new NotImplementedException();
+            this._meceRangeCreator 
+                = new MutuallyExclusiveCollectivelyExhaustiveRangeCreator();
+            this._outcomeGenerator = new PermutationWithRepetitionGenerator();
+        }
+        public double[] CalculateProbabilityTeamHasHighestValue(
+            List<List<(double, double)>> distributions
+        )
+        {
+            if(distributions == null || distributions.Count == 0)
+            {
+                return null;
+            }
+            var numDistributions = distributions.Count;
+            var meceRanges = this._meceRangeCreator.CreateMeceRanges(distributions);
+            var numRanges = meceRanges.First().Count;
+            // calculate size of each distribution
+            // used to determine the probability a certain distribution has a
+            // value within the range
+            var rangeSizes =
+                meceRanges.Select(
+                    r => r.Aggregate(
+                        0.0,
+                        (agg, v) => agg + (
+                            v.Item3 
+                                ? (v.Item2 - v.Item1)
+                                : 0.0
+                        )
+                    )
+                ).ToList();
+
+            // go through each possible combination of ranges
+            // calculate the probability that this range is selected
+            // calculate the probability of each team winning based on that probability
+            var currentOutcome = distributions.Select(d => 0).ToArray();
+            var probabilities = distributions.Select(d => 0.0).ToArray();
+            while(currentOutcome != null)
+            {
+                // calculate the probability this combination occurs
+                var probability = 1.0;
+                for(int i=0; i<numDistributions; i++)
+                {
+                    var range = meceRanges[i][currentOutcome[i]];
+                    probability *= 
+                        range.Item3 
+                            ? (range.Item2 - range.Item1) / rangeSizes[i] : 0.0;
+                }
+
+                // calculate the team that wins in this combination
+                if(probability > 0)
+                {
+                    var maxRange = currentOutcome.Max(x => x);
+                    var winningTeams = currentOutcome.Select(
+                        (rangeNum, index) => (rangeNum, index)
+                    ).Where(x => x.rangeNum == maxRange)
+                        .Select(x => x.index)
+                        .ToList();
+
+                    var numWinningTeams = winningTeams.Count;
+                    foreach(var winningTeam in winningTeams) {
+                        probabilities[winningTeam] += (probability / numWinningTeams);
+                    }
+                }
+
+                // advance to the next outcome
+                currentOutcome = this._outcomeGenerator.GenerateNextOutcome(
+                    currentOutcome,
+                    0,
+                    numRanges - 1
+                );
+            }
+            return probabilities;
         }
     }
 }
