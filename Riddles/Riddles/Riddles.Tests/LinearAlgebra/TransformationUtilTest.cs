b14900414ef1056ffcfa7b11ff59d4f9cef1ea79
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2023-04-09 00:19:19 -0400

    Add function to transform fraction to range

---
+++ Riddles/Riddles/Riddles.Tests/LinearAlgebra/TransformationUtilTest.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using Riddles.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riddles.Tests.LinearAlgebra
+{
+    public class TransformationUtilTest
+    {
+        private double Epsilon = 0.00000001;
+        private Dictionary<int, (double, List<(double, double)>, double)>
+            _testCaseDictionary
+                = new Dictionary<int, (double, List<(double, double)>, double)>
+                {
+                    { 1, (0.75, 
+                        new List<(double, double)> { (0.0, 0.25), (0.75, 1.0), },
+                        0.875) 
+                    },
+                    { 2, (0.25,
+                        new List<(double, double)> { (0.0, 0.25), (0.75, 1.0), },
+                        0.125)
+                    },
+                    { 3, (0.9,
+                        new List<(double, double)> { 
+                            (0.0, 1.5), (3.0, 3.5), (6.0, 7.5), (9.5, 11.0)},
+                        10.5)
+                    },
+                    { 4, (0.25,
+                        new List<(double, double)> {
+                            (0.0, 1.5), (3.0, 3.5), (6.0, 7.5), (9.5, 11.0)},
+                        1.25)
+                    },
+                    { 5, (0.313,
+                        new List<(double, double)> {
+                            (0.0, 1.5), (3.0, 3.5), (6.0, 7.5), (9.5, 11.0)},
+                        3.065)
+                    },
+                    { 6, (0.317,
+                        new List<(double, double)> {
+                            (2.0, 4.0)
+                        }, 2.634)
+                    }
+                };
+
+        [TestCase(1)] // two equal ranges
+        [TestCase(2)] // two equal ranges
+        [TestCase(3)] // several unequal ranges
+        [TestCase(4)] // several unequal ranges
+        [TestCase(5)] // several unequal ranges
+        [TestCase(6)] // single range
+        public void TestTransformFractionToUniformDistribution(int testCaseId)
+        {
+            var testCase = this._testCaseDictionary[testCaseId];
+            var transformationUtil = new TransformationUtil();
+            var fraction = testCase.Item1;
+            var ranges = testCase.Item2;
+            var expectedResult = testCase.Item3;
+            var actualResult = transformationUtil
+                .TransformFractionToUniformDistribution(fraction, ranges);
+            Assert.LessOrEqual(Math.Abs(expectedResult - actualResult), this.Epsilon);
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/LinearAlgebra/TransformationUtil.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Riddles.LinearAlgebra
+{
+    public class TransformationUtil
+    {
+        public double TransformFractionToUniformDistribution(
+            double fraction,
+            List<(double, double)> ranges)
+        {
+            var rangeSize = ranges.Sum(x => x.Item2 - x.Item1);
+            var scaledFraction = fraction * rangeSize;
+
+            foreach(var range in ranges)
+            {
+                var currentRangeSize = range.Item2 - range.Item1;
+                if(scaledFraction > currentRangeSize)
+                {
+                    scaledFraction -= currentRangeSize;
+                }
+                else
+                {
+                    return range.Item1 + scaledFraction;
+                }
+            }
+            return ranges.Last().Item2;
+        }
+    }
+}
