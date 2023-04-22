9f333aa7ad08fb2fbfae5b3391ae7551c999ba74
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2023-04-22 10:12:11 -0400

    Add in first draft of expressions

---
+++ Riddles/Riddles/Riddles.Tests/Algebra/Domain/ExpressionTest.cs
@@ -0,0 +1,193 @@
+﻿using NUnit.Framework;
+using Riddles.Algebra.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riddles.Tests.Algebra.Domain
+{
+    public class ExpressionTest
+    {
+        private Dictionary<int, (Expression, string)> _testCaseDictionary 
+            = new Dictionary<int, (Expression, string)>
+            {
+                {1, (new Term("A"), "A") },
+                {2, (new Term("B"), "B") },
+                {3, (new Sum(
+                    new List<Expression> { new Term("A"), new Term("C") }, 
+                    new List<Expression> { new Term("D"), new Term("B")}, true), 
+                    "((A+C)-(B+D))") },
+                {4, (new Sum(
+                    new List<Expression> { new Term("A"), new Term("C"), new Term("B") },
+                    new List<Expression> { new Term("D")}, true),
+                    "((A+B+C)-(D))") },
+                {5, (new Sum(
+                    new List<Expression> { new Term("D"), new Term("C"), new Term("B") },
+                    new List<Expression> { }, true),
+                    "(B+C+D)") },
+                {6, (new Sum(
+                    new List<Expression> {
+                        new Sum(new List<Expression>{
+                                new Term("X"),
+                                new Term("A"), 
+                                new Term("C"),
+                            }, 
+                            new List<Expression> {
+                                new Term("M"),
+                                new Term("R")
+                            }),
+                        new Sum(new List<Expression>{
+                                new Term("L"),
+                            },
+                            new List<Expression> {
+                                new Term("T"),
+                                new Term("V"),
+                                new Term("Y"),
+                            }),
+                        new Sum(new List<Expression>{
+                                new Term("P"),
+                            },
+                            new List<Expression> {
+                                new Term("Q"),
+                                new Term("Z"),
+                                new Term("O"),
+                            }),
+                    },
+                    new List<Expression> { }, false),
+                    "(((A+C+X)-(M+R))+((L)-(T+V+Y))+((P)-(O+Q+Z)))") },
+                {7, (new Sum(
+                    new List<Expression> {
+                        new Sum(
+                            new List<Expression>
+                            {
+                                new Term("H"),
+                                new Term("F")
+                            },
+                            new List<Expression>
+                            {
+                                new Term("G")
+                            },
+                            false
+                        ) },
+                    new List<Expression>{
+                        new Sum(new List<Expression>{
+                                new Term("X"),
+                                new Term("A"),
+                                new Term("C"),
+                            },
+                            new List<Expression> {
+                                new Term("M"),
+                                new Term("R")
+                            }),
+                        new Sum(new List<Expression>{
+                                new Term("L"),
+                            },
+                            new List<Expression> {
+                                new Term("T"),
+                                new Term("V"),
+                                new Term("Y"),
+                            }),
+                        new Sum(new List<Expression>{
+                                new Term("P"),
+                            },
+                            new List<Expression> {
+                                new Term("Q"),
+                                new Term("Z"),
+                                new Term("O"),
+                            }),
+                    }, false),
+                    "((((F+H)-(G)))-(((A+C+X)-(M+R))+((L)-(T+V+Y))+((P)-(O+Q+Z))))") },
+                {8, (new Sum(
+                    new List<Expression> {
+                        new Sum(new List<Expression>{
+                                new Term("X"),
+                                new Term("A"),
+                                new Term("C"),
+                            },
+                            new List<Expression> {
+                                new Term("M"),
+                                new Term("R")
+                            }),
+                        new Sum(new List<Expression>{
+                                new Term("L"),
+                            },
+                            new List<Expression> {
+                                new Term("T"),
+                                new Term("V"),
+                                new Term("Y"),
+                            }),
+                        new Sum(new List<Expression>{
+                                new Term("P"),
+                            },
+                            new List<Expression> {
+                                new Term("Q"),
+                                new Term("Z"),
+                                new Term("O"),
+                            }),
+                    },
+                    new List<Expression> { }, true),
+                    "((A+C+L+P+X)-(M+O+Q+R+T+V+Y+Z))") },
+                {9, (new Sum(
+                    new List<Expression> {
+                        new Sum(
+                            new List<Expression>
+                            {
+                                new Term("H"),
+                                new Term("F")
+                            },
+                            new List<Expression>
+                            {
+                                new Term("G")
+                            },
+                            true
+                        ) },
+                    new List<Expression>{
+                        new Sum(new List<Expression>{
+                                new Term("X"),
+                                new Term("A"),
+                                new Term("C"),
+                            },
+                            new List<Expression> {
+                                new Term("M"),
+                                new Term("R")
+                            }),
+                        new Sum(new List<Expression>{
+                                new Term("L"),
+                            },
+                            new List<Expression> {
+                                new Term("T"),
+                                new Term("V"),
+                                new Term("Y"),
+                            }),
+                        new Sum(new List<Expression>{
+                                new Term("P"),
+                            },
+                            new List<Expression> {
+                                new Term("Q"),
+                                new Term("Z"),
+                                new Term("O"),
+                            }),
+                    }, true),
+                    "((F+H+M+O+Q+R+T+V+Y+Z)-(A+C+G+L+P+X))") },
+            };
+
+        [TestCase(1)] // Simple term
+        [TestCase(2)] // Simple term
+        [TestCase(3)] // Addition and Subtraction, commutative property
+        [TestCase(4)] // Addition and Subtraction, commutative property lists
+        [TestCase(5)] // Addition, commutative property
+        [TestCase(6)] // Addition and Subtraction - associative and commutative, full form
+        [TestCase(7)] // Addition and Subtraction - associative and commutative, full form
+        [TestCase(8)] // Addition and Subtraction - associative and commutative, simplified
+        [TestCase(9)] // Addition and Subtraction - associative and commutative, simplified
+        // TODO: Add test where the correct ordering is applied for nested sums
+        public void TestGetStringRepresentation(int testCaseId)
+        {
+            var testCase = this._testCaseDictionary[testCaseId];
+            var expression = testCase.Item1;
+            var exprectedResult = testCase.Item2;
+            var actualResult = expression.GetStringRepresentation();
+            Assert.AreEqual(exprectedResult, actualResult);
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Algebra/Domain/Expression.cs
@@ -0,0 +1,163 @@
+﻿using Riddles.Caching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Riddles.Algebra.Domain
+{
+    public enum ExpressionType { 
+        Term = 0,
+        Product = 1,
+        Sum = 2
+    }
+
+    public abstract class Expression
+    {
+        protected List<string> _terms;
+
+        public List<string> Terms { get { return this._terms; } }
+        public abstract string GetStringRepresentation();
+        public abstract double Evaluate(Dictionary<string, int> termValues);
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Expression))
+            {
+                return false;
+            }
+            return this.GetStringRepresentation() == ((Expression)obj).GetStringRepresentation();
+        }
+
+        public override int GetHashCode()
+        {
+            return this.GetStringRepresentation().GetHashCode();
+        }
+    }
+
+    public class Term : Expression
+    {
+        private string? _stringRepresentation;
+        public Term(string term) {
+            this._terms = new List<string> { term };
+        }
+
+        public override string GetStringRepresentation()
+        {
+            if(this._stringRepresentation == null)
+            {
+                this._stringRepresentation = this._terms.Single();
+            }
+            return this._stringRepresentation;
+        }
+
+        public override double Evaluate(Dictionary<string, int> termValues)
+        {
+            return termValues[this._terms.Single()];
+        }
+    }
+
+    /// <summary>
+    /// A Sum class contains a list of expressions that should be summed
+    /// and a list of expressions that should be subracted.
+    /// It orders the expressions and evaluates them
+    /// </summary>
+    public class Sum : Expression
+    {
+        private List<Expression> _addedExpressions;
+        private List<Expression> _subtractedExpressions;
+        private string? _stringRepresentation;
+        public Sum(
+            List<Expression> addedExpressions, 
+            List<Expression> subtractedExpressions,
+            bool simplifyExpression = true
+        )
+        {
+            if (simplifyExpression)
+            {
+                this._addedExpressions = new List<Expression>();
+                this._subtractedExpressions = new List<Expression>();
+                foreach(var expression in addedExpressions)
+                {
+                    if(expression is Sum)
+                    {
+                        this._addedExpressions.AddRange(((Sum)expression).GetAddedExpressions());
+                        this._subtractedExpressions.AddRange(((Sum)expression).GetSubtractedExpressions());
+                    }
+                    else
+                    {
+                        this._addedExpressions.Add(expression);
+                    }
+                }
+                foreach(var expression in subtractedExpressions)
+                {
+                    if (expression is Sum)
+                    {
+                        this._subtractedExpressions.AddRange(((Sum)expression).GetAddedExpressions());
+                        this._addedExpressions.AddRange(((Sum)expression).GetSubtractedExpressions());
+                    }
+                    else
+                    {
+                        this._subtractedExpressions.Add(expression);
+                    }
+                }
+            }
+            else
+            {
+                this._addedExpressions = addedExpressions;
+                this._subtractedExpressions = subtractedExpressions;
+            }
+            
+            this._terms = 
+                addedExpressions.SelectMany(x => x.Terms).ToList().Concat(
+                    subtractedExpressions.SelectMany(x => x.Terms)
+                    ).ToList();
+        }
+
+        public override double Evaluate(Dictionary<string, int> termValues)
+        {
+            var addedExpressions = this._addedExpressions
+                .Sum(x => x.Evaluate(termValues));
+            var subtractedExpressions = this._subtractedExpressions
+                .Sum(x => x.Evaluate(termValues));
+            return addedExpressions - subtractedExpressions;
+        }
+
+        public override string GetStringRepresentation()
+        {
+            if(this._stringRepresentation == null)
+            {
+                var addedStringRepresentations =
+                    this._addedExpressions.Select(s =>
+                        s.GetStringRepresentation()
+                    ).OrderBy(x => x)
+                    .Aggregate("", (acc, exp) => acc == "" ? exp : $"{acc}+{exp}");
+                var subtractedStringRepresentations = 
+                    this._subtractedExpressions.Select(s =>
+                        s.GetStringRepresentation()
+                    ).OrderBy(x => x)
+                    .Aggregate("", (acc, exp) => acc == "" ? exp : $"{acc}+{exp}");
+                if (subtractedStringRepresentations == "")
+                {
+                    this._stringRepresentation = $"({addedStringRepresentations})";
+                }
+                else
+                {
+                    this._stringRepresentation 
+                        = $"(({addedStringRepresentations})-({subtractedStringRepresentations}))";
+
+                }
+            }
+            return this._stringRepresentation;
+        }
+
+        public List<Expression> GetAddedExpressions() { 
+            return this._addedExpressions.ToList(); 
+        }
+
+        public List<Expression> GetSubtractedExpressions()
+        {
+            return this._subtractedExpressions.ToList();
+        }
+    }
+}
