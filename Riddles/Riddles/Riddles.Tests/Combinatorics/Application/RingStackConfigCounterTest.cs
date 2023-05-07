7cc2db9b4184681dae0b70d2ff393f6df1ce2db1
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2023-05-07 04:53:23 -0400

    Add in first draft of ring counter

---
+++ Riddles/Riddles/Riddles.Tests/Combinatorics/Application/RingStackConfigCounterTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riddles.Tests.Combinatorics.Application
+{
+    internal class RingStackConfigCounterTest
+    {
+    }
+}
