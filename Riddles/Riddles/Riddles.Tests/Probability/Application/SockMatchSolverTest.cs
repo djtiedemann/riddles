50684c8986f03cbdce41e2df81207225a8f3c52d
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2023-05-11 22:17:13 -0400

    Add in code to calculate odds that you match the socks

---
+++ Riddles/Riddles/Riddles.Tests/Probability/Application/SockMatchSolverTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using Riddles.Probability.Application;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riddles.Tests.Probability.Application
+{
+    public class SockMatchSolverTest
+    {
+        private double epsilon = 0.00000000001;
+
+        private Dictionary<int, (int, int, double)> _testCaseDictionary =
+            new Dictionary<int, (int, int, double)> {
+                { 1, (1, 1, 1.0) },
+                { 2, (2, 1, 1.0/3.0) },
+                { 3, (3, 1, (1.0/5.0)*(1.0/3.0)) },
+                { 4, (2, 2, 1.0) },
+                { 5, (3, 2, 1.0/5.0 + 2.0/5.0)},
+                { 6, (14, 9, 0.70048993497640688)}
+        };
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(6)]
+        public void TestCalculateProbabilityOfMatchingSocksSuccessfully(int testCaseId)
+        {
+            var sockMatcherSolver = new SockMatcherSolver();
+            var testCase = this._testCaseDictionary[testCaseId];
+            var actualProbability = sockMatcherSolver
+                .CalculateProbabilityOfMatchingSocksSuccessfully(testCase.Item1, testCase.Item2);
+            Assert.LessOrEqual(Math.Abs(actualProbability - testCase.Item3) / actualProbability,
+                this.epsilon);
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Probability/Application/SockMatcherSolver.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riddles.Probability.Application
+{
+    public class SockMatcherSolver
+    {
+        // (numSocksOnChair, numEmptySpotsOnChair, numReadyToMatchSocks, numUnreadyToMatchSocks)
+        private Dictionary<(int, int, int, int), double> _cache 
+            = new Dictionary<(int, int, int, int), double>();
+
+        public double CalculateProbabilityOfMatchingSocksSuccessfully(
+            int numPairs, 
+            int numSpotsOnChair
+        )
+        {
+            return this.CalculateProbabilityOfMatchingSocksSuccessfully(
+                0,
+                numSpotsOnChair,
+                0,
+                numPairs*2
+            );
+        }
+
+        private double CalculateProbabilityOfMatchingSocksSuccessfully(
+            int numSocksOnChair,
+            int numEmptySpotsOnChair,
+            int numReadyToMatchSocks,
+            int numUnreadyToMatchSocks
+        )
+        {
+            if (!this._cache.ContainsKey((
+                numSocksOnChair, 
+                numEmptySpotsOnChair,
+                numReadyToMatchSocks,
+                numUnreadyToMatchSocks))){
+
+                // if there are no socks to match, you are successful
+                if(numReadyToMatchSocks + numUnreadyToMatchSocks == 0)
+                {
+                    this._cache[(numSocksOnChair,
+                        numEmptySpotsOnChair,
+                        numReadyToMatchSocks,
+                        numUnreadyToMatchSocks)] = 1;
+                }
+                else
+                {
+                    var probabilityOfMatchingASock =
+                        (double)numReadyToMatchSocks /
+                            (numReadyToMatchSocks + numUnreadyToMatchSocks);
+                    var probabilityOfNotMatchingASock =
+                        1 - probabilityOfMatchingASock;
+
+                    var oddsOfWinningIfMatchingASock = 
+                        numSocksOnChair > 0 && numReadyToMatchSocks > 0 ? 
+                            this.CalculateProbabilityOfMatchingSocksSuccessfully(
+                                numSocksOnChair - 1,
+                                numEmptySpotsOnChair + 1,
+                                numReadyToMatchSocks - 1,
+                                numUnreadyToMatchSocks
+                            ) : 0;
+
+                    var oddsOfWinningIfNotMatchingASock =
+                        numEmptySpotsOnChair > 0 && numUnreadyToMatchSocks > 0 ?
+                            this.CalculateProbabilityOfMatchingSocksSuccessfully(
+                                numSocksOnChair + 1,
+                                numEmptySpotsOnChair - 1,
+                                numReadyToMatchSocks + 1,
+                                numUnreadyToMatchSocks - 2
+                            ) : 0;
+
+                    var probability =
+                        probabilityOfMatchingASock * oddsOfWinningIfMatchingASock +
+                        probabilityOfNotMatchingASock * oddsOfWinningIfNotMatchingASock;
+
+                    this._cache[(numSocksOnChair,
+                        numEmptySpotsOnChair,
+                        numReadyToMatchSocks,
+                        numUnreadyToMatchSocks)] = probability;
+                }
+
+            }
+            return this._cache[(numSocksOnChair,
+                numEmptySpotsOnChair,
+                numReadyToMatchSocks,
+                numUnreadyToMatchSocks)];
+        }
+    }
+}
