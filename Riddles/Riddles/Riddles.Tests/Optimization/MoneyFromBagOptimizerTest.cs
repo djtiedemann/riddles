e6e1e8ccb9badb6df0802ae885d62ccd2e0d9d0e
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2023-05-20 11:22:42 -0400

    Add in MoneyFromBagOptimizer

---
+++ Riddles/Riddles/Riddles.Tests/Optimization/MoneyFromBagOptimizerTest.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using Riddles.Optimization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riddles.Tests.Optimization
+{
+    public class MoneyFromBagOptimizerTest
+    {
+        private double epsilon = 0.00000000001;
+
+        //[TestCase(1, 5, 100_000_000)]
+        // forms recurrence x[i] = x[i-1] + (x[i-1]+1)/2
+        public void GetSimulatedNumItemsInBag(int numInitialValues, int numDraws, int numIterations)
+        {
+            var moneyFromBagOptimizer = new MoneyFromBagOptimizer();
+            double sum = 0.0;
+            for(int i=0; i<numIterations; i++)
+            {
+                sum += moneyFromBagOptimizer
+                    .GetSimulatedNumberOfValuesInFinalBag(numInitialValues, numDraws);
+            }
+            var result = sum / numIterations;
+        }
+
+        [TestCase(0, 1.0)]
+        [TestCase(1, 2.0)]
+        [TestCase(2, 3.5)]
+        [TestCase(3, 5.75)]
+        [TestCase(4, 9.125)]
+        [TestCase(5, 14.1875)]
+        [TestCase(6, 21.78125)]
+        [TestCase(7, 33.171875)]
+        [TestCase(8, 50.2578125)]
+        public void TestGetExpectedItemsInBagAfterNAdds(int numAdds, double expected)
+        {
+            var moneyFromBagOptimizer = new MoneyFromBagOptimizer();
+            var actual = moneyFromBagOptimizer.GetExpectedItemsInBagAfterNAdds(numAdds);
+            Assert.LessOrEqual(Math.Abs(expected - actual) / expected, double.Epsilon);
+        }
+
+        [TestCase(1, 1)]
+        [TestCase(2, 2)]
+        [TestCase(3, 3)]
+        [TestCase(4, 4.5)]
+        [TestCase(5, 6.75)]
+        [TestCase(6, 10.125)]
+        [TestCase(7, 15.1875)]
+        [TestCase(8, 22.78125)]
+        [TestCase(9, 34.171875)]
+        [TestCase(10, 51.2578125)]
+        [TestCase(100, 3.613877133646359E+17)]
+        public void TestGetMaximumMoneyFromHat(int numDraws, double expected)
+        {
+            var moneyFromBagOptimizer = new MoneyFromBagOptimizer();
+            var actual = moneyFromBagOptimizer.GetMaximumMoneyFromHat(numDraws);
+            Assert.LessOrEqual(Math.Abs(expected - actual) / expected, double.Epsilon);
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Optimization/MoneyFromBagOptimizer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riddles.Optimization
+{
+    public class MoneyFromBagOptimizer
+    {
+        private Dictionary<int, double> _cache;
+
+        public MoneyFromBagOptimizer() {
+            this._cache = new Dictionary<int, double>();
+        }
+        public double GetSimulatedNumberOfValuesInFinalBag(int initialValuesInBag, int numDraws)
+        {
+            var random = new Random();
+            int numValuesInBag = initialValuesInBag;
+            for(int i=0; i<numDraws; i++)
+            {
+                var draw = random.Next(1, numValuesInBag+1);
+                numValuesInBag += draw;
+            }
+            return (double)numValuesInBag;
+        }
+
+        public double GetMaximumMoneyFromHat(int numRounds)
+        {
+            // populate cache
+            var bestNumAdds = 0;
+            var bestResult = double.MinValue;
+            for(int numAdds = 0; numAdds<=numRounds; numAdds++)
+            {
+                var result = (numRounds - numAdds)
+                    * (this.GetExpectedItemsInBagAfterNAdds(numAdds) + 1)
+                    / 2.0;
+                if(result >= bestResult)
+                {
+                    bestResult = result;
+                    bestNumAdds = numAdds;
+                }
+            }
+            return bestResult;
+        }
+
+        public double GetExpectedItemsInBagAfterNAdds(int numAdds)
+        {
+            if(numAdds <= 0) { return 1; }
+            if (!this._cache.ContainsKey(numAdds))
+            {
+                var numItemsInPreviousIteration = this
+                    .GetExpectedItemsInBagAfterNAdds(numAdds - 1);
+                this._cache[numAdds] = numItemsInPreviousIteration + 
+                    (numItemsInPreviousIteration + 1)/2.0;
+            }
+            return this._cache[numAdds];
+        }
+    }
+}
