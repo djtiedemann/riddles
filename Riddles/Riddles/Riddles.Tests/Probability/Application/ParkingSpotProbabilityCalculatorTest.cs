0c25766af7cb32e1e8089c981ac9acfc6a5d7473
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2023-05-27 22:59:22 -0400

    ParkingSpotProbabilityCalculator

---
+++ Riddles/Riddles/Riddles.Tests/Probability/Application/ParkingSpotProbabilityCalculatorTest.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using Riddles.Probability.Application;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riddles.Tests.Probability.Application
+{
+    public class ParkingSpotProbabilityCalculatorTest
+    {
+        private Dictionary<int, (int, int, int, double)> _testCaseDictionary =
+            new Dictionary<int, (int, int, int, double)> {
+                // no cars
+                { 1, (0, 0, 0, 0) },
+                // equal number of spaces to people arriving
+                { 2, (1, 1, 1, 1.0) },
+                // more spaces than people arriving
+                { 3, (5, 4, 4, 1.0 )},
+                { 4, (10, 4, 5, 1.0 )},
+                // no cars leaving
+                { 5, (2, 0, 2, 1.0) },
+                { 6, (1, 0, 2, 0.5) },
+                { 7, (0, 0, 2, 0.0) },
+                // non-trivial cases
+                { 8, (0, 1, 1, 0.5)},
+                { 9, (0, 2, 1, 2.0/3.0)},
+                { 10, (0, 1, 2, 1.0/3.0)},
+            };
+
+        private double _epsilon = 0.00000000001;
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(6)]
+        [TestCase(7)]
+        [TestCase(8)]
+        [TestCase(9)]
+        [TestCase(10)]
+        public void TestCalculateProbabilityOfFindingSpot(int testCaseId)
+        {
+            var testCase = this._testCaseDictionary[testCaseId];
+            var parkingSpotProbabilityCalculator = new ParkingSpotProbabilityCalculator();
+            var expected = testCase.Item4;
+            var actual = parkingSpotProbabilityCalculator.CalculateProbabilityOfFindingSpot(
+                    testCase.Item1,
+                    testCase.Item2,
+                    testCase.Item3
+                );
+
+            Assert.LessOrEqual(Math.Abs(actual - expected) / expected, this._epsilon);
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Probability/Application/ParkingSpotProbabilityCalculator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riddles.Probability.Application
+{
+    // https://fivethirtyeight.com/features/can-you-game-the-currency-exchange/
+    // Riddler Classic
+    public class ParkingSpotProbabilityCalculator
+    {
+        private Dictionary<(int, int, int), double> _cache; 
+        public ParkingSpotProbabilityCalculator()
+        {
+            this._cache = new Dictionary<(int, int, int), double>();
+        }
+
+        public double CalculateProbabilityOfFindingSpot(
+            int numSpotsAvailable,
+            int numCarsThatWillLeave,
+            int numCarsThatWillArrive
+        )
+        {
+            // if no cars arrive, no one will get a spot
+            if(numCarsThatWillArrive <= 0)
+            {
+                return 0;
+            }
+            var expectedNumberOfSpacesFilledEventually
+                = this.CalculateExpectedNumberOfSpacesFilledEventually(
+                    numSpotsAvailable,
+                    numCarsThatWillLeave,
+                    numCarsThatWillArrive
+                );
+            return expectedNumberOfSpacesFilledEventually / (double)numCarsThatWillArrive;
+        }
+
+        private double CalculateExpectedNumberOfSpacesFilledEventually(
+            int numSpotsAvailable,
+            int numCarsThatWillLeave,
+            int numCarsThatWillArrive
+        )
+        {
+            if (!this._cache.ContainsKey((numSpotsAvailable, numCarsThatWillLeave, numCarsThatWillArrive)))
+            {
+                double cacheValue;
+                if (numSpotsAvailable < 0 || numCarsThatWillArrive < 0 || numCarsThatWillLeave < 0)
+                {
+                    throw new InvalidOperationException("Can't have a negative input value");
+                }
+                else if(numSpotsAvailable >= numCarsThatWillArrive 
+                    || numCarsThatWillLeave == 0)
+                {
+                    cacheValue = Math.Min(numSpotsAvailable, numCarsThatWillArrive);
+                }
+                else
+                {
+                    var numSpotsFilledIfNextCarArrives = 
+                        (numSpotsAvailable > 0 ? 1 : 0);
+                    var expectedNumSpotsIfNextCarArrives =
+                         + numSpotsFilledIfNextCarArrives + 
+                         (numCarsThatWillArrive > 0 ? 
+                        this.CalculateExpectedNumberOfSpacesFilledEventually(
+                           Math.Max(numSpotsAvailable - numSpotsFilledIfNextCarArrives, 0),
+                           numCarsThatWillLeave,
+                           numCarsThatWillArrive - 1
+                        ) : 0);
+                    var expectedNumSpotsIfNextCarLeaves =
+                        (numCarsThatWillLeave > 0 ?
+                            this.CalculateExpectedNumberOfSpacesFilledEventually(
+                                numSpotsAvailable + 1,
+                                numCarsThatWillLeave - 1,
+                                numCarsThatWillArrive
+                            ) : 0
+                        );
+                    var probabilityOfCarArriving = (double)numCarsThatWillArrive / 
+                        ((double)numCarsThatWillArrive + (double)numCarsThatWillLeave);
+                    var probabilityOfCarLeaving = 1 - probabilityOfCarArriving;
+                    cacheValue = probabilityOfCarArriving * expectedNumSpotsIfNextCarArrives
+                        + probabilityOfCarLeaving * expectedNumSpotsIfNextCarLeaves;
+                }
+                this._cache[(numSpotsAvailable, numCarsThatWillLeave, numCarsThatWillArrive)] = cacheValue; ;
+            }
+            return this._cache[(numSpotsAvailable, numCarsThatWillLeave, numCarsThatWillArrive)];
+        }
+    }
+}
