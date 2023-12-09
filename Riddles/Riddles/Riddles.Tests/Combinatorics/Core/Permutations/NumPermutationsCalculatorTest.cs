0b9fff0ce8e74b7706dbef4f15613158ffd290c4
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2023-12-09 11:41:00 -0500

    Add NumPermutationCalculator

---
+++ Riddles/Riddles/Riddles.Tests/Combinatorics/Core/Permutations/NumPermutationsCalculatorTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Riddles.Combinatorics.Core.Permutations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riddles.Tests.Combinatorics.Core.Permutations
+{
+    public class NumPermutationsCalculatorTest
+    {
+        private Dictionary<int, Dictionary<string, int>> _testCaseDictionary = new Dictionary<int, Dictionary<string, int>> {
+            {1, new Dictionary<string, int> { {"H", 3 }, { "T", 4} } },
+            {2, new Dictionary<string, int> { { "0", 2}, { "1", 2}, { "2", 2} } },
+            {3, new Dictionary<string, int> { { "H", 2}, { "T", 1}, } },
+        };
+
+        [TestCase(1, 35)]
+        [TestCase(2, 90)]
+        [TestCase(3, 3)]
+        public void CalculateNumPermutations(int testCase, int expected)
+        {
+            var numPermutationsCalculator = new NumPermutationsCalculator();
+            var actual = numPermutationsCalculator.CalculateNumPermutations(this._testCaseDictionary[testCase]);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Combinatorics/Core/Permutations/NumPermutationsCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Riddles.Combinatorics.Core.Permutations
+{
+    public class NumPermutationsCalculator
+    {
+        private FactorialCalculator _factorialCalculator;
+        public NumPermutationsCalculator() { 
+            this._factorialCalculator = new FactorialCalculator();
+        }
+
+        /// <summary>
+        /// Calculates the number of permutations of the elements provided, taking into account duplicate items
+        /// </summary>
+        /// <returns></returns>
+        public int CalculateNumPermutations(Dictionary<string, int> elements)
+        {
+            var totalNumElements = elements.Values.Sum();
+            var duplicateOrderingCorrection = elements.Values.Where(v => v > 1).ToList().OrderByDescending(x => x).ToList();
+            var factorial = this._factorialCalculator.FactorialDivision(totalNumElements, duplicateOrderingCorrection[0]);
+            for(int i = 1; i<duplicateOrderingCorrection.Count; i++)
+            {
+                factorial = factorial / this._factorialCalculator.Factorial(duplicateOrderingCorrection[i]);
+            }
+            return (int)Math.Round(factorial);
+        }
+    }
+}
