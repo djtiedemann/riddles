17e6071cd60ebdb48b6457c976d94c330c33ba0f
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2023-12-11 22:14:03 -0500

    Add IndistinctBallsInDistinctBinsCalculator

---
+++ Riddles/Riddles/Riddles.Tests/Combinatorics/Core/IndistinctBallsInDistinctBinsCalculatorTest.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using Riddles.Combinatorics.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riddles.Tests.Combinatorics.Core
+{
+    public class IndistinctBallsInDistinctBinsCalculatorTest
+    {
+        private Dictionary<int, (List<char>, int, int)> _testCaseDictionary =
+            new Dictionary<int, (List<char>, int, int)> {
+                {1, (new List<char> { '0' }, 3, 1) },
+                {2, (new List<char> { 'H', 'T' }, 3, 4) },
+                {3, (new List<char> { '0', '1', '2' }, 2, 6) },
+                {4, (new List<char> { '0', '1', '2'}, 3, 10) },
+                {5, (new List<char> { '0', '1', '2'}, 4, 15) },
+                {6, (new List<char> { '0', '1', '2'}, 5, 21) },
+                {7, (new List<char> { '0', '1', '2', '3' }, 2, 10) },
+                {8, (new List<char> { '0', '1', '2', '3' }, 3, 20) },
+                {9, (new List<char> { '0', '1', '2', '3' }, 4, 35) },
+                {10, (new List<char> { '0', '1', '2', '3', '4' }, 2, 15) },
+                {11, (new List<char> { '0', '1', '2', '3', '4' }, 3, 35) },
+            };
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(6)]
+        [TestCase(7)]
+        [TestCase(8)]
+        [TestCase(9)]
+        [TestCase(10)]
+        [TestCase(11)]
+        public void TestDetermineWaysToPlaceBallsInBins(int testCaseId)
+        {
+            (List<char> bins, int numBalls, int expectedNumOutcomes) =
+                this._testCaseDictionary[testCaseId];
+
+            var indistinctBallsInDistinctBinsCalculator =
+                new IndistinctBallsInDistinctBinsCalculator();
+            var actualOutcomes = indistinctBallsInDistinctBinsCalculator
+                .DetermineWaysToPlaceBallsInBins(
+                    numBalls,
+                    bins
+                );
+            var actualNumOutcomes = indistinctBallsInDistinctBinsCalculator
+                .CountNumWaysToPlaceBallsInBins(numBalls, bins.Count);
+            Assert.AreEqual( expectedNumOutcomes, actualOutcomes.Count );
+            Assert.AreEqual(expectedNumOutcomes, actualNumOutcomes);
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Combinatorics/Core/IndistinctBallsInDistinctBinsCalculator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Riddles.Combinatorics.Core
+{
+    public class IndistinctBallsInDistinctBinsCalculator
+    {
+        private Dictionary<(string, int), List<string>> _cache;
+        private Dictionary<(int, int), int> _numWaysCache;
+        public IndistinctBallsInDistinctBinsCalculator() { 
+            this._cache= new Dictionary<(string, int), List<string>>();
+            this._numWaysCache= new Dictionary<(int, int), int>();
+        }
+        
+        public int CountNumWaysToPlaceBallsInBins(int numBalls, int numBins)
+        {
+            if(numBalls == 0 || numBins == 0)
+            {
+                return 0;
+            }
+            // if there is 1 bin, must put all balls in that bin
+            if (numBins == 1)
+            {
+                return 1;
+            }
+            // if there is 1 ball left, can place it in each of the bins
+            if (numBalls == 1)
+            {
+                return numBins;
+            }
+            // two options:
+            // 1) place a ball in the first bin, continue with 1 fewer ball
+            // 2) discard the first bin, continue with 1 fewer bin
+            if(!this._numWaysCache.ContainsKey((numBalls, numBins))) {
+                this._numWaysCache[(numBalls, numBins)] =
+                    this.CountNumWaysToPlaceBallsInBins(numBalls - 1, numBins) +
+                        this.CountNumWaysToPlaceBallsInBins(numBalls, numBins - 1);
+            }
+            return this._numWaysCache[(numBalls, numBins)];
+        }
+
+        public List<string> DetermineWaysToPlaceBallsInBins(
+            int numBalls,
+            List<char> binLabels
+        )
+        {
+            if(numBalls == 0) { 
+                var result = new List<string> { "" };
+                return result;
+            }
+            if (binLabels.Count == 1) {
+                var result = new List<string>
+                {
+                    new string(Enumerable.Range(0, numBalls)
+                            .Select(x => binLabels.Single()).ToArray())
+                };
+                return result;
+            }
+            var placements = new List<string>();
+            for(int i=0; i<=numBalls; i++)
+            {
+                var newBinLabels = binLabels.ToList();
+                var prefix = new string(Enumerable.Range(0, i)
+                    .Select(i => binLabels[0]).ToArray());
+                newBinLabels.RemoveAt(0);
+                var newNumBalls = numBalls - i;
+                var newBinLabelsString = new string(newBinLabels.ToArray());
+                if (!this._cache.ContainsKey((newBinLabelsString, newNumBalls))) {
+                    this._cache[(newBinLabelsString, newNumBalls)] =
+                        this.DetermineWaysToPlaceBallsInBins(
+                            newNumBalls, newBinLabels);
+                }
+                var result = this._cache[(newBinLabelsString, newNumBalls)];
+                placements.AddRange(
+                    result.Select(r => prefix + r)    
+                );
+            }
+            return placements;
+        }
+    }
+}
