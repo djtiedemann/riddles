955db7615c602c9d0b79eb3793cd3aad11953ef8
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2023-12-16 19:33:57 -0500

    Add in IterativeCoinFlipperProbabilityCalculator and tests

---
+++ Riddles/Riddles/Riddles.Tests/Probability/IterativeCoinFlipperProbabilityCalculatorTest.cs
@@ -0,0 +1,90 @@
+﻿using NUnit.Framework;
+using Riddles.Probability;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riddles.Tests.Probability
+{
+    public class IterativeCoinFlipperProbabilityCalculatorTest
+    {
+        double epsilon = 0.000000001;
+
+        private Dictionary<int, Dictionary<char, double>> _testCaseDictionary =
+            new Dictionary<int, Dictionary<char, double>>
+            {
+                { 1, new Dictionary<char, double> {
+                    { 'H', 0.5 },
+                    { 'T', 0.5 }
+                } }
+            };
+
+        private Dictionary<int, Dictionary<string, double>> _outputDictionary =
+            new Dictionary<int, Dictionary<string, double>>
+            {
+                { 1, new Dictionary<string, double>{ 
+                    { "01112223", Math.Pow(3, 6)*5.0*7.0 / Math.Pow(2, 19) } 
+                } },
+            };
+
+        private Dictionary<int, Dictionary<string, double>> _outputTupleDictionary =
+            new Dictionary<int, Dictionary<string, double>>
+            {
+                { 1, new Dictionary<string, double>{
+                    { "4310", 0.17503738403320313 },
+                    { "3311", 0.070896148681640625 }
+                } },
+            };
+
+        [TestCase(1, 3, 8)]
+        public void TestCalculateOddsOfGettingExactOutcome(
+            int testCaseId,
+            int numTrials,
+            int numIterations
+        )
+        {
+            var iterativeCoinFlipperProbabilityCalculator =
+                new IterativeCoinFlipperProbabilityCalculator();
+            var outcomes = iterativeCoinFlipperProbabilityCalculator
+                .CalculateOddsOfGettingExactOutcome(
+                    this._testCaseDictionary[testCaseId],
+                    numTrials,
+                    numIterations
+                );
+            var expectedOutcomes = _outputDictionary[testCaseId];
+            foreach(var expectedOutcome in expectedOutcomes)
+            {
+                Assert.LessOrEqual(
+                    Math.Abs(expectedOutcome.Value - outcomes[expectedOutcome.Key])
+                    , epsilon
+                );
+            }
+        }
+
+        [TestCase(1, 3, 8)]
+        public void TestFindMostLikelyCombinationOfFlips(
+            int testCaseId,
+            int numTrials,
+            int numIterations
+        )
+        {
+            var iterativeCoinFlipperProbabilityCalculator =
+                new IterativeCoinFlipperProbabilityCalculator();
+            var outcomes = iterativeCoinFlipperProbabilityCalculator
+                .CalculateOrderedTupleProbabilities(
+                    this._testCaseDictionary[testCaseId],
+                    numTrials,
+                    numIterations
+                );
+
+            var expectedOutcomes = _outputTupleDictionary[testCaseId];
+            foreach (var expectedOutcome in expectedOutcomes)
+            {
+                Assert.LessOrEqual(
+                    Math.Abs(expectedOutcome.Value - outcomes[expectedOutcome.Key])
+                    , epsilon
+                );
+            }
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Probability/IterativeCoinFlipperProbabilityCalculator.cs
@@ -0,0 +1,88 @@
+﻿using Riddles.Probability.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Riddles.Probability
+{
+    // https://thefiddler.substack.com/p/can-you-flip-the-coins-exactly-as
+    // Answer: https://thefiddler.substack.com/p/can-you-race-around-the-monopoly
+    public class IterativeCoinFlipperProbabilityCalculator
+    {
+        private IndependentTrialOutcomeDistributionCalculator
+            _independentTrialOutcomeDistributionCalculator;
+        public IterativeCoinFlipperProbabilityCalculator()
+        {
+            this._independentTrialOutcomeDistributionCalculator =
+                new IndependentTrialOutcomeDistributionCalculator();
+        }
+
+        public Dictionary<string, double> CalculateOddsOfGettingExactOutcome(
+            Dictionary<char, double> outcomeProbabilities,
+            int numCoinFlips,
+            int numIterationsOfExperiment
+        )
+        {
+            var distribution = this._independentTrialOutcomeDistributionCalculator
+                .CalculateOrderIndependentOutcomeProbabilities(
+                    numCoinFlips,
+                    outcomeProbabilities
+                ).OrderByDescending(x => x)
+                .Select((x, i) => ((i.ToString().ToCharArray().First()), x.Item2))
+                .ToDictionary(x => x.Item1, x => x.Item2);
+
+            var secondDistribution = this._independentTrialOutcomeDistributionCalculator
+                .CalculateOrderIndependentOutcomeProbabilities(
+                    numIterationsOfExperiment,
+                    distribution
+                ).ToDictionary(x => x.Item1, x => x.Item2);
+
+            return secondDistribution;
+        }
+
+        public Dictionary<string, double> CalculateOrderedTupleProbabilities(
+            Dictionary<char, double> outcomeProbabilities,
+            int numCoinFlips,
+            int numIterationsOfExperiment
+        )
+        {
+            var odds = this.CalculateOddsOfGettingExactOutcome(
+                outcomeProbabilities,
+                numCoinFlips,
+                numIterationsOfExperiment
+            );
+            var potentialOutcomes = odds.Keys
+                .SelectMany(x => x.ToCharArray()).Distinct().ToList();
+            var transformedOutcomes = new Dictionary<string, double>();
+            foreach (var outcome in odds.Keys)
+            {
+                var transformedOutcome = 
+                    this.TransformOutcome(outcome, potentialOutcomes);
+                if (transformedOutcomes.ContainsKey(transformedOutcome))
+                {
+                    transformedOutcomes[transformedOutcome] +=
+                        odds[outcome];
+                } else
+                {
+                    transformedOutcomes[transformedOutcome] = odds[outcome];
+                }
+            }
+            return transformedOutcomes;
+        }
+
+        private string TransformOutcome(string outcome, List<char> potentialOutcomes)
+        {
+            var outcomeCharArray = outcome.ToCharArray();
+            List<int> consolidatedOutcomes = new List<int>();
+            foreach(var key in potentialOutcomes)
+            {
+                consolidatedOutcomes.Add(
+                    outcomeCharArray.Where(c => c == key).Count()
+                );
+            }
+            return consolidatedOutcomes.OrderByDescending(c => c)
+                .Aggregate("", (agg, x) => $"{agg}{x}");
+        }
+    }
+}
