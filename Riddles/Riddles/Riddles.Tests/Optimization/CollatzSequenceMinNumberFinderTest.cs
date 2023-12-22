d0fd74be19874f41eb28ffa57d390b45b8a7f592
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2023-12-22 16:18:47 -0500

    Determine if A contains B in its Collatz Sequence

---
+++ Riddles/Riddles/Riddles.Tests/Optimization/CollatzSequenceMinNumberFinderTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Riddles.Optimization;
+
+namespace Riddles.Tests.Optimization
+{
+    public class CollatzSequenceMinNumberFinderTest
+    {
+        [TestCase(2024, 399)]
+        public void TestFindMinNumberWithValueInCollatzSequence(int searchingNumber, int expected)
+        {
+            var collatzSequenceMinNumberFinder = new CollatzSequenceMinNumberFinder();
+            var actual = collatzSequenceMinNumberFinder
+                .FindMinNumberWithValueInCollatzSequence(searchingNumber);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Optimization/CollatzSequenceMinNumberFinder.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Riddles.Optimization
+{
+    public class CollatzSequenceMinNumberFinder
+    {
+        private Dictionary<(int, int), bool> _cache;
+        public CollatzSequenceMinNumberFinder()
+        {
+            this._cache = new Dictionary<(int, int), bool>();
+        }
+
+        public int? FindMinNumberWithValueInCollatzSequence(int searchingValue)
+        {
+            if (searchingValue <= 0)
+            {
+                return null;
+            }
+            if(searchingValue == 1)
+            {
+                return searchingValue;
+            }
+            for(int i=1; i<searchingValue; i++)
+            {
+                if(this.CollatzSequenceContainsNumber(i, searchingValue))
+                {
+                    return i;
+                }
+            }
+            return searchingValue;
+        }
+
+        private bool CollatzSequenceContainsNumber(int currentValue, int searchingValue)
+        {
+            if (!this._cache.ContainsKey((currentValue, searchingValue)))
+            {
+                if (currentValue == searchingValue)
+                {
+                    this._cache[(currentValue, searchingValue)] = true;
+                }
+                else if(currentValue == 1)
+                {
+                    this._cache[(currentValue, searchingValue)] = false;
+                }
+                else if(currentValue % 2 == 0)
+                {
+                    this._cache[(currentValue, searchingValue)] = 
+                        this.CollatzSequenceContainsNumber(currentValue/2, searchingValue);
+                }
+                else
+                {
+                    this._cache[(currentValue, searchingValue)] =
+                        this.CollatzSequenceContainsNumber(currentValue*3 + 1, searchingValue);
+                }
+            }
+            return this._cache[(currentValue, searchingValue)];
+        }
+    }
+}
