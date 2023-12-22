d748d672de7ca5c90c6e8a9d9853e8d51ad0451c
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2023-12-22 16:51:28 -0500

    Pull out Expected Value Calculation from inner Markov Chain Calculation

--- Riddles/Riddles/Riddles/MarkovChains/MarkovChainSolver.cs
+++ Riddles/Riddles/Riddles/MarkovChains/MarkovChainSolver.cs
@@ -20,8 +20,22 @@ namespace Riddles.MarkovChains
 			this._matrixUtilities = new MatrixUtilities();
 		}
 
-		public double GetExpectedValueOfNumTurnsToReachTerminalState<TMarkovChainState, TAdditionalArgs>(TMarkovChainState initialState, 
+        public double GetExpectedValueOfNumTurnsToReachTerminalState<TMarkovChainState, TAdditionalArgs>(TMarkovChainState initialState,
+            Func<TMarkovChainState, TAdditionalArgs, Dictionary<TMarkovChainState, double>> getStateTransitions,
+            TAdditionalArgs additionalArgs = default(TAdditionalArgs))
+            where TMarkovChainState : IMarkovChainState
+        {
+            return this.CalculateMarkovChainValue(
+                initialState,
+                getStateTransitions,
+                this.CalculateExpectedValueOfNumTurnsToReachTerminalState<TMarkovChainState, TAdditionalArgs>,
+                additionalArgs
+            );
+        }
+
+		private double CalculateMarkovChainValue<TMarkovChainState, TAdditionalArgs>(TMarkovChainState initialState, 
 			Func<TMarkovChainState, TAdditionalArgs, Dictionary<TMarkovChainState, double>> getStateTransitions,
+            Func<TMarkovChainState, Dictionary<TMarkovChainState, Dictionary<TMarkovChainState, double>>, Dictionary<TMarkovChainState, int>, double> calculateMarkovChainValueFunc,
 			TAdditionalArgs additionalArgs = default(TAdditionalArgs))
 			where TMarkovChainState : IMarkovChainState
 		{
@@ -58,54 +72,65 @@ namespace Riddles.MarkovChains
 				nextStateId++;
 			}
 
-			// use those variableIds in order to construct the linear equations
-			var equationsToSolve = new List<LinearEquation>();
-			var constants = new List<double>();
-			foreach (var state in stateTransitionDictionary.Keys)
-			{
-				// basically a state transition means that 1 turn after the current state we will be in a set of states probabilistically
-				// this means that the expected value to finish is 1 turn more than the equation formed by each of the states
-				// so we can construct the linear equation:
-				// v_i = 1 + v_a*p_a + v_b*p_b + ... +
-				// where v_i is the state we're looking at and v_a, v_b, ..., are the variables in the state transition
-				// if the state is a terminal state, then we know the number of turns left to complete the game is zero,
-				// so we can hard-code that equation
-				// 
-				// if we bring all the variables to the left hand side and the constant to the right hand side we get
-				// 1) the coefficient for v_i = 1 - p_i (the probability we transition to the same state)
-				// 2) the coefficient for any variable v_a that's in the state transition dictionary is -p_a
-				// 3) the constant coefficient is always 1, expect for the terminal state where it is 0
-				if (state.IsStateTerminalState())
-				{
-					equationsToSolve.Add(new LinearEquation(
-						terms: new List<LinearTerm> { new LinearTerm(coefficient: 1, variableId: termDictionary[state]) },
-						constant: 0
-					));
-					continue;
-				}
-
-				var terms = new List<LinearTerm>();
-				var termForThisState = new LinearTerm(
-					coefficient: 1 - (stateTransitionDictionary[state].ContainsKey(state) ? stateTransitionDictionary[state][state] : 0),
-					variableId: termDictionary[state]
-				);
-				terms.Add(termForThisState);
-
-				foreach (var transitionState in stateTransitionDictionary[state].Keys)
-				{
-					if (!transitionState.Equals(state))
-					{
-						terms.Add(new LinearTerm(
-							coefficient: stateTransitionDictionary[state][transitionState] * -1,
-							variableId: termDictionary[transitionState]
-						));
-					}
-				}
-				equationsToSolve.Add(new LinearEquation(terms: terms, constant: 1));
-			}
-
-			var expectedValuesForEachState = this._matrixUtilities.SolveLinearSystemOfEquations(equationsToSolve);
-			return expectedValuesForEachState[termDictionary[initialState]];
+            return calculateMarkovChainValueFunc(
+                initialState, stateTransitionDictionary, termDictionary   
+            );
 		}
-	}
+
+        private double CalculateExpectedValueOfNumTurnsToReachTerminalState<TMarkovChainState, TAdditionalArgs>(
+            TMarkovChainState initialState,
+            Dictionary<TMarkovChainState, Dictionary<TMarkovChainState, double>> stateTransitionDictionary,
+            Dictionary<TMarkovChainState, int> termDictionary)
+            where TMarkovChainState : IMarkovChainState
+        {
+            // use those variableIds in order to construct the linear equations
+            var equationsToSolve = new List<LinearEquation>();
+            var constants = new List<double>();
+            foreach (var state in stateTransitionDictionary.Keys)
+            {
+                // basically a state transition means that 1 turn after the current state we will be in a set of states probabilistically
+                // this means that the expected value to finish is 1 turn more than the equation formed by each of the states
+                // so we can construct the linear equation:
+                // v_i = 1 + v_a*p_a + v_b*p_b + ... +
+                // where v_i is the state we're looking at and v_a, v_b, ..., are the variables in the state transition
+                // if the state is a terminal state, then we know the number of turns left to complete the game is zero,
+                // so we can hard-code that equation
+                // 
+                // if we bring all the variables to the left hand side and the constant to the right hand side we get
+                // 1) the coefficient for v_i = 1 - p_i (the probability we transition to the same state)
+                // 2) the coefficient for any variable v_a that's in the state transition dictionary is -p_a
+                // 3) the constant coefficient is always 1, expect for the terminal state where it is 0
+                if (state.IsStateTerminalState())
+                {
+                    equationsToSolve.Add(new LinearEquation(
+                        terms: new List<LinearTerm> { new LinearTerm(coefficient: 1, variableId: termDictionary[state]) },
+                        constant: 0
+                    ));
+                    continue;
+                }
+
+                var terms = new List<LinearTerm>();
+                var termForThisState = new LinearTerm(
+                    coefficient: 1 - (stateTransitionDictionary[state].ContainsKey(state) ? stateTransitionDictionary[state][state] : 0),
+                    variableId: termDictionary[state]
+                );
+                terms.Add(termForThisState);
+
+                foreach (var transitionState in stateTransitionDictionary[state].Keys)
+                {
+                    if (!transitionState.Equals(state))
+                    {
+                        terms.Add(new LinearTerm(
+                            coefficient: stateTransitionDictionary[state][transitionState] * -1,
+                            variableId: termDictionary[transitionState]
+                        ));
+                    }
+                }
+                equationsToSolve.Add(new LinearEquation(terms: terms, constant: 1));
+            }
+
+            var expectedValuesForEachState = this._matrixUtilities.SolveLinearSystemOfEquations(equationsToSolve);
+            return expectedValuesForEachState[termDictionary[initialState]];
+        }
+    }
 }
