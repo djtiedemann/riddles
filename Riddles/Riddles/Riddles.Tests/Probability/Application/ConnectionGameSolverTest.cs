97ec1b36ea848fd7078fad4cc7c4284d5e9deb7e
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2023-12-26 14:31:20 -0500

    ConnectionGameSolver

---
+++ Riddles/Riddles/Riddles.Tests/Probability/Application/ConnectionGameSolverTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Riddles.Probability.Application;
+
+namespace Riddles.Tests.Probability.Application
+{
+    public class ConnectionGameSolverTest
+    {
+        private double epsilon = 0.00000000001;
+
+        [TestCase(4, 4, 48.0/455.0)]
+        public void TestCalculateOddsOfGettingOneAwayOnFirstTry(
+            int numGroups, int sizeOfGroup, double odds)
+        {
+            var connectionGameSolver = new ConnectionGameSolver();
+            var actualOdds = connectionGameSolver
+                .CalculateOddsOfGettingOneAwayOnFirstTry(
+                    numGroups,
+                    sizeOfGroup
+                );
+            Assert.LessOrEqual(Math.Abs(actualOdds - odds), epsilon);
+
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Probability/Application/ConnectionGameSolver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Riddles.Combinatorics.Core;
+
+namespace Riddles.Probability.Application
+{
+    public class ConnectionGameSolver
+    {
+        private BinomialTheoremCalculator _binomialTheoremCalculator;
+        public ConnectionGameSolver() { 
+            this._binomialTheoremCalculator = new BinomialTheoremCalculator();
+        }
+
+        public double CalculateOddsOfGettingOneAwayOnFirstTry(int numGroups, int sizeOfGroup)
+        {
+            // in the case of the problem, where there are 4 groups of
+            // size 4. The result is 4*(4 choose 3)*(12 choose 1)/(16 choose 4)
+            return this._binomialTheoremCalculator.CalculateBinomialCoefficient(
+                sizeOfGroup,
+                sizeOfGroup - 1
+            ) * this._binomialTheoremCalculator.CalculateBinomialCoefficient(
+                sizeOfGroup * (numGroups - 1),
+                1
+            ) * numGroups / this._binomialTheoremCalculator.CalculateBinomialCoefficient(
+                numGroups * sizeOfGroup,
+                sizeOfGroup
+            );
+        }
+    }
+}
