84ba184668a991172c6f3598de577f88262162f5
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2023-12-27 17:47:20 -0500

    Add in CityBlockPursuitProbabilitySolver

---
+++ Riddles/Riddles/Riddles.Tests/Probability/Application/CityBlockCarPursuitProbabilitySolverTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Riddles.Probability.Application;
+
+namespace Riddles.Tests.Probability.Application
+{
+    public class CityBlockCarPursuitProbabilitySolverTest
+    {
+        private double epsilon = 0.00000000001;
+
+        private Dictionary<int, (int, int, double, double, double)> _finiteGridTestCaseDictionary
+            = new Dictionary<int, (int, int, double, double, double)>
+            {
+                { 1, (0, 1, 0.25, 0.25, 1.0)},
+                { 2, (0, 0, 0.25, 0.25, 0.0)},
+                { 3, (1, 0, 0.25, 0.25, 0.0)},
+                { 4, (1, 1, 0.25, 0.25, 0.75)},
+                { 5, (2, 1, 0.25, 0.25, 0.625)},
+                { 6, (3, 1, 0.25, 0.25, 0.546875)},
+                { 7, (4, 1, 0.25, 0.25, 63.0/128.0)},
+            };
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(6)]
+        [TestCase(7)]
+        public void TestCalculateOddsOfEscaping(int testCaseId)
+        {
+            var cityBlockCarPursuitProbabilitySolver = new CityBlockCarPursuitProbabilitySolver();
+            var testCase = this._finiteGridTestCaseDictionary[testCaseId];
+            var actualProbability = cityBlockCarPursuitProbabilitySolver.CalculateOddsOfEscaping(
+                testCase.Item1,
+                testCase.Item2,
+                testCase.Item3,
+                testCase.Item4
+            );
+            Assert.LessOrEqual( Math.Abs(actualProbability - testCase.Item5), epsilon );
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Probability/Application/CityBlockCarPursuitProbabilitySolver.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riddles.Probability.Application
+{
+    public class CityBlockCarPursuitProbabilitySolver
+    {
+        private Dictionary<(int, int), double> _cache;
+        public CityBlockCarPursuitProbabilitySolver()
+        {
+            _cache = new Dictionary<(int, int), double>();
+        }
+
+
+        public double CalculateOddsOfEscaping(
+            int numCityBlocks,
+            int sizeOfHeadStart,
+            double oddsOfOutpacing,
+            double oddsOfReceeding
+        )
+        {
+            if (!_cache.ContainsKey((numCityBlocks, sizeOfHeadStart)))
+            {
+                if (sizeOfHeadStart <= 0)
+                {
+                    _cache[(numCityBlocks, sizeOfHeadStart)] = 0.0;
+                }
+                else if (numCityBlocks < sizeOfHeadStart)
+                {
+                    _cache[(numCityBlocks, sizeOfHeadStart)] = 1.0;
+                }
+                else
+                {
+                    _cache[(numCityBlocks, sizeOfHeadStart)] =
+                        oddsOfOutpacing * CalculateOddsOfEscaping(
+                            numCityBlocks - 1,
+                            sizeOfHeadStart + 1,
+                            oddsOfOutpacing,
+                            oddsOfReceeding
+                        )
+                        + (1 - oddsOfOutpacing - oddsOfReceeding) *
+                            CalculateOddsOfEscaping(
+                                numCityBlocks - 1,
+                                sizeOfHeadStart,
+                                oddsOfOutpacing,
+                                oddsOfReceeding
+                            )
+                        + oddsOfReceeding * CalculateOddsOfEscaping(
+                                numCityBlocks - 1,
+                                sizeOfHeadStart - 1,
+                                oddsOfOutpacing,
+                                oddsOfReceeding
+                            );
+                }
+            }
+            return _cache[(numCityBlocks, sizeOfHeadStart)];
+        }
+    }
+}
