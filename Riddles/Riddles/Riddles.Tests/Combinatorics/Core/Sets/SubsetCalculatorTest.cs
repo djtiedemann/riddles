33e6aba73566370d2b577c5c40bedf9965466eac
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2023-12-28 10:00:16 -0500

    Add in SubsetCalculator + Test

--- Riddles/Riddles/Riddles.Tests/Combinatorics/Core/Sets/SubsetCalculatorTest.cs
+++ Riddles/Riddles/Riddles.Tests/Combinatorics/Core/Sets/SubsetCalculatorTest.cs
@@ -9,15 +9,22 @@ namespace Riddles.Tests.Combinatorics.Core.Sets
     public class SubsetCalculatorTest
     {
         private Dictionary<int, (List<int>, int)> _calculateSubsetsTestCaseDictionary = new Dictionary<int, (List<int>, int)> {
-            { 1, (new List<int> { 4, 4 }, 70 )}
+            { 1, (new List<int> { 4, 4 }, 70 )},
+            { 2, (new List<int> { 3, 4 }, 35 )},
+            { 3, (new List<int> { 3, 3 }, 20 )},
+            { 4, (new List<int> { 3, 2 }, 10 )},
         };
 
         [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
         public void TestCalculateSubsets(int testCaseId)
         {
             var subsetCalculator = new SubsetCalculator();
             var testCase = this._calculateSubsetsTestCaseDictionary[testCaseId];
             var allPermutations = subsetCalculator.CalculateSubsets(testCase.Item1);
+            Assert.AreEqual(testCase.Item2, allPermutations.Count);
         }
     }
 }
---
+++ Riddles/Riddles/Riddles/Combinatorics/Core/Sets/SubsetCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Riddles.Combinatorics.Core.Permutations;
+using System.Linq;
+
+namespace Riddles.Combinatorics.Core.Sets
+{
+    public class SubsetCalculator
+    {
+        private PermutationWithoutRepetitionGenerator _permutationGenerator;
+        public SubsetCalculator() { 
+            this._permutationGenerator = new PermutationWithoutRepetitionGenerator();
+        }
+
+        // split the initial members into groups of the following sizes
+        // note: can generate a more flexible outcome with more members than groups
+        // if necessary
+        // could be made to work for more than 10 groups by applying some abstractions
+        // and using lists of ints instead of strings in PermutationGenerator
+        public List<List<List<int>>> CalculateSubsets(
+            List<int> groupSizes)
+        {
+            var elementNums = Enumerable.Range(0, groupSizes.Sum());
+            var initialPermutation = groupSizes.SelectMany((x, i) => 
+                Enumerable.Range(0, x).Select(x => i)).ToArray();
+            var allPermutations = this._permutationGenerator.GenerateAllPermutations(initialPermutation);
+            
+            var groupings = allPermutations.Select(p => 
+                elementNums.GroupBy(x => p[x]).Select(g => g.ToList()).ToList()
+            ).ToList();
+            return groupings;
+        }
+    }
+}
