5258aa19b8dadff78043c9fe0d2e4abb9f8cd974
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2024-01-17 20:48:15 -0500

    Add in D&DDiceRollerCalculator

---
+++ Riddles/Riddles/Riddles.Tests/Probability/Application/DungeonsAndDragonsDiceRollerCalculatorTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Riddles.Probability.Application;
+using NUnit.Framework;
+
+namespace Riddles.Tests.Probability.Application
+{
+    public class DungeonsAndDragonsDiceRollerCalculatorTest
+    {
+        private Dictionary<int, (int, int[], Dictionary<int, double>, double)> testCaseDictionary
+            = new Dictionary<int, (int, int[], Dictionary<int, double>, double)> {
+                { 1, (1, new int[] { 4, 6, 8, 10, 12, 20 }, 
+                    new Dictionary<int, double> { { 1, 1.0 } }, 1.0)},
+                { 2, (2, new int[] { 4, 6, 8, 10, 12, 20 },
+                    new Dictionary<int, double> { 
+                        { 1, 0.093749999999999958 },
+                        { 2, 0.90625 },
+                    }, 1.906249999999999958)},
+                { 3, (3, new int[] { 4, 6, 8, 10, 12, 20 },
+                    new Dictionary<int, double> {
+                        { 1, 0.010530156893004114 },
+                        { 2, 0.24965952932098856 },
+                        { 3, 0.73981031378605544 }
+                    }, 2.729280156893147554 )},
+            };
+
+        private double epsilon = 0.00000000001;
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        public void TestCalculateProbabilitiesOfEachOutcome(int testCaseId)
+        {
+            var testCase = testCaseDictionary[testCaseId];
+            var dungeonsAndDragonsDiceRollerCalculator =
+                new DungeonsAndDragonsDiceRollerCalculator();
+            var (actualDictionary, expectedValue) = dungeonsAndDragonsDiceRollerCalculator
+                .CalculateOddsOfEachCombination(testCase.Item1, testCase.Item2);
+            var expectedDictionary = testCase.Item3;
+            Assert.AreEqual(expectedDictionary.Keys.Count, actualDictionary.Keys.Count);
+            foreach(var key in expectedDictionary.Keys)
+            {
+                Assert.LessOrEqual(
+                    Math.Abs(expectedDictionary[key] - actualDictionary[key]),
+                    epsilon
+                );
+            }
+            Assert.LessOrEqual(Math.Abs(expectedValue - testCase.Item4), epsilon);
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Probability/Application/DungeonsAndDragonsDiceRollerCalculator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Riddles.Probability.Application
+{
+    public class DungeonsAndDragonsDiceRollerCalculator
+    {
+        public (Dictionary<int, double>, double) CalculateOddsOfEachCombination(
+            int numPeople,
+            int[] diceSizes)
+        {
+            var probabilitiesOfEachRoll = new Dictionary<int, double>();
+            foreach (var dice in diceSizes) { 
+                for(int i=1; i<=dice; i++)
+                {
+                    if (!probabilitiesOfEachRoll.ContainsKey(i))
+                    {
+                        probabilitiesOfEachRoll[i] = 0;
+                    }
+                    probabilitiesOfEachRoll[i] += ((double)1 / (dice * diceSizes.Length));
+                }
+            }
+            var cumulativeProbability = Enumerable.Range(1, numPeople)
+                .ToDictionary(k => k, k => 0.0);
+            var currentCounts = probabilitiesOfEachRoll.Keys
+                .ToDictionary(k => k, k => 0);
+            this.CalculateProbabilitiesOfEachOutcome(
+                probabilitiesOfEachRoll,
+                numPeople,
+                currentCounts,
+                1.0,
+                cumulativeProbability
+            );
+            var expectedValue = cumulativeProbability.Sum(
+                x => x.Key * x.Value);
+            return (cumulativeProbability, expectedValue);
+        }
+
+        private void CalculateProbabilitiesOfEachOutcome(
+            Dictionary<int, double> distribution, 
+            int numPeople,
+            Dictionary<int, int> currentCounts, 
+            double probabilityOfCurrentOutcome,
+            Dictionary<int, double> cumulativeProbability)
+        {
+            if(numPeople == 0)
+            {
+                cumulativeProbability[currentCounts.Values.Where(v => v > 0).Count()] +=
+                    probabilityOfCurrentOutcome;
+                return;
+            }
+            foreach(var outcome in distribution)
+            {
+                currentCounts[outcome.Key] += 1;
+                this.CalculateProbabilitiesOfEachOutcome(
+                    distribution,
+                    numPeople - 1,
+                    currentCounts,
+                    probabilityOfCurrentOutcome * outcome.Value,
+                    cumulativeProbability
+                );
+                currentCounts[outcome.Key] -= 1;
+            }
+        } 
+    }
+}
