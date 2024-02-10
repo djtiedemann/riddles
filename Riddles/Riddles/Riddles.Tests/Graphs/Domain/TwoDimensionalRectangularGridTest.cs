85e3f9f23c07dbe8a419c4f668d82361f1f09b46
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2024-02-09 21:09:50 -0500

    Add in code to generate a 2D grid

---
+++ Riddles/Riddles/Riddles.Tests/Graphs/Domain/TwoDimensionalRectangularGridTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Riddles.Graphs.Domain;
+
+namespace Riddles.Tests.Graphs.Domain
+{
+    public class TwoDimensionalRectangularGridTest
+    {
+        [Test]
+        public void TestGenerate4x4Grid()
+        {
+            var twoDimensionalRectangularGrid = new TwoDimensionalRectangularGrid();
+            var grid = twoDimensionalRectangularGrid.GenerateGrid(4, 4)
+                .OrderBy(x => x.Id)
+                .ToList();
+            Assert.AreEqual(grid.Count, 16);
+            Assert.AreEqual(grid[11].Id, 11);
+            Assert.AreEqual(grid[6].Id, 6);
+            var square3Neighbors = grid[3].GetAdjacentLocations()
+                .OrderBy(x => x.Id)
+                .ToList();
+            Assert.AreEqual(square3Neighbors.Count, 2);
+            Assert.AreEqual(square3Neighbors[0].Id, 2);
+            Assert.AreEqual(square3Neighbors[1].Id, 7);
+            var square5Neighbors = grid[5].GetAdjacentLocations()
+                .OrderBy(x => x.Id)
+                .ToList();
+            Assert.AreEqual(square5Neighbors.Count, 4);
+            Assert.AreEqual(square5Neighbors[0].Id, 1);
+            Assert.AreEqual(square5Neighbors[1].Id, 4);
+            Assert.AreEqual(square5Neighbors[2].Id, 6);
+            Assert.AreEqual(square5Neighbors[3].Id, 9);
+            var square13Neighbors = grid[13].GetAdjacentLocations()
+                .OrderBy(x => x.Id)
+                .ToList();
+            Assert.AreEqual(square13Neighbors.Count, 3);
+            Assert.AreEqual(square13Neighbors[0].Id, 9);
+            Assert.AreEqual(square13Neighbors[1].Id, 12);
+            Assert.AreEqual(square13Neighbors[2].Id, 14);
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Graphs/Domain/TwoDimensionalRectangularGrid.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Riddles.Graphs.Domain
+{
+    public class TwoDimensionalRectangularGrid
+    {
+        public List<Location> GenerateGrid(int length, int width)
+        {
+            return Enumerable.Range(0, length * width)
+                .Select(x => new Location(id: x, length: length, width: width))
+                .OrderBy(x => x.Id)
+                .ToList();
+        }
+
+        public class Location
+        {
+            private List<Location> _adjacentLocations;
+            private int _length;
+            private int _width;
+            public Location(int x, int y, int length, int width)
+            {
+                this.X = x;
+                this.Y = y;
+                this._length = length;
+                this._width = width;
+                this.Id = x * width + y;
+            }
+
+            public Location(int id, int length, int width)
+            {
+                this.X = id / width;
+                this.Y = id % width;
+                this._length = length;
+                this._width = width;
+                this.Id = id;
+            }
+            public int X { get; }
+
+            public int Y { get; }
+
+            public int Id { get; }
+
+            public override bool Equals(object obj)
+            {
+                if (!(obj is Location))
+                {
+                    return false;
+                }
+                Location other = (Location)obj;
+                return other.X == this.X && other.Y == this.Y;
+            }
+
+            public override int GetHashCode()
+            {
+                return $"{this.X}_{this.Y}".GetHashCode();
+            }
+
+            public List<Location> GetAdjacentLocations()
+            {
+                if (this._adjacentLocations == null)
+                {
+                    var directions = new List<(int, int)> {
+                        (-1, 0),
+                        (0, -1),
+                        (1, 0),
+                        (0, 1),
+                    };
+                    this._adjacentLocations = directions.Select(
+                        d => (this.X + d.Item1, this.Y + d.Item2)
+                    ).Where(l =>
+                        l.Item1 >= 0
+                        && l.Item2 >= 0
+                        && l.Item1 < this._length
+                        && l.Item2 < this._width
+                    ).Select(l =>
+                        new Location(l.Item1, l.Item2, this._length, this._width)
+                    ).ToList();
+                }
+                return this._adjacentLocations;
+            }
+        }
+    }
+}
