357cb55abbd60717f45059b6cdcef5eb32778877
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2024-02-18 21:31:41 -0500

    Add in Card Slapping Solver

---
+++ Riddles/Riddles/Riddles.Tests/Optimization/CardSlappingSolverTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using Riddles.Optimization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Riddles.Tests.Optimization
+{
+    public class CardSlappingSolverTest
+    {
+
+        [TestCase(4, 9)]
+        [TestCase(5, 15)]
+        [TestCase(6, 21)]
+        //[TestCase(7, 31)] // commented out, takes 10s
+        public void TestFindLongestSequenceThatIsNeverSlappable(int numCards, int expected)
+        {
+            var cardSlappingSolver = new CardSlappingSolver();
+            var longestSequence = cardSlappingSolver.FindLongestSequenceThatIsNeverSlappable(numCards);
+            Assert.AreEqual(expected, longestSequence.Count);
+        }
+
+        [TestCase(new int[] { 3, 2, 3, 4, 1 }, true)]
+        [TestCase(new int[] { 1, 2, 4, 3, 3 }, true)]
+        [TestCase(new int[] { 2, 3, 1, 2 }, true)]
+        [TestCase(new int[] { 2, 3 }, false)]
+        [TestCase(new int[] { 2, 2 }, true)]
+        [TestCase(new int[] { 2, 2, 3 }, false)]
+        [TestCase(new int[] { 3, 2, 2 }, true)]
+        [TestCase(new int[] { 3, 2, 2, 4, 3 }, true)]
+        [TestCase(new int[] { 1, 2, 3, 4, 3 }, false)]
+        public void TestCanSequenceBeSlapped(int[] testSequence, bool expected)
+        {
+            var cardSlappingSolver = new CardSlappingSolver();
+            var actual = cardSlappingSolver.CanSequenceBeSlapped(testSequence.ToList());
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Optimization/CardSlappingSolver.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Riddles.Optimization
+{
+    /// <summary>
+    /// https://thefiddler.substack.com/p/can-you-sum-some-cards
+    /// You are playing a game with cards from 1 to n. You have infinitely many of each
+    /// If at any point a sequence of cards has the same sum as a sequence immediately
+    /// before it, you can slap it.
+    /// 
+    /// What is the largest sequence of cards that could never be slapped
+    /// </summary>
+    public class CardSlappingSolver
+    {
+        public CardSlappingSolver()
+        {
+
+        }
+
+        public List<int> FindLongestSequenceThatIsNeverSlappable(int numCards)
+        {
+            var currentSequence = new List<int>();
+            var longestSequence = new List<int>();
+            var finalLongestSequence = this.FindLongestSequenceThatIsNeverSlappable(
+                numCards,
+                currentSequence,
+                longestSequence
+            );
+            finalLongestSequence.RemoveAt(finalLongestSequence.Count - 1);
+            return finalLongestSequence;
+        }
+
+        private List<int> FindLongestSequenceThatIsNeverSlappable(
+            int numCards,
+            List<int> currentSequence,
+            List<int> currentLongestSequence)
+        {
+            if(this.CanSequenceBeSlapped(currentSequence))
+            {
+                if(currentSequence.Count > currentLongestSequence.Count)
+                {
+                    currentLongestSequence = currentSequence.ToList();
+                }
+                return currentLongestSequence;
+            }
+            for(int i=1; i<=numCards; i++)
+            {
+                currentSequence.Add(i);
+                var potentialLongestSequence = 
+                    this.FindLongestSequenceThatIsNeverSlappable(
+                        numCards,
+                        currentSequence,
+                        currentLongestSequence
+                    );
+                if(potentialLongestSequence.Count > currentLongestSequence.Count)
+                {
+                    currentLongestSequence = potentialLongestSequence;
+                }
+                currentSequence.RemoveAt(currentSequence.Count-1);
+            }
+            return currentLongestSequence;
+        }
+
+        /**
+         * The sequence can be slapped if any group of recent cards
+         * sums to a group of cards played immediately before it.
+         * 
+         * Cards are played to the end of the list
+         */
+        public bool CanSequenceBeSlapped(List<int> stackOfCards)
+        {
+            var sumOfRecentCards = 0;
+            for(int i=stackOfCards.Count-1; i >= 0; i--)
+            {
+                sumOfRecentCards += stackOfCards[i];
+                var sumOfOtherCards = 0;
+                for(int j=i-1; j >= 0; j--)
+                {
+                    sumOfOtherCards += stackOfCards[j];
+                    // if the sums are equal, then we found the grouping, return true
+                    if(sumOfRecentCards == sumOfOtherCards)
+                    {
+                        return true;
+                    }
+                    // if the j group of cards is greater than the i group,
+                    // adding more cards won't help. break out of loop and increase i
+                    if(sumOfOtherCards > sumOfRecentCards)
+                    {
+                        break;
+                    }
+                    // if the j group of cards is less than the i group and we've
+                    // run out of cards. Return false, adding cards to the i group
+                    // won't help
+                    if(j == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
