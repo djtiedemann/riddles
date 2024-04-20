8a4ec6a8122e875d91adbe3f4452ae0a7a30801a
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2024-04-19 21:30:32 -0400

    Add in CollaborativeCardFlippingGameSolver

---
+++ Riddles/Riddles/Riddles.Tests/Probability/Application/CollaborativeCardFlippingGameSolverTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Riddles.Probability.Application;
+
+namespace Riddles.Tests.Probability.Application
+{
+    internal class CollaborativeCardFlippingGameSolverTest
+    {
+        private double epsilon = 0.00000000001;
+
+        [TestCase(1, 0.0)]
+        [TestCase(2, 0.5)]
+        [TestCase(3, 1.0/3.0)]
+        [TestCase(4, 3.0/8.0)]
+        [TestCase(5, 11.0/30.0)]
+        [TestCase(52, 0.36787944117144217)]
+        public void TestCalculateProbabilityOfWinning(int numCards, double expected)
+        {
+            var collaborativeCardFlippingGameSolver = new CollaborativeCardFlippingGameSolver();
+            var actual = collaborativeCardFlippingGameSolver.CalculateProbabilityOfWinning(numCards);
+            Assert.LessOrEqual(Math.Abs(actual - expected), epsilon);
+        }
+
+        //[Test]
+        public void VerifyThroughSimulations()
+        {
+            int numWins = 0;
+            var numTries = 1_000_000;
+            for(int i=0; i< numTries; i++)
+            {
+                var random = new Random();
+                var sequence1 = Enumerable.Range(0, 52).ToList();
+                var sequence2 = Enumerable.Range(0, 52).ToList();
+                sequence1 = sequence1.OrderBy(_ => random.Next()).ToList();
+                sequence2 = sequence1.OrderBy(_ => random.Next()).ToList();
+                bool won = true;
+                for(int j=0; j<sequence1.Count(); j++)
+                {
+                    if (sequence1[j] == sequence2[j])
+                    {
+                        won = false;
+                        break;
+                    }
+                }
+                if (won)
+                {
+                    numWins++;
+                }
+            }
+            var winRate = (double)numWins / numTries;
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Probability/Application/CollaborativeCardFlippingGameSolver.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riddles.Probability.Application
+{
+    public class CollaborativeCardFlippingGameSolver
+    {
+        private Dictionary<(int, int), double> _cache;
+        public CollaborativeCardFlippingGameSolver() { 
+            this._cache = new Dictionary<(int, int), double>();
+        }
+
+        public double CalculateProbabilityOfWinning(int numCardsInEachDeck)
+        {
+            return this.CalculateProbabilityOfWinning(
+                numCardsInEachDeck,
+                0
+            );
+        }
+
+        private double CalculateProbabilityOfWinning(
+            int numUniqueCardsInEachDeck, 
+            int numSharedCardsInEachDeck)
+        {
+            if(numUniqueCardsInEachDeck < 0 || numSharedCardsInEachDeck < 0)
+            {
+                return 0;
+            }
+            if (numUniqueCardsInEachDeck == 0)
+            {
+                return 1.0;
+            }
+            if (numSharedCardsInEachDeck == 0 && numUniqueCardsInEachDeck == 1)
+            {
+                return 0.0;
+            }
+            if (!this._cache.ContainsKey(
+                (numUniqueCardsInEachDeck, numSharedCardsInEachDeck)
+            ))
+            {
+                var numCardsInEachDeck = numUniqueCardsInEachDeck +
+                    numSharedCardsInEachDeck;
+                // you could each draw a shared card
+                var oddsOfBothDrawingSharedCard = 
+                    (double)numSharedCardsInEachDeck / numCardsInEachDeck
+                    * numSharedCardsInEachDeck / numCardsInEachDeck;
+                var pWinBothShared = this.CalculateProbabilityOfWinning(
+                    numUniqueCardsInEachDeck,
+                    numSharedCardsInEachDeck - 1
+                );
+
+                // one of you could draw a unique card and the other could
+                // draw a shared card
+                var oddsOfDrawingOneUniqueAndOneSharedCard =
+                    ((double)numSharedCardsInEachDeck / numCardsInEachDeck
+                    * numUniqueCardsInEachDeck / numCardsInEachDeck) * 2;
+                var pWinOneUniqueOneShared = this.CalculateProbabilityOfWinning(
+                    numUniqueCardsInEachDeck - 1,
+                    numSharedCardsInEachDeck
+                );
+                // you could each draw a non-matching unique card
+                var oddsOfBothDrawingUniqueNonMatchingCard =
+                    (double)numUniqueCardsInEachDeck / numCardsInEachDeck 
+                    * (numUniqueCardsInEachDeck - 1) / numCardsInEachDeck;
+                var pWinBothUnique = this.CalculateProbabilityOfWinning(
+                    numUniqueCardsInEachDeck - 2,
+                    numSharedCardsInEachDeck + 1
+                );
+
+                // you could each draw the same card and lose
+                var oddsOfImmediatelyLosing = 
+                    (double)numUniqueCardsInEachDeck /(numCardsInEachDeck)
+                    * 1 / numCardsInEachDeck;
+
+                this._cache[(numUniqueCardsInEachDeck, numSharedCardsInEachDeck)]
+                    = oddsOfBothDrawingSharedCard * pWinBothShared
+                    + oddsOfDrawingOneUniqueAndOneSharedCard * pWinOneUniqueOneShared
+                    + oddsOfBothDrawingUniqueNonMatchingCard * pWinBothUnique;
+            }
+            return this._cache[(numUniqueCardsInEachDeck, numSharedCardsInEachDeck)]; 
+        }
+    }
+}
