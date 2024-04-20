f1db0c05fc1f9b878f86d8d25fea33352b2d0ecc
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2024-04-19 22:38:07 -0400

    Add in utility to split a shuffled deck into two decks and see the distribution

---
+++ Riddles/Riddles/Riddles.Tests/Probability/Application/DuplicateDeckShufflerAndDivider.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Riddles.Combinatorics.Core;
+
+namespace Riddles.Tests.Probability.Application
+{
+    /// <summary>
+    /// The idea here is that we take two identical decks, shuffle them
+    /// and deal them out to two players.
+    /// 
+    /// Calculate the distribution of how many cards are shared by each player
+    /// and how many pairs of cards each player has that is unique.
+    /// </summary>
+    public class DuplicateDeckShufflerAndDivider
+    {
+        private FactorialCalculator _factorialCalculator;
+        private BinomialTheoremCalculator _binomialTheoremCalculator;
+        public DuplicateDeckShufflerAndDivider()
+        {
+            this._factorialCalculator = new FactorialCalculator();
+            this._binomialTheoremCalculator = new BinomialTheoremCalculator();
+        }
+
+        public List<((int, int), double)> CalculateDistributionsWithProbability(
+            int numCardsInEachDeck)
+        {
+            var probabilities = new List<((int, int), double)> ();
+            var numPossibleGroupings = this._binomialTheoremCalculator
+                .CalculateBinomialCoefficient(numCardsInEachDeck * 2, numCardsInEachDeck);
+            // iterate through the number of pairs that each person is holding
+            // if numUniquePairs = 0, they each have the same deck
+            // if numUniquePairs = numCardsInEachDeck/2, they have no overlapping
+            // cards
+            for(int numUniquePairs=0; numUniquePairs <= numCardsInEachDeck/2; numUniquePairs ++)
+            {
+                var numSharedCards = numCardsInEachDeck - numUniquePairs * 2;
+                var numWaysToGenerateThisCombination =
+                    // first find the number of ways to pick the cards that will be shared
+                    this._binomialTheoremCalculator.CalculateBinomialCoefficient(
+                        numCardsInEachDeck,
+                        numSharedCards
+                    ) * 
+                    // for each shared card, their are 2 copies and each could go in each
+                    // deck
+                    Math.Pow(2, numSharedCards) *
+                    // for each pair, find the number of ways to split the 
+                    // pairs into each hand
+                    this._binomialTheoremCalculator.CalculateBinomialCoefficient(
+                        numUniquePairs * 2,
+                        numUniquePairs
+                    );
+                var probability = (double)numWaysToGenerateThisCombination /
+                    (double)numPossibleGroupings;
+                probabilities.Add(((numSharedCards, numUniquePairs * 2), probability));
+            }
+            var cumulativeProbability = probabilities.Sum(x => x.Item2);
+            return probabilities;
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles.Tests/Probability/Application/DuplicateDeckShufflerAndDividerTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Riddles.Probability.Core;
+
+namespace Riddles.Tests.Probability.Application
+{
+    public class DuplicateDeckShufflerAndDividerTest
+    {
+        private double epsilon = 0.00000000001;
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(10)]
+        [TestCase(11)]
+        [TestCase(52)]
+        public void TestCalculateDistributionsWithProbability(int numCards)
+        {
+            var duplicateDeckShufflerAndDivider =
+                new DuplicateDeckShufflerAndDivider();
+            var result = duplicateDeckShufflerAndDivider
+                .CalculateDistributionsWithProbability(numCards);
+            var cumulativeProbability = result.Sum(r => r.Item2);
+            Assert.LessOrEqual(Math.Abs(cumulativeProbability - 1), epsilon);
+        }
+    }
+}
