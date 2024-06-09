a2af29cc5c6c4fefad8d62fbd1a2e2af36eadbe9
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2024-06-09 11:46:27 -0400

    Add in Bernoulli Trial Calculator and Tests

---
+++ Riddles/Riddles/Riddles.Tests/Probability/Core/BernoulliTrialOutcomeDistributionCalculatorTest.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using Riddles.Probability.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riddles.Tests.Probability.Core
+{
+    public class BernoulliTrialOutcomeDistributionCalculatorTest
+    {
+        private double epsilon = 0.00000000001;
+
+        private Dictionary<int, (int, int, double, double)>
+            _testCaseDictionary = new Dictionary<int, (int, int, double, double)> {
+                { 1, (4, 0, 0.5, 1.0 )},
+                { 2, (4, 1, 0.5, 15.0/16.0 )},
+                { 3, (4, 2, 0.5, 11.0/16.0 )},
+                { 4, (4, 3, 0.5, 5.0/16.0 )},
+                { 5, (4, 4, 0.5, 1.0/16.0 )},
+                { 6, (4, 5, 0.5, 0.0/16.0 )},
+                { 7, (4, 0, 0.75, 1.0 )},
+                { 8, (4, 1, 0.75, 255.0/256.0 )},
+                { 9, (4, 2, 0.75, 243.0/256.0 )},
+                { 10, (4, 3, 0.75, 189.0/256.0 )},
+                { 11, (4, 4, 0.75, 81.0/256.0 )},
+                { 12, (4, 5, 0.75, 0.0/256.0 )},
+                { 13, (4, 0, 25, 1.0 )},
+                { 14, (4, 1, 0.25, 175.0/256.0 )},
+                { 15, (4, 2, 0.25, 67.0/256.0 )},
+                { 16, (4, 3, 0.25, 13.0/256.0 )},
+                { 17, (4, 4, 0.25, 1.0/256.0 )},
+                { 18, (4, 5, 0.25, 0.0/256.0 )},
+        };
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(6)]
+        [TestCase(7)]
+        [TestCase(8)]
+        [TestCase(9)]
+        [TestCase(10)]
+        [TestCase(11)]
+        [TestCase(12)]
+        [TestCase(13)]
+        [TestCase(14)]
+        [TestCase(15)]
+        [TestCase(16)]
+        [TestCase(17)]
+        [TestCase(18)]
+        public void TestCalculateOddsOfAnOutcomeOfAtLeastKSuccesses(int testCaseId)
+        {
+            var testCase = this._testCaseDictionary[testCaseId];
+            var bernoulliTrialOutcomeDistributionCalculator =
+                new BernoulliTrialOutcomeDistributionCalculator();
+            var actualProbability = bernoulliTrialOutcomeDistributionCalculator
+                .CalculateOddsOfAnOutcomeOfAtLeastKSuccesses(
+                    testCase.Item1,
+                    testCase.Item2,
+                    testCase.Item3
+                );
+            Assert.LessOrEqual(Math.Abs(actualProbability - testCase.Item4),
+                this.epsilon);
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Probability/Core/BernoulliTrialOutcomeDistributionCalculator.cs
@@ -0,0 +1,77 @@
+﻿using Riddles.Combinatorics.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riddles.Probability.Core
+{
+    public class BernoulliTrialOutcomeDistributionCalculator
+    {
+        private bool _useCache;
+        private Dictionary<(int, int, double), double> _cache;
+        private BinomialTheoremCalculator _binomialTheoremCalculator;
+        public BernoulliTrialOutcomeDistributionCalculator(
+            bool useCache=true
+        )
+        {
+            this._useCache = useCache;
+            this._cache = new Dictionary<(int, int, double), double>();
+            this._binomialTheoremCalculator = new BinomialTheoremCalculator();
+        }
+
+        public double CalculateOddsOfAnOutcomeOfAtLeastKSuccesses(
+            int numTrials,
+            int numSuccesses,
+            double probabilityOfSuccess)
+        {
+            if(!this._useCache || !this._cache.ContainsKey(
+                (numTrials, numSuccesses, probabilityOfSuccess)))
+            {
+                // input sanitzation
+                if(numTrials < 0 || numSuccesses < 0 
+                    || numSuccesses > numTrials)
+                {
+                    return 0.0;
+                }
+                // for efficiency - if the number of successes requires
+                // iterating over more than half of the outcomes,
+                // reverse the search. ask for the probability of failures
+                // and reverse the outcome.
+                var shouldCheckForFailures
+                    = numSuccesses < ((double)numTrials / 2);
+                if (shouldCheckForFailures)
+                {
+                    numSuccesses = numTrials - numSuccesses + 1;
+                    probabilityOfSuccess = 1 - probabilityOfSuccess;
+                }
+                // find the probability of exactly k successes
+                var probabilityOfExactNumberOfSuccesses 
+                    = this._binomialTheoremCalculator.CalculateBinomialCoefficient(
+                            numTrials,
+                            numSuccesses
+                        ) * Math.Pow(probabilityOfSuccess, numSuccesses)
+                        * Math.Pow((1 - probabilityOfSuccess), numTrials - numSuccesses);
+                // find the probability of more than k successes
+                var probabilityOfMoreSuccesses =
+                    this.CalculateOddsOfAnOutcomeOfAtLeastKSuccesses(
+                        numTrials,
+                        numSuccesses + 1,
+                        probabilityOfSuccess
+                    );
+                var cumulativeProbability = 
+                    probabilityOfExactNumberOfSuccesses + probabilityOfMoreSuccesses;
+                if (shouldCheckForFailures)
+                {
+                    cumulativeProbability = 1 - cumulativeProbability;
+                }
+                if (!this._useCache)
+                {
+                    return cumulativeProbability;
+                }
+                this._cache[(numTrials, numSuccesses, probabilityOfSuccess)]
+                    = cumulativeProbability;
+            }
+            return this._cache[(numTrials, numSuccesses, probabilityOfSuccess)];        
+        }
+    }
+}
