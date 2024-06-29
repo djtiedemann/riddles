cd08b39858bab6d9ff6adf33e98467694e50b074
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2024-06-28 23:11:21 -0400

    Add in TiledSquareTilerSolver and Test

---
+++ Riddles/Riddles/Riddles.Tests/Combinatorics/Application/TiltedSquareTilerTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Riddles.Combinatorics.Application;
+
+namespace Riddles.Tests.Combinatorics.Application
+{
+    public class TiltedSquareTilerTest
+    {
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(2, 2)]
+        [TestCase(3, 5)]
+        [TestCase(4, 14)]
+        [TestCase(5, 42)]
+        [TestCase(6, 132)]
+        [TestCase(7, 429)]
+        public void TestCountNumberOfWaysToTileSteps(int numSteps, float expected)
+        {
+            var tiltedSquareTiler = new TiltedSquareTiler();
+            var actual = tiltedSquareTiler.CountNumberOfUniqueWaysToTileSteps(numSteps);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(2, 2)]
+        [TestCase(3, 7)]
+        [TestCase(4, 38)]
+        [TestCase(5, 274)]
+        [TestCase(6, 2_350)]
+        [TestCase(7, 22_531)]
+        public void TestCountNumberOfWaysToTilePyramid(int height, float expected)
+        {
+            var tiltedSquareTiler = new TiltedSquareTiler();
+            var actual = tiltedSquareTiler
+                .CountNumberOfUniqueWaysToTilePyramid(height);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(2, 2)]
+        [TestCase(3, 9)]
+        [TestCase(4, 106)]
+        [TestCase(5, 3_002)]
+        [TestCase(6, 153_432)]
+        [TestCase(7, 11_209_105)]
+        public void TestCountNumberOfWaysToTileTiltedSquare(
+            int distanceFromOrigin, float expected)
+        {
+            var tiltedSquareTiler = new TiltedSquareTiler();
+            var actual = tiltedSquareTiler
+                .CountNumberOfUniqueWaysToTileTiltedSquare(distanceFromOrigin);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Combinatorics/Application/TiltedSquareTiler.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Riddles.Combinatorics.Application
+{
+    // https://thefiddler.substack.com/p/how-many-ways-can-you-tile-the-tilted
+    public class TiltedSquareTiler
+    {
+        private Dictionary<int, float> _numWaysToTileStepsCache;
+        private Dictionary<int, float> _numWaysToTilePyramidCache;
+        private Dictionary<int, float> _numWaysToTileTiltedSquareCache;
+        public TiltedSquareTiler()
+        {
+            this._numWaysToTileStepsCache = new Dictionary<int, float>();
+            this._numWaysToTilePyramidCache = new Dictionary<int, float>();
+            this._numWaysToTileTiltedSquareCache = new Dictionary<int, float>();
+        }
+
+        public float CountNumberOfUniqueWaysToTileTiltedSquare(int distanceFromOrigin)
+        {
+            if (!this._numWaysToTileTiltedSquareCache
+                .ContainsKey(distanceFromOrigin))
+            {
+                if(distanceFromOrigin <= 0)
+                {
+                    this._numWaysToTileTiltedSquareCache[distanceFromOrigin] 
+                        = 0;
+                }
+                else if(distanceFromOrigin == 1)
+                {
+                    this._numWaysToTileTiltedSquareCache[distanceFromOrigin]
+                        = 1;
+                }
+                else
+                {
+                    var numWaysToTile = 2 * 
+                        this.CountNumberOfUniqueWaysToTilePyramid(
+                            distanceFromOrigin - 1
+                        ) * this.CountNumberOfUniqueWaysToTilePyramid(
+                            distanceFromOrigin - 1
+                        );
+                    for(int i=1; i<distanceFromOrigin - 1; i++)
+                    {
+                        numWaysToTile +=
+                            this.CountNumberOfUniqueWaysToTilePyramid(
+                                i
+                            ) *
+                            this.CountNumberOfUniqueWaysToTilePyramid(
+                                i
+                            ) *
+                            this.CountNumberOfUniqueWaysToTilePyramid(
+                                distanceFromOrigin - 1 - i
+                            ) *
+                            this.CountNumberOfUniqueWaysToTilePyramid(
+                                distanceFromOrigin - 1 - i
+                            );
+                    }
+                    this._numWaysToTileTiltedSquareCache[distanceFromOrigin] = 
+                        numWaysToTile;
+                }
+            }
+            return this._numWaysToTileTiltedSquareCache[distanceFromOrigin];   
+        }
+
+        public float CountNumberOfUniqueWaysToTilePyramid(int height)
+        {
+            if (!this._numWaysToTilePyramidCache.ContainsKey(height))
+            {
+                if(height <= 0)
+                {
+                    this._numWaysToTilePyramidCache[height] = 0;
+                }
+                else if (height == 1)
+                {
+                    this._numWaysToTilePyramidCache[height] = 1;
+                }
+                else
+                {
+                    // if you pick the middle of the pyramid
+                    // you split it into two sets of steps
+                    float numWaysToTilePyramid = 
+                        this.CountNumberOfUniqueWaysToTileSteps(height - 1)
+                        * this.CountNumberOfUniqueWaysToTileSteps(height - 1);
+
+                    // if you pick the bottom row, then it's a smaller
+                    // pyramid
+                    numWaysToTilePyramid +=
+                        this.CountNumberOfUniqueWaysToTilePyramid(height - 1);
+
+                    // if you pick anything else, it's 2 steps and 1 pyramid
+                    for(int row = 1; row < height - 1; row++)
+                    {
+                        numWaysToTilePyramid +=
+                            this.CountNumberOfUniqueWaysToTilePyramid(
+                                height - 1 - row
+                            ) *
+                            this.CountNumberOfUniqueWaysToTileSteps(
+                                row
+                            ) *
+                            this.CountNumberOfUniqueWaysToTileSteps(
+                                row
+                            );
+                    }
+                    this._numWaysToTilePyramidCache[height] 
+                        = numWaysToTilePyramid; ;
+                }
+            }
+            return this._numWaysToTilePyramidCache[height];
+        }
+
+        public float CountNumberOfUniqueWaysToTileSteps(int numSteps)
+        {
+            if (!this._numWaysToTileStepsCache.ContainsKey(numSteps))
+            {
+                if (numSteps <= 0)
+                {
+                    this._numWaysToTileStepsCache[numSteps] = 0;
+                }
+                else if (numSteps == 1)
+                {
+                    this._numWaysToTileStepsCache[numSteps] = 1;
+                }
+                else
+                {
+                    float numWaysToSumSteps = 0;
+                    for (int i = 1; i < numSteps - 1; i++)
+                    {
+                        numWaysToSumSteps
+                            += this.CountNumberOfUniqueWaysToTileSteps(i) *
+                            this.CountNumberOfUniqueWaysToTileSteps(numSteps - 1 - i);
+                    }
+                    numWaysToSumSteps += 2 * this.CountNumberOfUniqueWaysToTileSteps(
+                        numSteps - 1
+                    );
+                    this._numWaysToTileStepsCache[numSteps] = numWaysToSumSteps;
+                }
+            }
+            return this._numWaysToTileStepsCache[numSteps];
+        }
+    }
+}
