0a081af9377bbd27e280ee72ff5ff3d7178476b2
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2024-07-20 22:51:25 -0400

    Add CumulativeTrialOutcomeDistributionCalculator and Test

---
+++ Riddles/Riddles/Riddles.Tests/Probability/Core/CumulativeTrialOutcomeDistributionCalculatorTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Riddles.Probability.Core;
+using NUnit.Framework;
+
+namespace Riddles.Tests.Probability.Core
+{
+    public class CumulativeTrialOutcomeDistributionCalculatorTest
+    {
+        private double epsilon = 0.00000000001;
+        private Dictionary<int, (List<Dictionary<int, double>>, Dictionary<int, double>)>
+            _testCaseDictionary =
+                new Dictionary<int, (List<Dictionary<int, double>>, Dictionary<int, double>)>
+                {
+                    { 1, (
+                        new List<Dictionary<int, double>>{
+                            new Dictionary<int, double>{
+                                { 1, 0.75 },
+                                { 2, 0.25 },
+                            },
+                            new Dictionary<int, double>{
+                                { 3, 0.75 },
+                                { 4, 0.25 },
+                            },
+                            new Dictionary<int, double>{ 
+                                { 5, 0.75 }, 
+                                { 6, 0.25 },
+                            },
+                        },
+                        new Dictionary<int, double>{
+                            { 9, 27.0/64.0 },
+                            { 10, 27.0/64.0 },
+                            { 11, 9.0/64.0 },
+                            { 12, 1.0/64.0 }
+                        }
+                    )},
+                    { 2, (
+                        new List<Dictionary<int, double>>{
+                            new Dictionary<int, double>{
+                                { 4, 1.0/8.0 },
+                                { 3, 1.0/4.0 },
+                                { 2, 5.0/8.0 }
+                            },
+                            new Dictionary<int, double>{
+                                { 4, 1.0/8.0 },
+                                { 3, 1.0/4.0 },
+                                { 2, 5.0/8.0 }
+                            },
+                            new Dictionary<int, double>{
+                                { -4, 1.0/8.0 },
+                                { -3, 1.0/4.0 },
+                                { -2, 5.0/8.0 }
+                            },
+                        },
+                        new Dictionary<int, double>{
+                            { 0, 0.048828125 },
+                            { 1, 0.13671875 },
+                            { 2, 0.349609375 },
+                            { 3, 0.2578125 },
+                            { 4, 0.154296875 },
+                            { 5, 0.04296875 },
+                            { 6, 0.009765625 }
+                        }
+                    )},
+                };
+
+        [TestCase(1)]
+        [TestCase(2)]
+        public void TestCalculateCumulativeOutcomeDistributionForTrials(
+            int testCaseId)
+        {
+            var (trials, expected) = _testCaseDictionary[testCaseId];
+            var outcomeDistributionCalculator = new
+                CumulativeTrialOutcomeDistributionCalculator();
+            var actual = outcomeDistributionCalculator
+                .CalculateCumulativeOutcomeDistributionForTrials(
+                    (int x, int y) => x + y,
+                    trials,
+                    0,
+                    1.0
+                );
+            Assert.AreEqual(expected.Count, actual.Count);
+            foreach(var key in actual.Keys)
+            {
+                Assert.LessOrEqual(Math.Abs(actual[key] - expected[key]),
+                    epsilon);
+            }
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Probability/Core/CumulativeTrialOutcomeDistributionCalculator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Riddles.Probability.Core
+{
+    public class CumulativeTrialOutcomeDistributionCalculator
+    {
+        // if you have independent trials each with a probability distribution
+        // and the outcomes are aggregated, what is the probability of each
+        // outcome
+        public Dictionary<T, double> 
+            CalculateCumulativeOutcomeDistributionForTrials<T>(
+                Func<T, T, T> aggregation,
+                List<Dictionary<T, double>> distributions,
+                T startingValue,
+                double startingProbability
+            )
+        {
+            var result = new Dictionary<T, double>();
+            this.CalculateCumulativeOutcomeDistributionForTrials(
+                aggregation,
+                distributions,
+                startingValue,
+                startingProbability,
+                result
+            );
+            return result;
+        }
+
+        private void CalculateCumulativeOutcomeDistributionForTrials<T>(
+                Func<T, T, T> aggregation,
+                List<Dictionary<T, double>> distributions,
+                T value,
+                double probability,
+                Dictionary<T, double> result
+            )
+        {
+            if(distributions.Count == 0)
+            {
+                if (!result.ContainsKey(value))
+                {
+                    result[value] = 0;
+                }
+                result[value] += probability;
+                return;
+            }
+            var distribution = distributions[0];
+            var distributionsClone = distributions.ToList();
+            distributionsClone.RemoveAt(0);
+            foreach(var key in distribution.Keys)
+            {
+                this.CalculateCumulativeOutcomeDistributionForTrials(
+                    aggregation,
+                    distributionsClone,
+                    aggregation(value, key),
+                    probability * distribution[key],
+                    result
+                );
+            }
+        }
+
+    }
+}
