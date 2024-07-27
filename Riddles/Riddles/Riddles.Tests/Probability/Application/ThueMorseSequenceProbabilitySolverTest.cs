0cbf1678647ddc872d9ea493048a90ff42cd34d9
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2024-07-26 23:07:05 -0400

    Add in ThueMorseSequenceProbabilitySolver

---
+++ Riddles/Riddles/Riddles.Tests/Probability/Application/ThueMorseSequenceProbabilitySolverTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Riddles.Probability.Application;
+
+namespace Riddles.Tests.Probability.Application
+{
+    public class ThueMorseSequenceProbabilitySolverTest
+    {
+        private double epsilon = 0.00000000001;
+        private Dictionary<int, (
+            ThueMorseSequenceProbabilitySolver.Strategy,
+            double,
+            double)
+        > _testCaseDictionary = new Dictionary<int, (
+            ThueMorseSequenceProbabilitySolver.Strategy,
+            double,
+            double)>
+            {
+                { 1, (
+                        ThueMorseSequenceProbabilitySolver.Strategy.Alternating, 
+                        1.0/6.0,
+                        0.54545454545006478
+                    )
+                },
+                { 2, (
+                        ThueMorseSequenceProbabilitySolver.Strategy.Snake,
+                        1.0/6.0,
+                        0.50819672130565718
+                    )
+                },
+                { 3, (
+                        ThueMorseSequenceProbabilitySolver.Strategy.ThueMorse,
+                        1.0/6.0,
+                        0.50159033919854623
+                    )
+                },
+            };
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        public void TestCalculateOddsOfFirstPersonWinning(
+            int testCaseId
+        )
+        {
+            var (strategy, oddsOfSuccess, expected) =
+                this._testCaseDictionary[testCaseId];
+            var sequenceProbabilitySolver =
+                new ThueMorseSequenceProbabilitySolver();
+            var actual = sequenceProbabilitySolver
+                .CalculateOddsOfFirstPersonWinning(
+                    strategy,
+                    oddsOfSuccess,
+                    epsilon
+                );
+            Assert.LessOrEqual(Math.Abs(expected - actual), epsilon);
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Probability/Application/ThueMorseSequenceProbabilitySolver.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Riddles.Probability.Application
+{
+    // https://thefiddler.substack.com/p/can-you-even-the-odds
+    // you and your friend are taking turns rolling a dice to see
+    // who is the first to roll a 5. What are your odds of winning
+    // 1) if you alternate turns
+    // 2) if you use a snake draft
+    // 3) if you use Thue-Morse
+    public class ThueMorseSequenceProbabilitySolver
+    {
+        public enum Strategy
+        {
+            Alternating,
+            Snake,
+            ThueMorse
+        }
+
+        public double CalculateOddsOfFirstPersonWinning(
+            Strategy strategy,
+            double oddsOfSuccess,
+            double epsilon
+        )
+        {
+            double oddsOfPlayerOneWinning = 0.0;
+            double oddsOfPlayerTwoWinning = 0.0;
+            List<bool> playerRollingThueMorse = new List<bool> { true };
+
+            int numRolls = 0;
+            List<bool> playersRolling = new List<bool>();
+            while(oddsOfPlayerOneWinning + oddsOfPlayerTwoWinning < 
+                1.0 - epsilon)
+            {
+                bool playerRolling = false;
+                (oddsOfPlayerOneWinning, oddsOfPlayerTwoWinning, playerRolling) 
+                    = this.UpdateOddsForNextRoll(
+                        strategy,
+                        oddsOfSuccess,
+                        oddsOfPlayerOneWinning,
+                        oddsOfPlayerTwoWinning,
+                        numRolls,
+                        playerRollingThueMorse
+                    );
+                playersRolling.Add( playerRolling );
+                numRolls++;
+            }
+            return oddsOfPlayerOneWinning;
+        }
+
+        private (double, double, bool) UpdateOddsForNextRoll(
+            Strategy strategy,
+            double oddsOfSuccess,
+            double cumulativeOddsOfPlayerOneWinning,
+            double cumulativeOddsOfPlayerTwoWinning,
+            int numPreviousRolls,
+            List<bool> playerRollingThueMorse
+        )
+        {
+            double oddsOfWinningThisRoll =
+                Math.Pow(1 - oddsOfSuccess, numPreviousRolls) * oddsOfSuccess;
+            bool isPlayerOneRolling = strategy
+                switch 
+                {
+                    Strategy.Alternating => 
+                        numPreviousRolls % 2 == 0,
+                    Strategy.Snake => 
+                        numPreviousRolls % 4 == 0
+                        || numPreviousRolls %4 == 3,
+                    Strategy.ThueMorse => 
+                        this.GetPlayerRollingThueMorse(
+                            numPreviousRolls,
+                            playerRollingThueMorse
+                        )
+                };
+            if (isPlayerOneRolling)
+            {
+                cumulativeOddsOfPlayerOneWinning += oddsOfWinningThisRoll;
+            }
+            else
+            {
+                cumulativeOddsOfPlayerTwoWinning += oddsOfWinningThisRoll;
+            }
+            return (
+                cumulativeOddsOfPlayerOneWinning, 
+                cumulativeOddsOfPlayerTwoWinning,
+                isPlayerOneRolling
+            );
+        }
+
+        private bool GetPlayerRollingThueMorse(
+            int numPreviousRolls,
+            List<bool> playerRollingThueMorse
+        )
+        {
+            if(numPreviousRolls >= playerRollingThueMorse.Count)
+            {
+                var copy = playerRollingThueMorse
+                .Select(x => !x)
+                .ToList();
+                playerRollingThueMorse.AddRange(copy);
+            }
+            return playerRollingThueMorse[numPreviousRolls];
+        }
+    }
+}
