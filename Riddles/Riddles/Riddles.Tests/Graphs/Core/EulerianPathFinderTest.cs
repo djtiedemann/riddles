881d1f6a8da1d0b7c2005dd93355e6587f158e08
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2024-10-01 21:11:14 -0400

    Add in EulerianPathFinder

---
+++ Riddles/Riddles/Riddles.Tests/Graphs/Core/EulerianPathFinderTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Riddles.Graphs.Core;
+using NUnit.Framework;
+using System.Linq;
+
+namespace Riddles.Tests.Graphs.Core
+{
+    public class EulerianPathFinderTest
+    {
+        [Test]
+        public void TestGetRockPaperScissorsPaths()
+        {
+            var rockPaperScissorsAdjacencies =
+                new Dictionary<string, HashSet<string>>
+                {
+                    { "Rock", new HashSet<string> { "Scissors" } },
+                    { "Scissors", new HashSet<string> { "Paper" } },
+                    { "Paper", new HashSet<string> { "Rock" } },
+                };
+
+            var eulerianPathFinder = new EulerianPathFinder();
+            var paths = eulerianPathFinder.FindPaths(
+                rockPaperScissorsAdjacencies
+            );
+            this.VerifyUniqueness(paths);
+            Assert.AreEqual(3, paths.Count);
+        }
+
+        [Test]
+        public void TestGetRockPaperScissorsLizardSpockPaths()
+        {
+            var rockPaperScissorsAdjacencies =
+                new Dictionary<string, HashSet<string>>
+                {
+                    { "Rock", new HashSet<string> { "Scissors", "Lizard" } },
+                    { "Scissors", new HashSet<string> { "Paper", "Lizard" } },
+                    { "Paper", new HashSet<string> { "Rock", "Spock" } },
+                    { "Lizard", new HashSet<string> { "Paper", "Spock" } },
+                    { "Spock", new HashSet<string> { "Scissors", "Rock" } },
+                };
+
+            var eulerianPathFinder = new EulerianPathFinder();
+            var paths = eulerianPathFinder.FindPaths(
+                rockPaperScissorsAdjacencies
+            );
+            this.VerifyUniqueness(paths);
+            Assert.AreEqual(110, paths.Count);
+        }
+
+        private void VerifyUniqueness(List<List<string>> paths)
+        {
+            var pathStrings = paths.Select(
+                p => p.Aggregate("", (agg, y) => $"{agg}{y}")
+            ).Distinct();
+            Assert.AreEqual(paths.Count, pathStrings.Count());
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Graphs/Core/EulerianPathFinder.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Riddles.Graphs.Core
+{
+    public class EulerianPathFinder
+    {
+        public EulerianPathFinder() { }
+
+        public List<List<string>> FindPaths(Dictionary<string, HashSet<string>> adjacencies)
+        {
+            var numEdges = adjacencies.Sum(
+                g => g.Value.Count
+            );
+
+            var paths = new List<List<string>>();
+            foreach(var startingNode in adjacencies.Keys)
+            {
+                var newPaths = this.GetPaths(
+                    startingNode,
+                    adjacencies,
+                    new List<string> { startingNode },
+                    new List<List<string>>(),
+                    numEdges
+                );
+                paths.AddRange( newPaths );
+            }
+            return paths;
+        }
+
+        private List<List<string>> GetPaths(
+            string currentNode,
+            Dictionary<string, HashSet<string>> adjacencies,
+            List<string> currentPath,
+            List<List<string>> allPaths,
+            int numEdges
+        )
+        {
+            // if we've travelled across all edges, add the path to the list
+            if(currentPath.Count() == numEdges + 1)
+            {
+                allPaths.Add(currentPath);
+            }
+            // if there are no remaining paths from here, return
+            if (adjacencies[currentNode].Count == 0)
+            {
+                return allPaths;
+            }
+
+            var nextNodes = adjacencies[currentNode].ToList();
+            foreach(var nextNode in nextNodes)
+            {
+                var clonedPath = currentPath.ToList();
+                clonedPath.Add(nextNode);
+                adjacencies[currentNode].Remove(nextNode);
+                this.GetPaths(
+                    nextNode,
+                    adjacencies,
+                    clonedPath,
+                    allPaths,
+                    numEdges
+                );
+                // add the edge back to the potential list of edges
+                adjacencies[currentNode].Add(nextNode);
+            }
+            return allPaths;
+        }
+    }
+}
