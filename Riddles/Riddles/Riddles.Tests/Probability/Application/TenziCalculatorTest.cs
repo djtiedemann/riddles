d225b13955558bc6e26f546ad0f14703e15e0688
djtiedemann <54342366+djtiedemann@users.noreply.github.com>
2024-10-06 08:14:32 -0400

    Add TenziCalculator

---
+++ Riddles/Riddles/Riddles.Tests/Probability/Application/TenziCalculatorTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Riddles.Probability.Application;
+using NUnit.Framework;
+
+namespace Riddles.Tests.Probability.Application
+{
+    public class TenziCalculatorTest
+    {
+        private double epsilon = 0.00000000001;
+        private Dictionary<int, (int, int, double)> _testCaseDictionary
+            = new Dictionary<int, (int, int, double)>
+            {
+                { 1, (10, 6, 3.4447466960702133)},
+                { 2, (3, 6, 53.0/36.0)}
+            };
+
+        [TestCase(1)]
+        [TestCase(2)]
+        public void TestCalculateExpectedNumDiceSetAside(int testCaseId)
+        {
+            var tenziCalculator = new TenziCalculator();
+            var testCase = this._testCaseDictionary[testCaseId];
+            var expectedValue = tenziCalculator.CalculateExpectedNumDiceSetAside(
+                numDice: testCase.Item1,
+                numSides: testCase.Item2
+            );
+            Assert.LessOrEqual( 
+                Math.Abs(expectedValue - testCase.Item3),
+                epsilon);
+        }
+    }
+}
---
+++ Riddles/Riddles/Riddles/Probability/Application/TenziCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Riddles.Combinatorics.Application;
+
+namespace Riddles.Probability.Application
+{
+    public class TenziCalculator
+    {
+        private MostCommonDieRolledDistributionCalculator _distributionCalculator;
+        public TenziCalculator() { 
+            this._distributionCalculator = new MostCommonDieRolledDistributionCalculator();
+        }
+
+        public double CalculateExpectedNumDiceSetAside(int numDice, int numSides)
+        {
+            var distribution = this._distributionCalculator
+                .CalculateDistributionOfMostCommonValuesRolled(
+                    numDice: numDice,
+                    numSides: numSides
+                );
+            var totalOutcomes = distribution.Sum(v => v.Value);
+            var expectedValue =
+                distribution.Select(x => x.Key * x.Value / totalOutcomes)
+                .Sum();
+            return expectedValue;
+        }
+    }
+}
